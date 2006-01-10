da257ace788ad7846a122bcbac5edc00ce6a1094
Giovanni Bajo <rasky@develer.com>
2006-01-10 11:13:32 +0000

    Fix module name so that importing without the full PyCrypto path works.

--- source/crypto/block_template.c
+++ source/crypto/block_template.c
@@ -735,7 +735,7 @@ _MODULE_NAME (void)
 	ALGtype.ob_type = &PyType_Type;
 
 	/* Create the module and add the functions */
-	m = Py_InitModule("Crypto.Cipher." _MODULE_STRING, modulemethods);
+	m = Py_InitModule(_MODULE_STRING, modulemethods);
 
 	PyModule_AddIntConstant(m, "MODE_ECB", MODE_ECB);
 	PyModule_AddIntConstant(m, "MODE_CBC", MODE_CBC);
