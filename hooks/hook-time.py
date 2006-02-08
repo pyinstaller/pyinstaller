6fccdf27ba3eb8f1f93826baa9248da5e1bd9066
Giovanni Bajo <rasky@develer.com>
2006-02-08 12:11:56 +0000

    Add import hook for time.strptime.

---
+++ hooks/hook-time.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2006, Giovanni Bajo
+# Based on previous work under copyright (c) 2001, 2002 McMillan Enterprises, Inc.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+
+import sys
+
+# Since Python 2.3, builtin module "time" imports Python module _strptime
+# to implement "time.strptime".
+if hasattr(sys, "version_info") and sys.version_info >= (2,3):
+    hiddenimports = ['_strptime']
