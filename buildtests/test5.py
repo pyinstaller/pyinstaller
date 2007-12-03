6bcc4c20f88e82adb4a184e0271824feb78b54fc
Matteo Bertini <naufraghi@develer.com>
2007-12-03 19:03:12 +0000

    Fix some fragile imports in tests.

--- buildtests/test5.py
+++ buildtests/test5.py
@@ -15,5 +15,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 print "test5 - W ignore"
-import regex
+try:
+    import regex
+except ImportError:
+    import re
 print "test5 - done"
