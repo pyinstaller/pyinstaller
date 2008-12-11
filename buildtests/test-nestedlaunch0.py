11aff5a7a69a86831a62aa617a7e6452401294a2
Matteo Bertini <naufraghi@develer.com>
2008-12-11 19:03:08 +0000

    Add a nested launch test (onefile launches another onefile).

---
+++ buildtests/test-nestedlaunch0.py
@@ -0,0 +1,5 @@
+import cmath
+
+if __name__ == "__main__":
+    print dir()
+
---
+++ buildtests/test-nestedlaunch1.py
@@ -0,0 +1,5 @@
+import subprocess
+import datetime
+
+if __name__ == "__main__":
+    subprocess.check_call(["test-nestedlaunch0"])
