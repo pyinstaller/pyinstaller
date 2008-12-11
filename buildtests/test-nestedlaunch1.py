82bcb68551adf60d3bcb87a886f26b289e0ea17d
Matteo Bertini <naufraghi@develer.com>
2008-12-11 19:20:58 +0000

    Fix nested onefile test.

--- buildtests/test-nestedlaunch1.py
+++ buildtests/test-nestedlaunch1.py
@@ -1,5 +1,7 @@
+import os
+import sys
 import subprocess
 import datetime
 
 if __name__ == "__main__":
-    subprocess.check_call(["test-nestedlaunch0"])
+    subprocess.check_call([os.path.dirname(sys.executable) + "/../test-nestedlaunch0/test-nestedlaunch0"])
