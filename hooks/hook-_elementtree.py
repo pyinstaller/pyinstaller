7dc166b88159f7e9a99dc7ada8584949655517f1
Giovanni Bajo <rasky@develer.com>
2009-03-01 10:55:25 +0000

    Improve the elementtree hook.

--- hooks/hook-_elementtree.py
+++ hooks/hook-_elementtree.py
@@ -15,4 +15,4 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 
-hiddenimports = ['pyexpat', 'xml.etree.ElementTree']
+hiddenimports = ['pyexpat', 'xml.etree.ElementTree', 'copy']
