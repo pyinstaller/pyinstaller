4fa3345d83d48e9c992f966509e359b8740b3606
Giovanni Bajo <rasky@develer.com>
2009-05-04 10:57:08 +0000

    Remove Configure step after make for symmetry with other platforms.

--- make-linux.sh
+++ make-linux.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "USAGE $0 some-python-version"
-cd source/linux && $1 Make.py && make clean && make && cd - && $1 Configure.py
+cd source/linux && $1 Make.py && make clean && make && cd -
 
