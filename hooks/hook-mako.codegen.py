3055d6b0aa2432524577ffc8a2057fe76e9b25b6
Giovanni Bajo <rasky@develer.com>
2009-05-18 12:44:56 +0000

    Ticket #61: add import hook for mako.

---
+++ hooks/hook-mako.codegen.py
@@ -0,0 +1,3 @@
+# codegen generates Python code that is then executed through exec().
+# This Python code imports the following modules.
+hiddenimports = ['mako.cache', 'make.runtime', 'mako.filters']
