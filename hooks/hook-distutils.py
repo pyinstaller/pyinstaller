374d857cc0f133c428f00d2dac1943e5dcb2f410
Giovanni Bajo <rasky@develer.com>
2009-08-01 16:13:21 +0000

    Ticket #60: fix distutils package when used under virtualenv

---
+++ hooks/hook-distutils.py
@@ -0,0 +1,12 @@
+# Contributed by jkp@kirkconsulting.co.uk
+# This hook checks for the distutils hacks present when using the 
+# virtualenv package.
+def hook(mod):
+    import distutils
+    if hasattr(distutils, "distutils_path"):
+        import os
+        import marshal
+        mod_path = os.path.join(distutils.distutils_path, "__init__.pyc")
+        parsed_code = marshal.loads(open(mod_path, "r").read()[8:])
+        mod.__init__('distutils', mod_path, parsed_code)
+    return mod
