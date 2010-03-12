d7260d180a8fb5d342fd89a8b0b74e0579ab2adf
Giovanni Bajo <rasky@develer.com>
2010-03-12 19:47:07 +0000

    Ticket #88: Fix missing matplolib submodule.

--- hooks/hook-matplotlib.numerix.py
+++ hooks/hook-matplotlib.numerix.py
@@ -1,6 +1,9 @@
 # Contributed by Peter Burgers
 # The matplotlib.numerix package sneaks these imports in under the radar:
-hiddenimports = ['fft',
-                 'linear_algebra',
-                 'random_array',
-                ]
+hiddenimports = [
+    'fft',
+    'linear_algebra',
+    'random_array',
+    'ma',
+]
+
