9182a7816bdcae6f729f96d11434673a22f5099f
Giovanni Bajo <rasky@develer.com>
2010-03-18 10:08:43 +0000

    Ticket #168: fix hook distutils under Windows.

--- hooks/hook-distutils.py
+++ hooks/hook-distutils.py
@@ -7,6 +7,6 @@ def hook(mod):
         import os
         import marshal
         mod_path = os.path.join(distutils.distutils_path, "__init__.pyc")
-        parsed_code = marshal.loads(open(mod_path, "r").read()[8:])
+        parsed_code = marshal.loads(open(mod_path, "rb").read()[8:])
         mod.__init__('distutils', mod_path, parsed_code)
     return mod
