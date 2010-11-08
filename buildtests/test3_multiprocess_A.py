264a8e0ab46130da9c692cf3fd47c745849d6331
Gendo Ikari <gendoikari@delever.com>
2010-11-08 15:38:56 +0000

    Added four test cases for multiprocess feature.

---
+++ buildtests/test3_multiprocess_A.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from PyQt4 import Qt
+import os
+import sys
+import numpy
+
+def main():
+    print "Hello World!"
+
+if __name__ == "__main__":
+    main()
---
+++ buildtests/test4_multiprocess_B.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from PyQt4 import Qt
+import os
+import numpy
+
+def main():
+    print "Hello World!"
+
+if __name__ == "__main__":
+    main()
