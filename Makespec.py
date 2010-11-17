3a8fb4a40392b2bc577ffac45be7464392c1c2b2
Daniel Sanchez <codeverse@develer.com>
2010-11-17 09:50:49 +0000

    Reorganizing code.

--- Makespec.py
+++ Makespec.py
@@ -24,23 +24,49 @@ import os
 import sys
 
 # For Python 1.5 compatibility
-# if keywords True and False don't exist
-# it sets them manually
 try:
     True
 except:
     True = (1 is 1)
     False = not True
 
-# This is the part of the spec present both in onefile and in
-# onedir template
-# It is the part that the user has to edit on his own
-common_part = """\
-############################################################
-# This file was automatically genereted by the Makespec.py #
-############################################################
+# This is the part of the spec present in both onefile and onedir template
+common_part = """# -*- mode: python -*-
+#(i) This file was automatically genereted by the Makespec.py
 
-############################################################
+
+###########################
+### Edit it to your liking
+
+name_of_exe = '%(exename)s'
+path_to_exe = %(pathex)s
+
+# Set here your resources paths as strings
+#  If you don't set paths, PyInstaller won't be able to find them
+resourcesPaths = [
+#   ("/where/to/find","/where/to/put")
+#   ("/path/to/images","../relative/path/to/images")
+#   ("/path/to/fonts","/my/home/project/fonts")
+#   ("/path/to/configfiles","./config/files")
+#   ("/these/are/only/examples","../../this/too")
+]
+
+useDebug = True # set True or False
+
+# Remove the Debug symbols from the ELF executable (only for UNIX)
+useStrip = False # set True or False
+
+# UPX Packer (good for Windows)
+useUPX = True # set True or False
+
+
+##############################
+### Only for PyInstaller eyes
+#
+#(!) Edit with *caution*
+#(i) For more information take a check out the documentation
+#    on www.pyinstaller.org
+
 def collectResources(exploring_path, final_path, debug=False):
     import os
     data = []
@@ -56,43 +82,6 @@ def collectResources(exploring_path, final_path, debug=False):
     if debug:
         (print "Found", filename[0]) for filename in data
     return data
-############################################################
-
-############################################################
-### Edit it to your liking
-
-name_of_exe = '%(exename)s'
-
-paths_of_srcs = %(paths)s
-
-# Set here your resources paths as strings
-#  If you don't set paths, PyInstaller won't be able to find them
-resourcesPaths = [
-#   ("/where/to/find","/where/to/put")
-#   ("/path/to/images","../relative/path/to/images")
-#   ("/path/to/fonts","/my/home/project/fonts")
-#   ("/path/to/configfiles","./config/files")
-#   ("/these/are/only/examples","../../this/too")
-]
-
-useDebug = True # set True or False
-
-
-# This will remove the Debug symbols from the ELF executable
-# making it smaller (only for UNIX)
-useStrip = False # set True or False
-
-# UPX is an executable packer that makes the executable
-# smaller. It is convenient especially under Windows
-useUPX = True # set True or False
-
-
-############################################################
-### Only for PyInstaller eyes
-
-#(!) Edit with caution
-#(i) For more information take a check out the documentation
-#    on www.pyinstaller.org
 
 a = Analysis(
     %(scripts)s,
@@ -101,9 +90,6 @@ a = Analysis(
 pyz = PYZ(a.pure)
 """
 
-# The OneDir template generate (as final package) a directory
-# package containing the executable, the dynamic libraries and
-# all the resources needed by the program
 onedir_tpl = """
 exe = EXE(
     pyz,
@@ -119,14 +105,12 @@ coll = COLLECT(
     a.binaries,
     a.zipfiles,
     a.datas,
+    [collectResources(src, dest, useDebug) for src, dest in resourcesPaths]
     strip=useStrip,
     upx=useUPX,
     name=os.path.join(path_to_exe, name_of_exe)
 """
 
-# The OneFile template generate (as final package) an executable
-# containing dynamic libraries, data resources, and the pure modules.
-# When the executable will be launched
 onefile_tpl = """
 exe = EXE(
     pyz,
@@ -134,7 +118,7 @@ exe = EXE(
     a.binaries,
     a.zipfiles,
     a.datas,
-    [collectResources(src, dest) for src, dest in resourcesPaths],
+    [collectResources(src, dest, useDebug) for src, dest in resourcesPaths],
     name=os.path.join(path_to_exe, name_of_exe),
     debug=useDebug,
     strip=useStrip,
@@ -143,46 +127,29 @@ exe = EXE(
 
 HOME = os.path.abspath(os.path.dirname(sys.argv[0]))
 
-def createSpecFile(scripts, options):
-
+def createSpecFile(exename, scripts, options):
     #configfile = os.path.join(HOME, "config.dat")
     configfile = "/home/codeverse/.pyinstaller/config.dat"
+    workingdir = os.path.join(os.getcwd())
 
     try:
         config = eval(open(configfile, 'r').read())
     except IOError:
-        raise SystemExit("Configfile is missing or unreadable. Please run Configure.py before building!")
+        msg = "Configfile is missing or unreadable. Please run Configure.py before building!"
+        raise SystemExit(msg)
 
-    if config['pythonVersion'] != sys.version:
-        print "The current version of Python is not the same with which PyInstaller was configured."
-        print "Please re-run Configure.py with this version."
-        raise SystemExit(1)
+    if config["pythonVersion"] != sys.version:
+        msg = """PyInstaller configfile and current Python version are incompatible.
+            Please re-run Configure.py with this version."""
+        raise SystemExit(msg)
 
-    exename, filetype = os.path.splitext(os.path.basename(scripts[0]))
-
-    # Check for parsing errors
-    if filetype == ".spec":
-        if len(scripts) > 1:
-            parser.error("Too many arguments. Give only one spec at time")
-
-        try:
-            old_specfile = open(exename + ".spec", 'r')
-        except IOError as (errno, strerror):
-            print "I/O error(%s): %s" % (errno, strerror)
-
-    elif filetype == ".py":
-        for filename in scripts:
-            if not filetype in filename:
-                parser.error("Arguments must be all python scripts (*.py)")
-    else:
-        parser.error("Give in input .py or .spec files only")
-
+    pathex = os.path.dirname(os.path.join(workingdir, scripts[0]))
 
     dic = {
         "exename"   : exename,
-        "paths"     : "blablabla",
+        "pathex"    : [pathex],
         "scripts"   : scripts,
-        "builddir"  : os.getcwd(),
+        "builddir"  : os.path.dirname(os.path.join(pathex, scripts[0])),
         "onedir"    : options["onedir"],
         "onefile"   : not options["onedir"]}
 
@@ -213,9 +180,8 @@ if __name__ == '__main__':
 
     import pyi_optparse as optparse
 
-    # Creating an argument parser
     parser = optparse.OptionParser(
-        usage = "usage: %prog [--onefile | --onedir] <scriptname> [<scriptname ...] | <specname>")
+        usage = "usage: %prog [--onefile | --onedir] <scriptname> [<scriptname> ...] | <specname>")
 
     parser.add_option(
         "-F", "--onefile", dest="onedir", action="store_false", default=True,
@@ -224,19 +190,35 @@ if __name__ == '__main__':
         "-D", "--onedir", dest="onedir", action="store_true", default=True,
         help="Create a single directory deployment")
 
-
     opts, args = parser.parse_args()
     opts = opts.__dict__
+
+    # Check for parsing errors
+    if not args:
+        parser.error('Requires at least one scriptname file')
+
+    exename, filetype = os.path.splitext(os.path.basename(args[0]))
+
+    if filetype == ".spec":
+        if len(scripts) > 1:
+            parser.error("Too many arguments. Give only one spec at time")
+        try:
+            old_specfile = open(exename + ".spec", 'r')
+        except IOError as (errno, strerror):
+            print "I/O error(%s): %s" % (errno, strerror)
+    elif filetype == ".py":
+        for filename in args:
+            if not filetype in filename:
+                parser.error("Arguments must be all python scripts (*.py)")
+    else:
+        parser.error("Give in input .py or .spec files only")
+
+    specfile_name = createSpecFile(exename, args, opts)
 
     if opts["onedir"]:
         dep_mode = "onedir"
     else:
         dep_mode = "onefile"
-
-    if not args:
-        parser.error('Requires at least one scriptname file')
-
-    specfile_name = createSpecFile(args, opts)
 
     print "%s has been wrote in %s mode" % (os.path.join(os.getcwd(), specfile_name), dep_mode)
     print "Now you can edit it and run the Build.py"
