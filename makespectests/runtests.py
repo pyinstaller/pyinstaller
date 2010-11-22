78c0c529e8d3b05ab74ab6c9b74ad0db25acf718
Daniel Sanchez <codeverse@develer.com>
2010-11-22 15:21:36 +0000

    Implementing runtests.py with unittest

--- makespectests/runtests.py
+++ makespectests/runtests.py
@@ -2,39 +2,67 @@
 import glob
 import os, sys
 import shutil
+import unittest
 
 MST_DIR = os.path.abspath(os.path.split(sys.argv[0])[0])
 HOME = os.path.normpath(os.path.join(MST_DIR, ".."))
-
-TO_CLEAN = """python_exe.build
+MAKESPEC_EXE = os.path.join(HOME, "Makespec.py")
+BUILD_EXE = os.path.join(HOME, "Build.py")
+SCRIPT_FOR_TEST = os.path.join(MST_DIR, "script_for_tests.py")
+CLEANUP = """python_exe.build
 logdict*.log
 warn*.txt
 *.py[co]
 */*.py[co]
 */*/*.py[co]
+*.spec
 build/
 dist/
 """.split()
 
-def clean():
-    for pattern in TO_CLEAN:
-        clean_list = glob.glob(pattern)
-        for path in clean_list:
-            try:
-                if os.path.isdir(path):
-                    shutil.rmtree(path)
-                else:
-                    os.remove(path)
-            except OSError, e:
-                print e
+class MakespecTest(unittest.TestCase):
+
+    def setUp(self):
+        """
+        Init the spec files used for tests
+        """
+        os.system("%s -n spec_od --onedir %s" % (MAKESPEC_EXE, os.path.join(MST_DIR, SCRIPT_FOR_TEST)))
+        os.system("%s -n spec_of --onefile %s" % (MAKESPEC_EXE, os.path.join(MST_DIR, SCRIPT_FOR_TEST)))
+
+    def tearDown(self):
+        """
+        Clean all the directory removing files created for tests
+        """
+        for clean in CLEANUP:
+            clean = glob.glob(clean)
+            for path in clean:
+                try:
+                    if os.path.isdir(path):
+                        shutil.rmtree(path)
+                    else:
+                        os.remove(path)
+                except OSError, e:
+                    print e
+
+    def test_build_onedir(self):
+        os.system("%s -y spec_od.spec" % BUILD_EXE)
+
+    def test_build_onefile(self):
+        os.system("%s -y spec_of.spec" % BUILD_EXE)
+
+    def test_switch_to_onedir(self):
+        """
+        Tests the validity of switching a spec file from onefile to onedir deployment
+        """
+        os.system("%s -n _spec_od --onedir spec_of.spec" % MAKESPEC_EXE)
+        os.system("%s -y _spec_od.spec" % BUILD_EXE)
+
+    def test_switch_to_onefile(self):
+        """
+        Tests the validity of switching a spec file from onedir to onefile deployment
+        """
+        os.system("%s -n _spec_of --onefile %s" % (MAKESPEC_EXE, "spec_od.spec"))
+        os.system("%s -y _spec_of.spec" % BUILD_EXE)
 
 if __name__ == "__main__":
-    tests = glob.glob(os.path.join(MST_DIR, "test*.py"))
-    for t in tests:
-        testname = "%s" % os.path.basename(os.path.splitext(t)[0]).upper()
-        print testname, ":"
-        res = os.system("python %s" % t)
-        if res != 0:
-            raise SystemExit("%s failed." % t)
-
-        clean()
+    unittest.main()
