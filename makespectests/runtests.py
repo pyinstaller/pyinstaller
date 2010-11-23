68a6409f0c9f25f00826972c8107ab60a00309e9
Daniel Sanchez <codeverse@develer.com>
2010-11-23 10:04:44 +0000

    surrounding systems calls with assert

--- makespectests/runtests.py
+++ makespectests/runtests.py
@@ -20,20 +20,25 @@ warn*.txt
 build/
 dist/
 """.split()
+newSpecFail = "Unable to makespec %s"
+switchSpecFail = "Unable to convert the %s"
+buildFail = "Unable to build the %s file"
 
 class MakespecTest(unittest.TestCase):
-
     def setUp(self):
         """
         Init the spec files used for tests
         """
-        os.system("%s -n spec_od --onedir %s" % (MAKESPEC_EXE, SCRIPT_FOR_TEST))
-        os.system("%s -n spec_of --onefile %s" % (MAKESPEC_EXE, SCRIPT_FOR_TEST))
+        self.assertEqual(os.system("%s -n spec_od --onedir %s" % (MAKESPEC_EXE, SCRIPT_FOR_TEST)), 0,
+            newSpecFail % SCRIPT_FOR_TEST)
+        self.assertEqual(os.system("%s -n spec_of --onefile %s" % (MAKESPEC_EXE, SCRIPT_FOR_TEST)), 0,
+            newSpecFail % SCRIPT_FOR_TEST)
 
     def tearDown(self):
+        """\
+        Cleaning tests resouces
         """
-        Clean all the directory removing files created for tests
-        """
+        self.shortDescription()
         for clean in CLEANUP:
             clean = glob.glob(clean)
             for path in clean:
@@ -46,33 +51,67 @@ class MakespecTest(unittest.TestCase):
                     print e
 
     def test_build_onedir(self):
-        os.system("%s -y spec_od.spec" % BUILD_EXE)
+        """\
+        Building onedir spec deployment
+        """
+        self.shortDescription()
+        self.assertEqual(os.system("%s -y spec_od.spec" % BUILD_EXE), 0,
+            buildFail % "spec_od.spec")
 
     def test_build_onefile(self):
-        os.system("%s -y spec_of.spec" % BUILD_EXE)
+        """\
+        Building onefile spec deployment
+        """
+        self.shortDescription()
+        self.assertEqual(os.system("%s -y spec_of.spec" % BUILD_EXE), 0,
+            buildFail % "spec_of.spec")
 
     def test_switch_to_onedir(self):
+        """\
+        Switching a spec file from onefile to onedir deployment
         """
-        Tests the validity of switching a spec file from onefile to onedir deployment
-        """
-        os.system("%s -n _spec_od --onedir spec_of.spec" % MAKESPEC_EXE)
-        os.system("%s -y _spec_od.spec" % BUILD_EXE)
+        self.shortDescription()
+        self.assertEqual(os.system("%s -n _spec_od --onedir spec_of.spec" % MAKESPEC_EXE), 0,
+            switchSpecFail % "spec_of.spec")
+        self.assertEqual(os.system("%s -y _spec_od.spec" % BUILD_EXE), 0,
+            buildFail % "_spec_od.spec")
 
     def test_switch_to_onefile(self):
+        """\
+        Switching a spec file from onedir to onefile deployment
         """
-        Tests the validity of switching a spec file from onedir to onefile deployment
-        """
-        os.system("%s -n _spec_of --onefile spec_od.spec" % MAKESPEC_EXE)
-        os.system("%s -y _spec_of.spec" % BUILD_EXE)
+        self.shortDescription()
+        self.assertEqual(os.system("%s -n _spec_of --onefile spec_od.spec" % MAKESPEC_EXE), 0,
+            switchSpecFail % "spec_od.spec")
+        self.assertEqual(os.system("%s -y _spec_of.spec" % BUILD_EXE), 0,
+            buildFail % "_spec_of.spec")
 
     def test_spec_edit(self):
-        # script to edit for editing tests
-        # re-run runtests.py if files do no exist
+        """\
+        Building an edited spec (try to edit to_edit.spec manually first)
+        """
+        self.shortDescription()
+        # edit the to_edit.spec file before running this test
         if not os.path.isfile(os.path.join(MST_DIR, "to_edit.spec")):
-            os.system("%s -n to_edit %s" % (MAKESPEC_EXE, SCRIPT_FOR_TEST))
-            print "### Test spec_edit skipped: edit the public part of to_edit.spec and re-run runtests.py"
+            self.assertEqual(os.system("%s -n to_edit %s" % (MAKESPEC_EXE, SCRIPT_FOR_TEST)), 0,
+                newSpecFail % SCRIPT_FOR_TEST)
+        else:
+            self.assertEqual(os.system("%s -y to_edit.spec" % BUILD_EXE), 0,
+                buildFail % "to_edit.spec")
 
-        os.system("%s -y to_edit.spec" % BUILD_EXE)
+    def test_switch_edited_file(self):
+        """\
+        Switching an edited spec file
+        """
+        self.shortDescription()
+        if not os.path.isfile(os.path.join(MST_DIR, "to_edit.spec")):
+            self.assertEqual(os.system("%s -n to_edit %s" % (MAKESPEC_EXE, SCRIPT_FOR_TEST)), 0,
+                newSpecFail % SCRIPT_FOR_TEST)
+        else:
+            self.assertEqual(os.system("%s -n _to_edit --onefile to_edit.spec" % MAKESPEC_EXE), 0,
+                switchSpecFail % "to_edit.spec")
+            self.assertEqual(os.system("%s -y _to_edit.spec" % BUILD_EXE), 0,
+                buildFail % "_to_edit.spec")
 
 if __name__ == "__main__":
     unittest.main()
