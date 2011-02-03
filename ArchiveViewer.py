5d0ff830b4f3600b4a72b89a5dcd16e743aa754e
Hartmut Goebel <h.goebel@goebel-consult.de>
2011-02-03 00:57:29 +0000

    Moving modules into PyInstaller/.

--- ArchiveViewer.py
+++ ArchiveViewer.py
@@ -16,8 +16,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
-import archive
-import carchive
+
+import PyInstaller.archive as archive
+import PyInstaller.carchive as carchive
+
 import sys, string, tempfile, os
 try:
     import zlib
