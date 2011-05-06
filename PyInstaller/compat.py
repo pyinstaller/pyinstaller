570c94a87edfbb35838a9e953a9036a1666a573e
Martin Zibricky <mzibr.public@gmail.com>
2011-05-06 14:19:24 +0000

    Ticket #331: Forgot to upload a file.

---
+++ PyInstaller/compat.py
@@ -0,0 +1,71 @@
+#! /usr/bin/env python
+#
+# Various classes and functions to provide some backwards-compatibility
+# with previous versions of Python from 2.3 onward.
+#
+# Copyright (C) 2011, Martin Zibricky
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+
+import os
+import sys
+
+
+try:
+    # Python 2.5+
+    import hashlib
+except ImportError:
+    import md5
+    import sha
+
+    class _Hash(object):
+        def __init__(self):
+            self.md5 = md5.new
+            self.sha = sha.new
+    hashlib = _Hash()
+
+
+def architecture():
+    """
+    Returns the bit depth of the python interpreter's architecture as
+    a string ('32bit' or '64bit'). Similar to platform.architecture(),
+    but with fixes for universal binaries on MacOS.
+    """
+    try:
+        # Python 2.3+
+        import platform
+        if sys.platform == 'darwin':
+            # Darwin's platform.architecture() is buggy and always
+            # returns "64bit" event for the 32bit version of Python's
+            # universal binary. So we roll out our own (that works
+            # on Darwin).
+            if sys.maxint > 2L ** 32:
+                return '64bit'
+            else:
+                return '32bit'
+        else:
+            return platform.architecture()[0]
+    except ImportError:
+        return '32bit'
+
+
+def system():
+    try:
+        # Python 2.3+
+        import platform
+        return platform.system()
+    except ImportError:
+        n = {'nt': 'Windows', 'linux2': 'Linux', 'darwin': 'Darwin'}
+        return n[os.name]
