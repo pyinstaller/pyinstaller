b54b8b8c08d85b9b8ad9daa91bf7a1e0f4b57551
Martin Zibricky <mzibr.public@gmail.com>
2011-07-06 13:56:28 +0000

    Ticket #12: Enchant hook didn't work well with some Python version. The Work-around to not include wx and gtk libs does not work and is disabled.

--- PyInstaller/hooks/hook-enchant.checker.wxSpellCheckerDialog.py
+++ PyInstaller/hooks/hook-enchant.checker.wxSpellCheckerDialog.py
@@ -2,6 +2,10 @@
 # enchant.checker.wxSpellCheckerDialog causes pyinstaller to include
 # whole gtk and wx libraries if they are installed. This module is
 # thus ignored to prevent this.
+
 # TODO find better workaround
 def hook(mod):
-    return None
+    # Workaround DOES NOT work with well with python 2.6
+    # let's just disable it
+    #return None
+    return mod
