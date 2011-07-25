994713cf3ed477db0563ca78b8e123a64ed50d95
Hartmut Goebel <h.goebel@goebel-consult.de>
2011-07-25 21:42:38 +0000

    Updated macholib to 1.4.2 which includes a bug fix.

--- PyInstaller/lib/macholib/__init__.py
+++ PyInstaller/lib/macholib/__init__.py
@@ -5,5 +5,7 @@ See the relevant header files in /usr/include/mach-o
 
 And also Apple's documentation.
 """
-__version__ = '1.4.1'
+# For PyInstaller/lib/ define the version here, since there is no
+# package-resource.
 
+__version__ = '1.4.2'
