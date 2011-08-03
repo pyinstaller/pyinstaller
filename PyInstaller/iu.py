b2c47c3a11ae41fcef8a27427a173ecbe8bda28a
Hartmut Goebel <h.goebel@goebel-consult.de>
2011-08-03 17:50:10 +0000

    Minimalistic cleanup of comments.

--- PyInstaller/iu.py
+++ PyInstaller/iu.py
@@ -25,10 +25,10 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 #
-#
-# **NOTE** This module is used during bootstrap. Import *ONLY* builtin modules.
-#
 
+
+### **NOTE** This module is used during bootstrap. 
+### Import *ONLY* builtin modules. 
 import sys
 import imp
 import marshal
