8fdec65d222c3dab5c5e97ade70710c6b74568b2
Hartmut Goebel <h.goebel@goebel-consult.de>
2011-10-09 15:56:22 +0000

    Removed some unused imports.

--- PyInstaller/versionInfo.py
+++ PyInstaller/versionInfo.py
@@ -14,10 +14,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+
 import win32api
 import struct
 import pywintypes
-import pprint
 
 TEST=0
 
