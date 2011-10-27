4f714ef9355d20cc3a5da2f20d1c1b99fed5b25f
Hartmut Goebel <h.goebel@goebel-consult.de>
2011-10-27 18:28:34 +0000

    PEP 8 code cleanup.

--- PyInstaller/utils/icon.py
+++ PyInstaller/utils/icon.py
@@ -34,19 +34,21 @@ import PyInstaller.log as logging
 logger = logging.getLogger('PyInstaller.icon')
 
 class Structure:
-    def __init__ (self):
-        size = self._sizeInBytes = struct.calcsize (self._format_)
-        self._fields_ = list (struct.unpack (self._format_, '\000' * size))
+    def __init__(self):
+        size = self._sizeInBytes = struct.calcsize(self._format_)
+        self._fields_ = list(struct.unpack(self._format_, '\000' * size))
         indexes = self._indexes_ = {}
-        for i in range (len (self._names_)):
+        for i in range(len(self._names_)):
             indexes[self._names_[i]] = i
-    def dump (self):
+
+    def dump(self):
         logger.info("DUMP of %s", self)
         for name in self._names_:
             if name[0] != '_':
                 logger.info("%20s = %s", name, getattr(self, name))
         logger.info("")
-    def __getattr__ (self, name):
+
+    def __getattr__(self, name):
         if name in self._names_:
             index = self._indexes_[name]
             return self._fields_[index]
@@ -54,87 +56,92 @@ class Structure:
             return self.__dict__[name]
         except KeyError:
             raise AttributeError, name
-    def __setattr__ (self, name, value):
+
+    def __setattr__(self, name, value):
         if name in self._names_:
             index = self._indexes_[name]
             self._fields_[index] = value
         else:
             self.__dict__[name] = value
-    def tostring (self):
-        return apply (struct.pack, [self._format_,] + self._fields_)
-    def fromfile (self, file):
-        data = file.read (self._sizeInBytes)
-        self._fields_ = list (struct.unpack (self._format_, data))
 
-class ICONDIRHEADER (Structure):
+    def tostring(self):
+        return apply(struct.pack, [self._format_,] + self._fields_)
+
+    def fromfile(self, file):
+        data = file.read(self._sizeInBytes)
+        self._fields_ = list(struct.unpack(self._format_, data))
+
+class ICONDIRHEADER(Structure):
     _names_ = "idReserved", "idType", "idCount"
     _format_ = "hhh"
 
-class ICONDIRENTRY (Structure):
-    _names_ = "bWidth", "bHeight", "bColorCount", "bReserved", "wPlanes", "wBitCount", "dwBytesInRes", "dwImageOffset"
+class ICONDIRENTRY(Structure):
+    _names_ = ("bWidth", "bHeight", "bColorCount", "bReserved", "wPlanes",
+               "wBitCount", "dwBytesInRes", "dwImageOffset")
     _format_ = "bbbbhhii"
 
-class GRPICONDIR (Structure):
+class GRPICONDIR(Structure):
     _names_ = "idReserved", "idType", "idCount"
     _format_ = "hhh"
 
-class GRPICONDIRENTRY (Structure):
-    _names_ = "bWidth", "bHeight", "bColorCount", "bReserved", "wPlanes", "wBitCount", "dwBytesInRes", "nID"
+class GRPICONDIRENTRY(Structure):
+    _names_ = ("bWidth", "bHeight", "bColorCount", "bReserved", "wPlanes",
+               "wBitCount", "dwBytesInRes", "nID")
     _format_ = "bbbbhhih"
 
 class IconFile:
-    def __init__ (self, path):
+    def __init__(self, path):
         self.path = path
-        file = open (path, "rb")
+        file = open(path, "rb")
         self.entries = []
         self.images = []
         header = self.header = ICONDIRHEADER()
-        header.fromfile (file)
-        for i in range (header.idCount):
+        header.fromfile(file)
+        for i in range(header.idCount):
             entry = ICONDIRENTRY()
-            entry.fromfile (file)
-            self.entries.append (entry)
+            entry.fromfile(file)
+            self.entries.append(entry)
         for e in self.entries:
-            file.seek (e.dwImageOffset, 0)
-            self.images.append (file.read (e.dwBytesInRes))
+            file.seek(e.dwImageOffset, 0)
+            self.images.append(file.read(e.dwBytesInRes))
 
-    def grp_icon_dir (self):
+    def grp_icon_dir(self):
         return self.header.tostring()
 
-    def grp_icondir_entries (self, id=1):
+    def grp_icondir_entries(self, id=1):
         data = ""
         for entry in self.entries:
             e = GRPICONDIRENTRY()
             for n in e._names_[:-1]:
-                setattr(e, n, getattr (entry, n))
+                setattr(e, n, getattr(entry, n))
             e.nID = id
             id = id + 1
             data = data + e.tostring()
         return data
 
 
-def CopyIcons_FromIco (dstpath, srcpath, id=1):
+def CopyIcons_FromIco(dstpath, srcpath, id=1):
     import win32api #, win32con
     icons = map(IconFile, srcpath)
     logger.info("Updating icons from %s to %s", srcpath, dstpath)
 
-    hdst = win32api.BeginUpdateResource (dstpath, 0)
+    hdst = win32api.BeginUpdateResource(dstpath, 0)
 
     iconid = 1
     for i in range(len(icons)):
         f = icons[i]
         data = f.grp_icon_dir()
         data = data + f.grp_icondir_entries(iconid)
-        win32api.UpdateResource (hdst, RT_GROUP_ICON, i, data)
+        win32api.UpdateResource(hdst, RT_GROUP_ICON, i, data)
         logger.info("Writing RT_GROUP_ICON %d resource with %d bytes", i, len(data))
         for data in f.images:
-            win32api.UpdateResource (hdst, RT_ICON, iconid, data)
+            win32api.UpdateResource(hdst, RT_ICON, iconid, data)
             logger.info("Writing RT_ICON %d resource with %d bytes", iconid, len(data))
             iconid = iconid + 1
 
-    win32api.EndUpdateResource (hdst, 0)
+    win32api.EndUpdateResource(hdst, 0)
 
-def CopyIcons (dstpath, srcpath):
+def CopyIcons(dstpath, srcpath):
     import os.path
 
     if type(srcpath) in StringTypes:
@@ -165,27 +172,27 @@ def CopyIcons (dstpath, srcpath):
     srcpath,index = srcpath[0]
     srcext = os.path.splitext(srcpath)[1]
     if srcext.lower() == '.ico':
-        return CopyIcons_FromIco (dstpath, [srcpath])
+        return CopyIcons_FromIco(dstpath, [srcpath])
     if index is not None:
         logger.info("Updating icons from %s, %d to %s", srcpath, index, dstpath)
     else:
         logger.info("Updating icons from %s to %s", srcpath, dstpath)
     import win32api #, win32con
-    hdst = win32api.BeginUpdateResource (dstpath, 0)
-    hsrc = win32api.LoadLibraryEx (srcpath, 0, LOAD_LIBRARY_AS_DATAFILE)
+    hdst = win32api.BeginUpdateResource(dstpath, 0)
+    hsrc = win32api.LoadLibraryEx(srcpath, 0, LOAD_LIBRARY_AS_DATAFILE)
     if index is None:
-        grpname = win32api.EnumResourceNames (hsrc, RT_GROUP_ICON)[0]
+        grpname = win32api.EnumResourceNames(hsrc, RT_GROUP_ICON)[0]
     elif index >= 0:
-        grpname = win32api.EnumResourceNames (hsrc, RT_GROUP_ICON)[index]
+        grpname = win32api.EnumResourceNames(hsrc, RT_GROUP_ICON)[index]
     else:
         grpname = -index
-    data = win32api.LoadResource (hsrc, RT_GROUP_ICON, grpname)
-    win32api.UpdateResource (hdst, RT_GROUP_ICON, grpname, data)
-    for iconname in win32api.EnumResourceNames (hsrc, RT_ICON):
-        data = win32api.LoadResource (hsrc, RT_ICON, iconname)
-        win32api.UpdateResource (hdst, RT_ICON, iconname, data)
-    win32api.FreeLibrary (hsrc)
-    win32api.EndUpdateResource (hdst, 0)
+    data = win32api.LoadResource(hsrc, RT_GROUP_ICON, grpname)
+    win32api.UpdateResource(hdst, RT_GROUP_ICON, grpname, data)
+    for iconname in win32api.EnumResourceNames(hsrc, RT_ICON):
+        data = win32api.LoadResource(hsrc, RT_ICON, iconname)
+        win32api.UpdateResource(hdst, RT_ICON, iconname, data)
+    win32api.FreeLibrary(hsrc)
+    win32api.EndUpdateResource(hdst, 0)
 
 if __name__ == "__main__":
     import sys
