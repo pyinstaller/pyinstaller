45f71118101b3ea1a92c9c76e198cf6484c70039
Martin Zibricky <mzibr.public@gmail.com>
2011-11-16 17:31:11 +0000

    Fix pyqt hook to resolve dll dependencies for phonon plugin.

--- PyInstaller/hooks/hook-PyQt4.phonon.py
+++ PyInstaller/hooks/hook-PyQt4.phonon.py
@@ -1,13 +1,8 @@
-import os.path
-
 hiddenimports = ['sip', 'PyQt4.QtGui', 'PyQt4._qt']
 
-from PyInstaller.hooks.hookutils import qt4_phonon_plugins_dir
-pdir = qt4_phonon_plugins_dir()
+from PyInstaller.hooks.hookutils import qt4_plugins_binaries
 
-_dest_dir = os.path.join("qt4_plugins", "phonon_backend")
-datas = [
-     (os.path.join(pdir, "phonon_backend", "*.so"),    _dest_dir),
-     (os.path.join(pdir, "phonon_backend", "*.dll"),   _dest_dir),
-     (os.path.join(pdir, "phonon_backend", "*.dylib"), _dest_dir),
-]
+
+def hook(mod):
+    mod.binaries.extend(qt4_plugins_binaries('phonon_backend'))
+    return mod
