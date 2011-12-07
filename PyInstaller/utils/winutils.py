cbf70d0a4ca591747ec990c7db4ba45e8a8720bc
Martin Zibricky <mzibr.public@gmail.com>
2011-12-07 14:05:49 +0000

    Ticket #473: fix a windows regression

--- PyInstaller/utils/winutils.py
+++ PyInstaller/utils/winutils.py
@@ -14,7 +14,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
 
 """
 Utils for Windows platform.
@@ -26,6 +26,7 @@ __all__ = ['get_windows_dir']
 import os
 
 from PyInstaller import is_win
+from PyInstaller import compat
 
 import PyInstaller.log as logging
 logger = logging.getLogger('PyInstaller.build.bindepend')
