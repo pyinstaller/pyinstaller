7bbad0354fa460ee678a8d85b526007a669bc802
Hartmut Goebel <h.goebel@goebel-consult.de>
2011-12-09 16:15:26 +0000

    Backporting e2etests/common/maketests.py 2011-12-01T13:46:52Z!h.goebel@goebel-consult.de to branch 1.5. I had too many errors when trying to run these tests on my machine. So the easiest is to backport, even if this is not essential for 1.5 series.

--- e2etests/common/maketests.py
+++ e2etests/common/maketests.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# Copyright (C) 2011, Hartmut Goebel
 # Copyright (C) 2005, Giovanni Bajo
 # Based on previous work under copyright (c) 1999, 2002 McMillan Enterprises, Inc.
 #
@@ -14,37 +15,66 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
-import sys, string, os
-if sys.platform[:3] == 'win':
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+
+import os
+import sys
+import subprocess
+
+def exec_python_rc(*args, **kwargs):
+    cmdargs = [sys.executable]
+    if is_darwin:
+        mapping = {'32bit': '-i386', '64bit': '-x86_64'}
+        py_prefix = ['arch', mapping[architecture()]]
+        cmdargs = py_prefix + cmdargs
+    cmdargs.extend(args)
+    return subprocess.call(cmdargs, **kwargs)
+
+utils_dir = os.path.normpath(os.path.join(__file__, '..', '..', '..'))
+makespec = os.path.join(utils_dir, 'Makespec.py')
+build = os.path.join(utils_dir, 'Build.py')
+
+is_win = sys.platform.startswith('win')
+is_linux = sys.platform == 'linux2'
+is_darwin = sys.platform == 'darwin'
+
+if is_win:
     stripopts = ('',)
     consoleopts = ('', '--noconsole')
 else:
     stripopts = ('', '--strip')
     consoleopts = ('',)
-if string.find(sys.executable, ' ') > -1:
-    exe = '"%s"' % sys.executable
-else:
-    exe = sys.executable
-if sys.platform[:3] == 'win':
-    spec = "%s ../../Makespec.py --tk %%s %%s %%s %%s %%s --out t%%d hanoi.py" % exe
-    bld = "%s ../../Build.py t%%d/hanoi.spec" % exe
-else:
-    spec = "%s ../../Makespec.py --tk %%s %%s %%s %%s %%s --out /u/temp/t%%d hanoi.py" % exe
-    bld = "%s ../../Build.py /u/temp/t%%d/hanoi.spec" % exe
 
-i = 0
+out_pattern = 't%d'
+if is_linux:
+    import tempfile
+    out_pattern = os.path.join(tempfile.gettempdir(), 'hanoi', out_pattern)
+dist_pattern_dir = os.path.join(out_pattern, 'dist', 'hanoi', 'hanoi')
+dist_pattern_file = os.path.join(out_pattern, 'dist', 'hanoi')
+
+script_name = os.path.abspath(os.path.join(__file__, '..', 'hanoi.py'))
+
+def build_test(cnt, bldconfig, *options):
+    options = filter(None, options)
+    exec_python_rc(makespec, script_name, '--tk',
+                   '--out', out_pattern % cnt, bldconfig, *options)
+    exec_python_rc(build, os.path.join(out_pattern % cnt, 'hanoi.spec'),
+                   '--noconfirm')
+    if is_linux:
+        # create symlinks
+        if os.path.islink('hanoi%d' % cnt):
+            os.remove('hanoi%d' % cnt)
+        if bldconfig == '--onedir':
+            os.symlink(dist_pattern_dir % cnt, 'hanoi%d' % cnt)
+        else:
+            os.symlink(dist_pattern_file % cnt, 'hanoi%d' % cnt)
+
+i = 1
 for bldconfig in ('--onedir', '--onefile'):
     for console in consoleopts:
         for dbg in ('--debug', ''):
             for stripopt in stripopts:
                 for upxopt in ('', '--upx'):
-                    cmd = spec % (bldconfig, console, dbg, stripopt, upxopt, i)
-                    os.system(cmd)
-                    os.system(bld % i)
-                    if sys.platform[:5] == 'linux':
-                        if bldconfig == '--onedir':
-                            os.system("ln -s /u/temp/t%d/disthanoi/hanoi hanoi%d" % (i,i))
-                        else:
-                            os.system("ln -s /u/temp/t%d/hanoi hanoi%d" % (i,i))
+                    build_test(i, bldconfig, console, dbg, stripopt)
                     i += 1
+                    raise SystemExit()
