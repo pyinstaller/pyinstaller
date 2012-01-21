4cd4a62c39fe5cf67ad1165099333fafd8b85b6c
Hartmut Goebel <h.goebel@goebel-consult.de>
2012-01-21 22:08:10 +0100

    Fix hook-distutils for the case distutils.__init__.pyc is missing.
    The hook failed if it was not able to read the .pyc . Now the .py gets
    compiled on the fly. (Also see last commit.)

--- PyInstaller/hooks/hook-distutils.py
+++ PyInstaller/hooks/hook-distutils.py
@@ -7,6 +7,9 @@ def hook(mod):
         import os
         import marshal
         mod_path = os.path.join(distutils.distutils_path, "__init__.pyc")
-        parsed_code = marshal.loads(open(mod_path, "rb").read()[8:])
+        try:
+            parsed_code = marshal.loads(open(mod_path, "rb").read()[8:])
+        except IOError:
+            co = compile(open(mod_path[:-1], 'rU').read(), map_path, 'exec')
         mod.__init__('distutils', mod_path, parsed_code)
     return mod
