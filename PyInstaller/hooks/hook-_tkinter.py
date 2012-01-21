a4bf343390657bc381e84134d0b4f5f265972cbd
Hartmut Goebel <h.goebel@goebel-consult.de>
2012-01-21 12:32:00 +0100

    Replace hook-Tkinter by hook-_tkinter.
    This saves the additional instantiation of a new ImportTracker. This
    hook is only used for collecting the data files, so we only need to
    find libtcl8.6 (et al) and are not interested in the other
    dependencies.

--- PyInstaller/hooks/hook-_tkinter.py
+++ PyInstaller/hooks/hook-_tkinter.py
@@ -2,15 +2,14 @@ import sys
 import os
 import re
 
-from PyInstaller import is_win, is_darwin, is_unix, mf
+from PyInstaller import is_win, is_darwin, is_unix
 from PyInstaller import bindepend
 from PyInstaller.build import Tree
-from PyInstaller.depend import dylib
 from PyInstaller.hooks.hookutils import logger
 
 def find_tk_win():
     pattern = re.compile(r'(?i)tcl(\d)(\d)\.dll')
-    for nm, fnm, typ in binaries:
+    for nm, fnm in binaries:
         mo = pattern.match(nm)
         if not mo:
             continue
@@ -31,7 +30,7 @@ def find_tk_win():
 
 def find_tk_darwin(binaries):
     pattern = re.compile(r'_tkinter')
-    for nm, fnm, typ in binaries:
+    for nm, fnm in binaries:
         mo = pattern.match(nm)
         if not mo:
             continue
@@ -42,7 +41,7 @@ def find_tk_darwin(binaries):
 
 def find_tk_unix(binaries):
     pattern = re.compile(r'libtcl(\d\.\d)?\.so')
-    for nm, fnm, typ in binaries:
+    for nm, fnm in binaries:
         mo = pattern.match(nm)
         if not mo:
             continue
@@ -77,47 +76,24 @@ def collect_tkfiles(tclroot, tkroot):
                   excludes=['demos', 'encoding', '*.lib', 'tkConfig.sh'])
     return (tcltree + tktree)
 
-
-def _prep_hook():
-    # :fixme: find another way for accessing all dependencies
-    # :fixme:     w/o a new ImportTracker instance.
-    tracker = mf.ImportTracker()
-    tracker.analyze_r('Tkinter')
-    binaries = []
-    for modnm, mod in tracker.modules.items():
-        if isinstance(mod, mf.ExtensionModule):
-            binaries.append((mod.__name__, mod.__file__, 'EXTENSION'))
-
-    # Always add python's dependencies first. This ensures that
-    # assembly depencies under Windows get pulled in first and we do
-    # not need to add assembly DLLs to the exclude list explicitly.
-    binaries.extend(bindepend.Dependencies([('', sys.executable, '')]))
-    binaries.extend(bindepend.Dependencies(binaries))
-
+def hook(mod):
+    # get the shared libs used by _tkinter.so (resp. .dll, et al)
+    binaries = bindepend.selectImports(mod.__file__)
     if is_win:
         tcl_tk = find_tk_win(binaries)
     elif is_darwin:
         tcl_tk = find_tk_darwin(binaries)
-    elif is_unix:
+    elif not is_unix:
         tcl_tk = find_tk_unix(binaries)
     else:
         # If no pattern is in place for this platform, skip TCL/TK detection.
         tcl_tk = -1
 
     if tcl_tk == -1:
-        logger.info("... skipping TCL/TK detection on this target platform (%s)"
-                    % sys.platform)
+        logger.info("... skipping TCL/TK detection on this platform (%s)",
+                    sys.platform)
     elif tcl_tk is None:
         logger.error("could not find TCL/TK")
     else:
-        global tktree
-        tktree = collect_tkfiles(*tcl_tk)
-
-# _prep_hook must be run only once to avoid recursion as _prep_hook
-# calls ImportTracker().analyze_r('Tkinter') which imports this hook
-# again.
-_prep_hook()
-
-def hook(mod):
-    mod.datas.extend(tktree)
+        mod.datas.extend(collect_tkfiles(*tcl_tk))
     return mod
