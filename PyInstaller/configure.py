c0005ec8be476705d354e804ea680c697127bb41
Giovanni Bajo <rasky@develer.com>
2012-02-03 03:03:25 +0100

    Rework the 32-bit/64-bit warning on Darwin.
    It looks like there is not a single way to switch to the 32-bit version
    of Python: you must either set an environment variable or use the 'arch'
    command, depending on whether you are using the generic /usr/bin/python
    or a version-specific wrapper (eg. python2.6).
    
    So we rework the warning message to apply both suggestions at the same
    time, so that it will work by just copy & pasting without understanding
    the whole mess.

--- PyInstaller/configure.py
+++ PyInstaller/configure.py
@@ -162,12 +162,10 @@ def find_PYZ_dependencies(config):
 
 def get_config(upx_dir, **kw):
     if is_darwin and compat.architecture() == '64bit':
-        logger.warn('You are running 64-bit Python. Created binary will not'
-            ' work on Mac OS X 10.4 or 10.5. For this version it is necessary'
-            ' to create 32-bit binaries.'
-            ' If you need 32-bit version of Python, run Python as 32-bit binary'
-            ' by command:\n\n'
-            '    arch -i386 python\n')
+        logger.warn('You are running 64-bit Python: created binaries will only'
+            ' work on Mac OS X 10.6+.\nIf you need 10.4-10.5 compatibility,'
+            ' run Python as a 32-bit binary with this command:\n\n'
+            '    VERSIONER_PYTHON_PREFER_32_BIT=yes arch -i386 %s\n' % sys.executable)
         # wait several seconds for user to see this message
         time.sleep(4)
 
