432c7dc7f74f8893a51514a7ecbcb947fbd4f6bd
Martin Zibricky <mzibr.public@gmail.com>
2012-02-07 23:19:56 +0100

    Add: Function to find path to an executable in system PATH.

--- PyInstaller/utils/misc.py
+++ PyInstaller/utils/misc.py
@@ -22,6 +22,7 @@ This module is for the miscellaneous routines which do not fit somewhere else.
 
 import glob
 import os
+import sys
 
 
 def dlls_in_subdirs(directory):
@@ -38,3 +39,42 @@ def dlls_in_dir(directory):
     files.extend(glob.glob(os.path.join(directory, '*.dll')))
     files.extend(glob.glob(os.path.join(directory, '*.dylib')))
     return files
+
+
+def find_executable(executable, path=None):
+    """
+    Try to find 'executable' in the directories listed in 'path' (a
+    string listing directories separated by 'os.pathsep'; defaults to
+    os.environ['PATH']).
+
+    Returns the complete filename or None if not found.
+
+    Code from http://snippets.dzone.com/posts/show/6313
+    """
+    if path is None:
+        path = os.environ['PATH']
+    paths = path.split(os.pathsep)
+    extlist = ['']
+
+    if sys.platform == 'win32':
+        (base, ext) = os.path.splitext(executable)
+        # Executable files on windows have an arbitrary extension, but
+        # .exe is automatically appended if not present in the name.
+        if not ext:
+            executable = executable + ".exe"
+        pathext = os.environ['PATHEXT'].lower().split(os.pathsep)
+        (base, ext) = os.path.splitext(executable)
+        if ext.lower() not in pathext:
+            extlist = pathext
+
+    for ext in extlist:
+        execname = executable + ext
+        if os.path.isfile(execname):
+            return execname
+        else:
+            for p in paths:
+                f = os.path.join(p, execname)
+                if os.path.isfile(f):
+                    return f
+    else:
+        return None
