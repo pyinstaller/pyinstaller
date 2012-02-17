b9cd00b7adae06fb00323e812b582bc1587698fd
Hartmut Goebel <h.goebel@goebel-consult.de>
2012-02-17 15:24:42 +0100

    Use a real Tix-widget, not only Tk once to test if tix is really included.

--- buildtests/interactive/test_tix.py
+++ buildtests/interactive/test_tix.py
@@ -5,6 +5,7 @@ root = tix.Tk()
 root.title("Test for TiX")
 
 tix.Label(text="Press <ESC> to exit").pack()
+tix.DirList(root).pack()
 tix.Button(root, text="Close", command=root.destroy).pack()
 root.bind("<Escape>", lambda x: root.destroy())
 
