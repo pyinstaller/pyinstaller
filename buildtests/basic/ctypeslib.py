6eb43234f9bef4dd697287a81ba9be32065732b2
Hartmut Goebel <h.goebel@goebel-consult.de>
2012-02-17 10:00:09 +0100

    Minor code cleanup.

--- buildtests/basic/ctypeslib.py
+++ buildtests/basic/ctypeslib.py
@@ -3,10 +3,11 @@ import sys
 from ctypes import *
 
 def dummy(arg):
-    if sys.platform.startswith("linux") or sys.platform.startswith('sun') or \
-        sys.platform.startswith('aix'):
+    if (sys.platform.startswith("linux") or
+        sys.platform.startswith("sun") or
+        sys.platform.startswith("aix")):
         tct = CDLL("ctypes/testctypes.so")
-    elif sys.platform[:6] == "darwin":
+    elif sys.platform.startswith("darwin"):
         tct = CDLL("ctypes/testctypes.dylib")
     elif sys.platform == "win32":
         tct = CDLL("ctypes\\testctypes.dll")
