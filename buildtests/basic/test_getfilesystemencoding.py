1f003d65b75609f58ee957bc5ccd2b059d609f45
Martin Zibricky <mzibr.public@gmail.com>
2012-03-15 01:55:30 +0100

    Fix: Test case getfilesystemencoding for PYTHONHOME change.

--- buildtests/basic/test_getfilesystemencoding.py
+++ buildtests/basic/test_getfilesystemencoding.py
@@ -15,20 +15,41 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 
+# Requires Python 2.5+
+
 print "test_getfilesystemencoding"
 
+
+import copy
+import os
 import sys
-if sys.version_info[:2] >= (2, 5):
-    import subprocess
-    import email
+import subprocess
+import email
 
-    assert type(email.Header) == email.LazyImporter
 
-    pyexe = open("python_exe.build").readline().strip()
-    out = subprocess.Popen([pyexe, '-c', 'import sys; print sys.getfilesystemencoding()'],
-                           stdout=subprocess.PIPE).stdout.read().strip()
-    assert str(sys.getfilesystemencoding()) == out, (str(sys.getfilesystemencoding()), out)
+if not type(email.Header) == email.LazyImporter:
+    raise SystemExit()
 
-    print "test_getfilesystemencoding DONE"
+
+if hasattr(sys, 'frozen'):
+    # In frozen mode current working directory is the path with final executable.
+    pyexe_file = os.path.join('..', '..', 'python_exe.build')
 else:
-    print "Python < 2.5 test_getfilesystemencoding skipped"
+    pyexe_file = 'python_exe.build'
+
+
+pyexe = open(pyexe_file).readline().strip()
+
+
+# Unset PYTHONHOME for Python interpreter running in a subprocess.
+env = copy.deepcopy(os.environ)
+del env['PYTHONHOME']
+
+
+frozen_encoding = str(sys.getfilesystemencoding())
+encoding = subprocess.Popen([pyexe, '-c', 'import sys; print sys.getfilesystemencoding()'],
+        env=env, stdout=subprocess.PIPE).stdout.read().strip()
+
+
+if not frozen_encoding == encoding:
+    raise SystemExit()
