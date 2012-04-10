498dc1a094787f4fb5702cf1cd681ff5487f4fba
Martin Zibricky <mzibr.public@gmail.com>
2012-04-10 15:47:16 +0200

    Fix: Makefile test do not check existence of makefile on windows.

--- buildtests/basic/test_python_makefile_onefile.py
+++ buildtests/basic/test_python_makefile_onefile.py
@@ -2,15 +2,21 @@
 # installation.
 
 import os
+import sys
 from distutils import sysconfig
 
-makefile = sysconfig.get_config_h_filename()
-config_h = sysconfig.get_makefile_filename()
 
-print('pyconfig.h: ' + makefile)
-print('Makefile: ' + config_h)
+config_h = sysconfig.get_config_h_filename()
+print('pyconfig.h: ' + config_h)
+files = [config_h]
+
+# On Windows Makefile does not exist.
+if not sys.platform.startswith('win'):
+    makefile = sysconfig.get_makefile_filename()
+    print('Makefile: ' + config_h)
+    files.append(makefile)
 
 
-for f in (makefile, config_h):
+for f in files:
     if not os.path.exists(f):
         raise SystemExit('File does not exist: %s' % f)
