b26a495018416954ad0b45e219fa190430ffcd46
Martin Zibricky <mzibr.public@gmail.com>
2012-04-15 13:43:08 +0200

    Clean: pep8 cleanup for pyusb hook.

--- PyInstaller/hooks/hook-usb.py
+++ PyInstaller/hooks/hook-usb.py
@@ -1,3 +1,4 @@
+#
 # Copyright (C) 2012, Chien-An "Zero" Cho
 #
 # This program is free software; you can redistribute it and/or
@@ -14,13 +15,17 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 
-from PyInstaller.depend.utils import _resolveCtypesImports
+
 import ctypes.util
-import sys
 import os
+
+from PyInstaller.depend.utils import _resolveCtypesImports
+from PyInstaller.compat import is_cygwin
+
 
 # Include glob for library lookup in run-time hook.
 hiddenimports = ['glob']
+
 
 # This method will try to resolve your libusb libraries in the
 # following orders:
@@ -29,24 +34,28 @@ hiddenimports = ['glob']
 #
 # NOTE: Mind updating run-time hook when adding further libs.
 libusb_candidates = (
-  # libusb10
-  'usb-1.0', 'usb', 'libusb-1.0',
-  # libusb01
-  'usb-0.1', 'libusb0',
-  # openusb
-  'openusb'
+    # libusb10
+    'usb-1.0', 'usb', 'libusb-1.0',
+    # libusb01
+    'usb-0.1', 'libusb0',
+    # openusb
+    'openusb',
 )
-def hook(mod):
-  for candidate in libusb_candidates:
-    libname = ctypes.util.find_library(candidate)
-    if libname is not None: break
 
-  if libname is not None:
-    # Use basename here because Python returns full library path
-    # on Mac OSX when using ctypes.util.find_library.
-    bins = [os.path.basename(libname)]
-    mod.binaries.extend(_resolveCtypesImports(bins))
-  elif sys.platform == 'cygwin':
-    bins = ['cygusb-1.0-0.dll', 'cygusb0.dll']
-    mod.binaries.extend(_resolveCtypesImports(bins)[0:1])
-  return mod
+
+def hook(mod):
+    for candidate in libusb_candidates:
+        libname = ctypes.util.find_library(candidate)
+        if libname is not None:
+            break
+
+    if libname is not None:
+        # Use basename here because Python returns full library path
+        # on Mac OSX when using ctypes.util.find_library.
+        bins = [os.path.basename(libname)]
+        mod.binaries.extend(_resolveCtypesImports(bins))
+    elif is_cygwin:
+        bins = ['cygusb-1.0-0.dll', 'cygusb0.dll']
+        mod.binaries.extend(_resolveCtypesImports(bins)[0:1])
+
+    return mod
