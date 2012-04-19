1bcff5b3a05b71e01be54da81599599b1d2ae0ad
Matteo Baracani <bara@develer.com>
2012-04-19 13:01:50 +0200

    change the cookie structure. In CArchive is add the parameter usesystemlibrary, whose default value is False, use to know if the program must to use the Python system library or not; this information is saved in cookie by changing the pyvers value in negative if the program must to use the Python system library. To continue using the code everytime status->cookie.pyvers is used abs function was added

--- PyInstaller/loader/carchive.py
+++ PyInstaller/loader/carchive.py
@@ -118,12 +118,13 @@ class CArchive(archive.Archive):
     TRLSTRUCT = '!8siiii'
     TRLLEN = 24
     LEVEL = 9
-    def __init__(self, path=None, start=0, len=0):
+    def __init__(self, path=None, start=0, len=0, usesystemlibrary=False):
         """Constructor.
 
            PATH is path name of file (create an empty CArchive if path is None).
            START is the seekposition within PATH.
            LEN is the length of the CArchive (if 0, then read till EOF). """
+        self.usesystemlibrary = usesystemlibrary
         self.len = len
         archive.Archive.__init__(self, path, start)
 
@@ -258,6 +259,8 @@ class CArchive(archive.Archive):
            back to the start. """
         totallen = tocpos + self.toclen + self.TRLLEN
         pyvers = sys.version_info[0]*10 + sys.version_info[1]
+        if self.usesystemlibrary:
+            pyvers = -pyvers
         trl = struct.pack(self.TRLSTRUCT, self.MAGIC, totallen,
                           tocpos, self.toclen, pyvers)
         self.lib.write(trl)
