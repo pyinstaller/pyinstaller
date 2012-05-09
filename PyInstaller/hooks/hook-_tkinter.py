06b9e575a117454e206c9f5fbf1d480c221f93d5
Martin Zibricky <mzibr.public@gmail.com>
2012-05-09 14:12:39 +0200

    Issue #540: Fix including .tcl files on Mac.

--- PyInstaller/hooks/hook-_tkinter.py
+++ PyInstaller/hooks/hook-_tkinter.py
@@ -1,13 +1,34 @@
-import sys
+#
+# Copyright (C) 2012, Martin Zibricky
+# Copyright (C) 2011, Hartmut Goebel
+# Copyright (C) 2005, Giovanni Bajo
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+
 import os
 import re
+import sys
 
-from PyInstaller import is_win, is_darwin, is_unix
-from PyInstaller import bindepend
+import PyInstaller.bindepend
+
+from PyInstaller.compat import is_win, is_darwin, is_unix
 from PyInstaller.build import Tree
 from PyInstaller.hooks.hookutils import logger
 
-def find_tk_win(binaries):
+
+def _find_tk_win(binaries):
     pattern = re.compile(r'(?i)tcl(\d)(\d)\.dll')
     for nm, fnm in binaries:
         mo = pattern.match(nm)
@@ -28,18 +49,21 @@ def find_tk_win(binaries):
                 return TCL_root, TK_root
 
 
-def find_tk_darwin(binaries):
-    pattern = re.compile(r'_tkinter')
+def _find_tk_darwin_frameworks(binaries):
+    """
+    Tcl and Tk are installed as Mac OS X Frameworks.
+    """
+    tcl_root = tk_root = None
     for nm, fnm in binaries:
-        mo = pattern.match(nm)
-        if not mo:
-            continue
-        TCL_root = "/System/Library/Frameworks/Tcl.framework/Versions/Current"
-        TK_root = "/System/Library/Frameworks/Tk.framework/Versions/Current"
-        return TCL_root, TK_root
+        if nm == 'Tcl':
+            tcl_root = os.path.join(os.path.dirname(fnm), 'Resources/Scripts')
+        if nm == 'Tk':
+            tk_root = os.path.join(os.path.dirname(fnm), 'Resources/Scripts')
+    return tcl_root, tk_root
 
 
-def find_tk_unix(binaries):
+def _find_tk_unix(binaries):
+    tcl_root = tk_root = None
     pattern = re.compile(r'libtcl(\d\.\d)?\.so')
     for nm, fnm in binaries:
         mo = pattern.match(nm)
@@ -62,38 +86,85 @@ def find_tk_unix(binaries):
         return TCL_root, TK_root
 
 
-def collect_tkfiles(tclroot, tkroot):
-    if is_darwin:
-        tcldir = "Tcl.framework"
-        tkdir = "Tk.framework"
-    else:
-        tcldir = "tcl"
-        tkdir = "tk"
+def _find_tk(mod):
+    """
+    Find paths with Tcl and Tk data files to be bundled by PyInstaller.
 
-    tcltree = Tree(tclroot, os.path.join('_MEI', tcldir),
+    Return:
+        tcl_root  path to Tcl data files.
+        tk_root   path to Tk data files.
+    """
+    bins = mod.binaries
+    print 10 * '#'
+    print 'bins', bins
+    print 10 * '#'
+
+    if is_win:
+        tcl_tk = _find_tk_win(bins)
+
+    elif is_darwin:
+        # _tkinter depends on system Tcl/Tk frameworks.
+        if not bins:
+            # 'mod.binaries' can't be used because on Mac OS X _tkinter.so
+            # might depend on system Tcl/Tk frameworks and these are not
+            # included in 'mod.binaries'.
+            bins = PyInstaller.bindepend.getImports(mod.__file__)
+            # Reformat data structure from
+            #     set(['lib1', 'lib2', 'lib3'])
+            # to
+            #     [('Tcl', '/path/to/Tcl'), ('Tk', '/path/to/Tk')]
+            mapping = {}
+            for l in bins:
+                mapping[os.path.basename(l)] = l
+            bins = [
+                ('Tcl', mapping['Tcl']),
+                ('Tk', mapping['Tk']),
+            ]
+
+        print 10 * '#'
+        print 'bins', bins
+        print 10 * '#'
+        # _tkinter depends on Tcl/Tk compiled as frameworks.
+        path_to_tcl = bins[0][1]
+        if 'Library/Frameworks' in path_to_tcl:
+            tcl_tk = _find_tk_darwin_frameworks(bins)
+        # Tcl/Tk compiled as on Linux other Unices.
+        # For example this is the case of Tcl/Tk from macports.
+        else:
+            tcl_tk = _find_tk_unix(bins)
+
+    else:
+        tcl_tk = _find_tk_unix(bins)
+
+    return tcl_tk
+
+
+def _collect_tkfiles(mod):
+    tcl_root, tk_root = _find_tk(mod)
+
+    tcldir = "tcl"
+    tkdir = "tk"
+
+    tcltree = Tree(tcl_root, os.path.join('_MEI', tcldir),
                    excludes=['demos', 'encoding', '*.lib', 'tclConfig.sh'])
-    tktree = Tree(tkroot, os.path.join('_MEI', tkdir),
+    tktree = Tree(tk_root, os.path.join('_MEI', tkdir),
                   excludes=['demos', 'encoding', '*.lib', 'tkConfig.sh'])
     return (tcltree + tktree)
 
-def hook(mod):
-    # get the shared libs used by _tkinter.so (resp. .dll, et al)
-    binaries = bindepend.selectImports(mod.__file__)
-    if is_win:
-        tcl_tk = find_tk_win(binaries)
-    elif is_darwin:
-        tcl_tk = find_tk_darwin(binaries)
-    elif is_unix:
-        tcl_tk = find_tk_unix(binaries)
-    else:
-        # If no pattern is in place for this platform, skip TCL/TK detection.
-        tcl_tk = -1
 
-    if tcl_tk == -1:
+def hook(mod):
+    # If not supported platform, skip TCL/TK detection.
+    if not (is_win or is_darwin or is_unix):
         logger.info("... skipping TCL/TK detection on this platform (%s)",
-                    sys.platform)
-    elif tcl_tk is None:
-        logger.error("could not find TCL/TK")
-    else:
-        mod.datas.extend(collect_tkfiles(*tcl_tk))
+                sys.platform)
+        return mod
+
+    # Get the Tcl/Tk data files for bundling with executable.
+    #try:
+    tk_files = _collect_tkfiles(mod)
+    mod.datas.extend(tk_files)
+    #except Exception as e:
+    #    print str(e)
+    logger.error("could not find TCL/TK")
+
     return mod
