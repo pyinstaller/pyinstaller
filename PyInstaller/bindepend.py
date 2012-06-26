132b9b7dce9958f6651803300e5f7070ff0c9e63
Martin Zibricky <mzibr.public@gmail.com>
2012-06-26 14:45:34 +0200

    Issue #572: Fix handling @rpath in .dylib dependencies on osx.

--- PyInstaller/bindepend.py
+++ PyInstaller/bindepend.py
@@ -102,7 +102,7 @@ def _getImports_pe(pth):
     # https://code.google.com/p/pefile/wiki/UsageExamples
     pe = pefile.PE(pth, fast_load=True)
     pe.parse_data_directories(directories=[
-        pefile.DIRECTORY_ENTRY['IMAGE_DIRECTORY_ENTRY_IMPORT']]) 
+        pefile.DIRECTORY_ENTRY['IMAGE_DIRECTORY_ENTRY_IMPORT']])
     # Some libraries have no other binary dependencies. Use empty list
     # in that case. Otherwise pefile would return None.
     # e.g. C:\windows\system32\kernel32.dll on Wine
@@ -443,10 +443,13 @@ def _getImports_macholib(pth):
     This implementation is for Mac OS X and uses library macholib.
     """
     from PyInstaller.lib.macholib.MachO import MachO
+    from PyInstaller.lib.macholib.mach_o import LC_RPATH
     from PyInstaller.lib.macholib.dyld import dyld_find
     rslt = set()
     seen = set()  # Libraries read from binary headers.
-    # Walk through mach binary headers.
+
+    ## Walk through mach binary headers.
+
     m = MachO(pth)
     for header in m.headers:
         for idx, name, lib in header.walkRelocatables():
@@ -454,24 +457,66 @@ def _getImports_macholib(pth):
             if lib not in seen:
                 seen.add(lib)
 
+    # Walk through mach binary headers and look for LC_RPATH.
+    # macholib can't handle @rpath. LC_RPATH has to be read
+    # from the MachO header.
+    # FIXME PyInstaller should probably handle multiple @rpath
+    #       items not only one.
+    run_paths = set()
+    for header in m.headers:
+        for command in header.commands:
+            # A command is a tupple like:
+            #   (<macholib.mach_o.load_command object at 0x>,
+            #    <macholib.mach_o.rpath_command object at 0x>,
+            #    '../lib\x00\x00')
+            cmd_type = command[0].cmd
+            if cmd_type == LC_RPATH:
+                rpath = command[2]
+                # Remove trailing '\x00' characters.
+                # e.g. '../lib\x00\x00'
+                rpath = rpath.rstrip('\x00')
+                # Make rpath absolute. According to Apple doc LC_RPATH
+                # is always relative to the binary location.
+                rpath = os.path.normpath(os.path.join(os.path.dirname(pth), rpath))
+                run_paths.update([rpath])
+
     ## Try to find files in file system.
 
     # In cases with @loader_path or @executable_path
     # try to look in the same directory as the checked binary is.
     # This seems to work in most cases.
-    exec_path = os.path.abspath(os.path.dirname(pth))
+    exec_paths = [os.path.abspath(os.path.dirname(pth))]
 
     for lib in seen:
-        if lib.startswith('@loader_path'):
+
+        # Suppose that @rpath is not used for system libraries and
+        # using macholib can be avoided.
+        # macholib can't handle @rpath.
+        if lib.startswith('@rpath'):
+            lib = lib.replace('@rpath', '.')  # Make path relative.
+            final_lib = None  # Absolute path to existing lib on disk.
+            # Try multiple locations.
+            for run_path in run_paths:
+                if os.path.exists(os.path.join(run_path, lib)):
+                    final_lib = os.path.join(run_path, lib)
+                    rslt.add(final_lib)
+                    break
+            # Log error if no existing file found.
+            if not final_lib:
+                logger.error('Can not find path %s (needed by %s)', lib, pth)
+
+        # Macholib has to be used to get absolute path to libraries.
+        else:
             # macholib can't handle @loader_path. It has to be
             # handled the same way as @executable_path.
             # It is also replaced by 'exec_path'.
-            lib = lib.replace('@loader_path', '@executable_path')
-        try:
-            lib = dyld_find(lib, executable_path=exec_path)
-            rslt.add(lib)
-        except ValueError:
-            logger.error('Can not find path %s (needed by %s)', lib, pth)
+            if lib.startswith('@loader_path'):
+                lib = lib.replace('@loader_path', '@executable_path')
+            try:
+                lib = dyld_find(lib, executable_path=exec_paths)
+                rslt.add(lib)
+            except ValueError:
+                logger.error('Can not find path %s (needed by %s)', lib, pth)
 
     return rslt
 
