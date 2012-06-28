438e465a194ab995cabcfe5984ae8c5aaa4b9cbe
Martin Zibricky <mzibr.public@gmail.com>
2012-06-28 23:15:05 +0200

    Clean: Better code formatting of import/test_app_with_plugins test.

--- buildtests/import/test_app_with_plugins.py
+++ buildtests/import/test_app_with_plugins.py
@@ -1,43 +1,68 @@
+#
+# Copyright (C) 2012, Daniel Hyams
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
 
-#
-# this little sample application generates a plugin on the fly, and then tries to import it.
-#
-import os,sys
 
-# 
-# we first import a static plugin; the application might have certain plugins that it always loads.
-#
-mdl = __import__("static_plugin")
+# This little sample application generates a plugin on the fly,
+# and then tries to import it.
+
+
+import os
+import sys
+
+
+# We first import a static plugin; the application might have
+# certain plugins that it always loads.
+try:
+    print('Attempting to import static_plugin...')
+    mdl = __import__('static_plugin')
+except ImportError:
+    raise SystemExit('Failed to import the static plugin.')
 
 
 plugin_contents = """
-print("DYNAMIC PLUGIN IMPORTED.")
-print("This is some user-generated plugin that does not come into existence util after the application starts,")
-print("  and other modules in the directory it will reside in happen to have been imported (like the static_plugin)")
+print('DYNAMIC PLUGIN IMPORTED.')
+print('This is some user-generated plugin that does not come into existence')
+print('until the application starts, and other modules in the directory')
+print('it will reside in happen to have been imported (like the')
+print('static_plugin)')
 """
 
-#
-# create the dynamic plugin in the same directory as the executable.
-#
-program_dir = os.path.abspath(sys.path[0])
-plugin_filename = os.path.join(program_dir,"dynamic_plugin.py")
-fp = open(plugin_filename,"w")
+
+# Create the dynamic plugin in the same directory as the executable.
+program_dir = os.path.abspath(sys.prefix)
+plugin_filename = os.path.join(program_dir, 'dynamic_plugin.py')
+fp = open(plugin_filename, 'w')
 fp.write(plugin_contents)
 fp.close()
 
-try:
-   print("Attempting to import dynamic_plugin...") 
-   mdl = __import__("dynamic_plugin")
-   sys.exit(0)
-except Exception as e:
-   print("Failed to import the dynamic plugin.")
-   sys.exit(1)
-finally:
-   # 
-   # clean up.     
-   #
-   try:             os.remove(plugin_filename)
-   except OSError: pass
-   try:             os.remove(plugin_filename+"c")
-   except OSError: pass
 
+# Try import dynamic plugin.
+try:
+    print('Attempting to import dynamic_plugin...')
+    mdl = __import__('dynamic_plugin')
+except ImportError:
+    raise SystemExit('Failed to import the dynamic plugin.')
+finally:
+    # Clean up. Remove files dynamic_plugin.py[c]
+    try:
+        os.remove(plugin_filename)
+    except OSError:
+        pass
+    try:
+        os.remove(plugin_filename + 'c')
+    except OSError:
+        pass
