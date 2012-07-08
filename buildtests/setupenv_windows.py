9c505a6f7191ab596c9eabc39825c15a6312fa80
Martin Zibricky <mzibr.public@gmail.com>
2012-07-08 22:39:53 +0200

    Enh: Improve script to set up win environment for continuous integration.

--- buildtests/setupenv_windows.py
+++ buildtests/setupenv_windows.py
@@ -19,8 +19,12 @@
 # Install necessary 3rd party Python modules to run all tests.
 # This script is supposed to be used in a continuous integration system:
 # https://jenkins.shiningpanda.com/pyinstaller/
+# Python there is mostly 64bit. Only Python 2.4 is 32bit on Windows 7.
 
 
+import glob
+import optparse
+import os
 import platform
 import sys
 
@@ -28,30 +32,66 @@ import sys
 from setuptools.command import easy_install
 
 
-PYVER = (sys.version_info[0], sys.version_info[1])
+try:
+    import PyInstaller
+except ImportError:
+    # if importing PyInstaller fails, try to load from parent
+    # directory to support running without installation
+    import imp
+    if not hasattr(os, "getuid") or os.getuid() != 0:
+        imp.load_module('PyInstaller', *imp.find_module('PyInstaller',
+            [os.path.dirname(os.path.dirname(os.path.abspath(__file__)))]))
 
 
-def pywin32_architecture():
+from PyInstaller.compat import is_py24, is_py25, is_py26
+
+
+PYVER = '.'.join([str(x) for x in sys.version_info[0:2]])
+
+
+def py_arch():
+    """
+    .exe installers of Python modules contain architecture name in filename.
+    """
     mapping = {'32bit': 'win32', '64bit': 'win-amd64'}
     arch = platform.architecture()[0]
     return mapping[arch]
 
 
 _PACKAGES = {
-    # 'modulename': 'pypi_name_or_url'
-    'win32api': 'http://downloads.sourceforge.net/project/pywin32/pywin32/Build%%20217/pywin32-217.%s-py%d.%d.exe' %
-        (pywin32_architecture(), sys.version_info[0], sys.version_info[1]),
-    'simplejson': 'simplejson',
-    'sqlalchemy': 'sqlalchemy',
+    # 'modulename': 'pypi_name_or_url_or_path'
+    'MySQLdb': ['MySQL-python-*%s-py%s.exe' % (py_arch(), PYVER)],
+    'numpy': ['numpy-unoptimized-*%s-py%s.exe' % (py_arch(), PYVER)],
+    'PIL': ['PIL-*%s-py%s.exe' % (py_arch(), PYVER)],
+    'psycopg2': ['psycopg2-*%s-py%s.exe' % (py_arch(), PYVER)],
+    'pycrypto': ['pycrypto'],
+    'simplejson': ['simplejson'],
+    'sqlalchemy': ['SQLAlchemy-*%s-py%s.exe' % (py_arch(), PYVER)],
+    'wx': ['wxPython-common-*%s-py%s.exe' % (py_arch(), PYVER),
+        'wxPython-2*%s-py%s.exe' % (py_arch(), PYVER)],
+    'win32api': ['http://downloads.sourceforge.net/project/pywin32/pywin32/Build%%20217/pywin32-217.%s-py%s.exe' %
+        (py_arch(), PYVER)],
 }
 
 _PY_VERSION = {
-    'simplejson': (2, 5),
-    'sqlalchemy': (2, 4),
+    'MySQLdb': is_py26,
+    'numpy': is_py26,
+    'PIL': is_py26,
+    'psycopg2': is_py26,
+    'simplejson': is_py25,
+    'sqlalchemy': is_py24,
+    # Installers are available only for Python 2.6/2.7.
+    'wx': is_py26,
 }
 
 
 def main():
+    parser = optparse.OptionParser()
+    parser.add_option('-d', '--download-dir',
+        help='Directory with maually downloaded python modules.'
+    )
+    opts, _ = parser.parse_args()
+
     # Install packages.
     for k, v in _PACKAGES.items():
         # Test python version for module.
@@ -61,10 +101,31 @@ def main():
                 continue
         try:
             __import__(k)
-            print 'Skipping module... %s' % k
+            print 'Already installed... %s' % k
         # Module is not available - install it.
         except ImportError:
-            easy_install.main([v])
+            # If not url or module name then look for installers in download area.
+            if not v[0].startswith('http') and v[0].endswith('exe'):
+                files = []
+                # Try all file patterns.
+                for pattern in v:
+                    pattern = os.path.join(opts.download_dir, pattern)
+                    files += glob.glob(pattern)
+                # No file with that pattern was not found - skip it.
+                if not files:
+                    print 'Skipping module... %s' % k
+                    continue
+                # Full path to installers in download directory.
+                v = files
+            print 'Installing module... %s' % k
+            # Some modules might require several .exe files to install.
+            for f in v:
+                print '  %s' % f
+                # Use --no-deps ... installing module dependencies might fail
+                # because easy_install tries to install the same module from
+                # PYPI from source code and if fails because of C code that
+                # that needs to be compiled.
+                easy_install.main(['--no-deps', '--always-unzip', f])
 
 
 if __name__ == '__main__':
