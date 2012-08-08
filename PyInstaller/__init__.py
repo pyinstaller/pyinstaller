e7ab3ea79edc06a9f6d57ea4eae1b1810fa85184
Hartmut Goebel <h.goebel@goebel-consult.de>
2012-08-08 20:27:20 +0200

    Fix: get_version() failed when version is not a developer one.
    E.g.: VERSION = (2,0,0) failed since get_version did expect at least four
    tuple elements.

--- PyInstaller/__init__.py
+++ PyInstaller/__init__.py
@@ -84,9 +84,9 @@ def get_version():
     version = '%s.%s' % (VERSION[0], VERSION[1])
     if VERSION[2]:
         version = '%s.%s' % (version, VERSION[2])
-    if VERSION[3]:
+    if len(VERSION) >= 4 and VERSION[3]:
         version = '%s%s' % (version, VERSION[3])
-    # include git revision in version string
-    if VERSION[3] == 'dev' and VERSION[4] > 0:
-        version = '%s-%s' % (version, VERSION[4])
+        # include git revision in version string
+        if VERSION[3] == 'dev' and VERSION[4] > 0:
+            version = '%s-%s' % (version, VERSION[4])
     return version
