015696a2f639161129082133976c8cfb3dca1c29
Bryan A. Jones <bjones AT ece.msstate.edu>
2012-08-14 11:59:05 -0500

    Word-wrapped long lines

--- buildtests/libraries/test_sphinx.py
+++ buildtests/libraries/test_sphinx.py
@@ -19,14 +19,21 @@
 # Verify packaging of Sphinx, which relies on jinja2 and on docutils. Sphinx and docutils rely on data files in their module directories, which their respective hook scripts must find and copy.
 
 import sphinx, sys
-# See http://sphinx.pocoo.org/invocation.html#invocation for more details of the options below.
-# Also, note that this is run in the dist/test_sphihnx direcotry, but uses conf.py and index.rst from this directory, so the command-line options uses '../..' to refer to these files.
+# See http://sphinx.pocoo.org/invocation.html#invocation for more details of 
+# the options below.
+#
+# Also, note that this is run in the dist/test_sphihnx direcotry, but uses
+# conf.py and index.rst from the sphinx/ subdirectory, so the command-line
+# options uses '../../sphinx' to refer to these files.
 ret = sphinx.main(['', # First param is name of program (anything is fine)
                    '-a', '-E',  # Rebuild all files
                    '-b', 'html', # -b html produces html output
-                   '-d', '_build/doctrees', # Specify an output directory for data files
-                   '-c', '../../sphinx', # Specify the directory where conf.py lives
+                   '-d', '_build/doctrees', # Specify an output directory
+                                            # for data files
+                   '-c', '../../sphinx', # Specify the directory where
+                                         #  conf.py lives
                    '../../sphinx', # Location of the source (index.rst)
-                   '_build/html' # Output directory for the resulting HTML files
+                   '_build/html' # Output directory for the resulting HTML
+                                 # files
                      ])
 sys.exit(ret)
