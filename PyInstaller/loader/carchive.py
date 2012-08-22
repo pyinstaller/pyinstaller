b1af45d8d860c0c54d3f45a5442a96eff86e3ff4
Martin Zibricky <mzibr.public@gmail.com>
2012-08-22 12:27:26 +0200

    Clean: PEP8 syntax conformance in carchive.py.

--- PyInstaller/loader/carchive.py
+++ PyInstaller/loader/carchive.py
@@ -31,26 +31,33 @@ except ImportError:
 
 
 class CTOC(object):
-    """A class encapsulating the table of contents of a CArchive.
+    """
+    A class encapsulating the table of contents of a CArchive.
 
-       When written to disk, it is easily read from C."""
-    ENTRYSTRUCT = '!iiiibc' #(structlen, dpos, dlen, ulen, flag, typcd) followed by name
+    When written to disk, it is easily read from C.
+    """
+    ENTRYSTRUCT = '!iiiibc'  # (structlen, dpos, dlen, ulen, flag, typcd) followed by name
+
     def __init__(self):
         self.data = []
 
     def frombinary(self, s):
-        """Decode the binary string into an in memory list.
+        """
+        Decode the binary string into an in memory list.
 
-            S is a binary string."""
+        S is a binary string.
+        """
         entrylen = struct.calcsize(self.ENTRYSTRUCT)
         p = 0
-        while p<len(s):
+
+        while p < len(s):
             (slen, dpos, dlen, ulen, flag, typcd) = struct.unpack(self.ENTRYSTRUCT,
-                                                        s[p:p+entrylen])
+                                                        s[p:p + entrylen])
             nmlen = slen - entrylen
             p = p + entrylen
-            (nm,) = struct.unpack(`nmlen`+'s', s[p:p+nmlen])
+            (nm,) = struct.unpack(repr(nmlen) + 's', s[p:p + nmlen])
             p = p + nmlen
+            # FIXME Why are here two versions? Could be one eliminated?
             # version 4
             # self.data.append((dpos, dlen, ulen, flag, typcd, nm[:-1]))
             # version 5
@@ -62,13 +69,15 @@ class CTOC(object):
                 self.data.append((dpos, dlen, ulen, flag, typcd, nm[:pos]))
             #end version 5
 
-
     def tobinary(self):
-        """Return self as a binary string."""
+        """
+        Return self as a binary string.
+        """
         entrylen = struct.calcsize(self.ENTRYSTRUCT)
         rslt = []
         for (dpos, dlen, ulen, flag, typcd, nm) in self.data:
             nmlen = len(nm) + 1       # add 1 for a '\0'
+            # FIXME Why are here two versions? Is it safe to remove version 4?
             # version 4
             # rslt.append(struct.pack(self.ENTRYSTRUCT+`nmlen`+'s',
             #   nmlen+entrylen, dpos, dlen, ulen, flag, typcd, nm+'\0'))
@@ -79,99 +88,119 @@ class CTOC(object):
                 pad = '\0'
             else:
                 padlen = 16 - (toclen % 16)
-                pad = '\0'*padlen
+                pad = '\0' * padlen
                 nmlen = nmlen + padlen
-            rslt.append(struct.pack(self.ENTRYSTRUCT+`nmlen`+'s',
-                            nmlen+entrylen, dpos, dlen, ulen, flag, typcd, nm+pad))
+            rslt.append(struct.pack(self.ENTRYSTRUCT + repr(nmlen) + 's',
+                            nmlen + entrylen, dpos, dlen, ulen, flag, typcd, nm + pad))
             # end version 5
 
         return ''.join(rslt)
 
     def add(self, dpos, dlen, ulen, flag, typcd, nm):
-        """Add an entry to the table of contents.
+        """
+        Add an entry to the table of contents.
 
-           DPOS is data position.
-           DLEN is data length.
-           ULEN is the uncompressed data len.
-           FLAG says if the data is compressed.
-           TYPCD is the "type" of the entry (used by the C code)
-           NM is the entry's name."""
+        DPOS is data position.
+        DLEN is data length.
+        ULEN is the uncompressed data len.
+        FLAG says if the data is compressed.
+        TYPCD is the "type" of the entry (used by the C code)
+        NM is the entry's name.
+        """
         self.data.append((dpos, dlen, ulen, flag, typcd, nm))
 
     def get(self, ndx):
-        """return the toc entry (tuple) at index NDX"""
+        """
+        Return the table of contents entry (tuple) at index NDX.
+        """
         return self.data[ndx]
 
     def __getitem__(self, ndx):
         return self.data[ndx]
 
     def find(self, name):
-        """Return the index of the toc entry with name NAME.
+        """
+        Return the index of the toc entry with name NAME.
 
-           Return -1 for failure."""
+        Return -1 for failure.
+        """
         for i, nm in enumerate(self.data):
             if nm[-1] == name:
                 return i
         return -1
 
-class CArchive(archive.Archive):
-    """An Archive subclass that an hold arbitrary data.
 
-       Easily handled from C or from Python."""
+class CArchive(archive.Archive):
+    """
+    An Archive subclass that can hold arbitrary data.
+
+    Easily handled from C or from Python.
+    """
     MAGIC = 'MEI\014\013\012\013\016'
     HDRLEN = 0
     TOCTMPLT = CTOC
     TRLSTRUCT = '!8siiii'
     TRLLEN = 24
     LEVEL = 9
-    def __init__(self, path=None, start=0, len=0):
-        """Constructor.
 
-           PATH is path name of file (create an empty CArchive if path is None).
-           START is the seekposition within PATH.
-           LEN is the length of the CArchive (if 0, then read till EOF). """
+    def __init__(self, path=None, start=0, len=0):
+        """
+        Constructor.
+
+        PATH is path name of file (create an empty CArchive if path is None).
+        START is the seekposition within PATH.
+        LEN is the length of the CArchive (if 0, then read till EOF).
+        """
         self.len = len
         archive.Archive.__init__(self, path, start)
 
     def checkmagic(self):
-        """Verify that self is a valid CArchive.
+        """
+        Verify that self is a valid CArchive.
 
-            Magic signature is at end of the archive."""
-        #magic is at EOF; if we're embedded, we need to figure where that is
+        Magic signature is at end of the archive.
+        """
+        # Magic is at EOF; if we're embedded, we need to figure where that is.
         if self.len:
-            self.lib.seek(self.start+self.len, 0)
+            self.lib.seek(self.start + self.len, 0)
         else:
             self.lib.seek(0, 2)
         filelen = self.lib.tell()
         if self.len:
-            self.lib.seek(self.start+self.len-self.TRLLEN, 0)
+            self.lib.seek(self.start + self.len - self.TRLLEN, 0)
         else:
             self.lib.seek(-self.TRLLEN, 2)
         (magic, totallen, tocpos, toclen, pyvers) = struct.unpack(self.TRLSTRUCT,
                             self.lib.read(self.TRLLEN))
         if magic != self.MAGIC:
-            raise RuntimeError("%s is not a valid %s archive file"
-                               % (self.path, self.__class__.__name__))
+            raise RuntimeError("%s is not a valid %s archive file" %
+                    (self.path, self.__class__.__name__))
         self.pkgstart = filelen - totallen
         if self.len:
             if totallen != self.len or self.pkgstart != self.start:
-                raise RuntimeError, "Problem with embedded archive in %s" % self.path
+                raise RuntimeError('Problem with embedded archive in %s' %
+                        self.path)
         self.tocpos, self.toclen = tocpos, toclen
 
     def loadtoc(self):
-        """Load the table of contents into memory."""
+        """
+        Load the table of contents into memory.
+        """
         self.toc = self.TOCTMPLT()
-        self.lib.seek(self.pkgstart+self.tocpos)
+        self.lib.seek(self.pkgstart + self.tocpos)
         tocstr = self.lib.read(self.toclen)
         self.toc.frombinary(tocstr)
 
     def extract(self, name):
-        """Get the contents of an entry.
+        """
+        Get the contents of an entry.
 
-           NAME is an entry name.
-           Return the tuple (ispkg, contents).
-           For non-Python resoures, ispkg is meaningless (and 0).
-           Used by the import mechanism."""
+        NAME is an entry name.
+
+        Return the tuple (ispkg, contents).
+        For non-Python resoures, ispkg is meaningless (and 0).
+        Used by the import mechanism.
+        """
         if type(name) == type(''):
             ndx = self.toc.find(name)
             if ndx == -1:
@@ -179,14 +208,14 @@ class CArchive(archive.Archive):
         else:
             ndx = name
         (dpos, dlen, ulen, flag, typcd, nm) = self.toc.get(ndx)
-        self.lib.seek(self.pkgstart+dpos)
+        self.lib.seek(self.pkgstart + dpos)
         rslt = self.lib.read(dlen)
         if flag == 2:
             global AES
             import AES
             key = rslt[:32]
             # Note: keep this in sync with bootloader's code
-            rslt = AES.new(key, AES.MODE_CFB, "\0"*AES.block_size).decrypt(rslt[32:])
+            rslt = AES.new(key, AES.MODE_CFB, "\0" * AES.block_size).decrypt(rslt[32:])
         if flag == 1 or flag == 2:
             rslt = zlib.decompress(rslt)
         if typcd == 'M':
@@ -194,30 +223,35 @@ class CArchive(archive.Archive):
         return (0, rslt)
 
     def contents(self):
-        """Return the names of the entries"""
+        """
+        Return the names of the entries.
+        """
         rslt = []
         for (dpos, dlen, ulen, flag, typcd, nm) in self.toc:
             rslt.append(nm)
         return rslt
 
     def add(self, entry):
-        """Add an ENTRY to the CArchive.
+        """
+        Add an ENTRY to the CArchive.
 
-           ENTRY must have:
-             entry[0] is name (under which it will be saved).
-             entry[1] is fullpathname of the file.
-             entry[2] is a flag for it's storage format (0==uncompressed,
-             1==compressed)
-             entry[3] is the entry's type code.
-             Version 5:
-               If the type code is 'o':
-                 entry[0] is the runtime option
-                 eg: v  (meaning verbose imports)
-                     u  (menaing unbuffered)
-                     W arg (warning option arg)
-                     s  (meaning do site.py processing."""
+        ENTRY must have:
+          entry[0] is name (under which it will be saved).
+          entry[1] is fullpathname of the file.
+          entry[2] is a flag for it's storage format (0==uncompressed,
+          1==compressed)
+          entry[3] is the entry's type code.
+          Version 5:
+            If the type code is 'o':
+              entry[0] is the runtime option
+              eg: v  (meaning verbose imports)
+                  u  (menaing unbuffered)
+                  W arg (warning option arg)
+                  s  (meaning do site.py processing.
+        """
         (nm, pathnm, flag, typcd) = entry[:4]
-        # version 5 - allow type 'o' = runtime option
+        # FIXME Could we make the version 5 the default one?
+        # Version 5 - allow type 'o' = runtime option.
         try:
             if typcd in ('o', 'd'):
                 s = ''
@@ -238,10 +272,11 @@ class CArchive(archive.Archive):
             s = zlib.compress(s, self.LEVEL)
         if flag == 2:
             global AES
-            import AES, Crypt
+            import AES
+            import Crypt
             key = Crypt.gen_random_key(32)
             # Note: keep this in sync with bootloader's code
-            s = key + AES.new(key, AES.MODE_CFB, "\0"*AES.block_size).encrypt(s)
+            s = key + AES.new(key, AES.MODE_CFB, "\0" * AES.block_size).encrypt(s)
         dlen = len(s)
         where = self.lib.tell()
         if typcd == 'm':
@@ -251,29 +286,38 @@ class CArchive(archive.Archive):
         self.lib.write(s)
 
     def save_toc(self, tocpos):
-        """Save the table of contents to disk."""
+        """
+        Save the table of contents to disk.
+        """
         self.tocpos = tocpos
         tocstr = self.toc.tobinary()
         self.toclen = len(tocstr)
         self.lib.write(tocstr)
 
     def save_trailer(self, tocpos):
-        """Save the trailer to disk.
+        """
+        Save the trailer to disk.
 
-           CArchives can be opened from the end - the trailer points
-           back to the start. """
+        CArchives can be opened from the end - the trailer points
+        back to the start.
+        """
         totallen = tocpos + self.toclen + self.TRLLEN
-        pyvers = sys.version_info[0]*10 + sys.version_info[1]
+        pyvers = sys.version_info[0] * 10 + sys.version_info[1]
         trl = struct.pack(self.TRLSTRUCT, self.MAGIC, totallen,
                           tocpos, self.toclen, pyvers)
         self.lib.write(trl)
 
     def openEmbedded(self, name):
-        """Open a CArchive of name NAME embedded within this CArchive."""
+        """
+        Open a CArchive of name NAME embedded within this CArchive.
+        """
         ndx = self.toc.find(name)
+
         if ndx == -1:
-            raise KeyError, "Member '%s' not found in %s" % (name, self.path)
+            raise KeyError("Member '%s' not found in %s" % (name, self.path))
         (dpos, dlen, ulen, flag, typcd, nm) = self.toc.get(ndx)
+
         if flag:
-            raise ValueError, "Cannot open compressed archive %s in place"
-        return CArchive(self.path, self.pkgstart+dpos, dlen)
+            raise ValueError('Cannot open compressed archive %s in place' %
+                    name)
+        return CArchive(self.path, self.pkgstart + dpos, dlen)
