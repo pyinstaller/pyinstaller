67bbdfd16e3a604880a91e91a4f6f08b305f07f6
Martin Zibricky <mzibr.public@gmail.com>
2012-11-06 16:25:25 +0100

    Issue #356: Missing function declaration in launch.h.

--- bootloader/common/launch.h
+++ bootloader/common/launch.h
@@ -87,6 +87,12 @@ int init(ARCHIVE_STATUS *status, char const * archivePath, char  const * archive
  */
 int extractBinaries(ARCHIVE_STATUS *status_list[]);
 
+/*
+ * Check if binaries need to be extracted. If not, this is probably a onedir
+ * solution, and a child process will not be required on windows.
+ */
+int needToExtractBinaries(ARCHIVE_STATUS *status_list[]);
+
 /**
  * Load Python and execute all scripts in the archive
  *
