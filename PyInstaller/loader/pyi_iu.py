e3cade9c2ad3ebdd7a46914aa92484448a5ee854
Martin Zibricky <mzibr.public@gmail.com>
2012-12-22 01:08:30 +0100

    Issue #507: Cleanup of pyi_iu.py.

--- PyInstaller/loader/pyi_iu.py
+++ PyInstaller/loader/pyi_iu.py
@@ -1,4 +1,5 @@
 #
+# Copyright (C) 2012, Martin Zibricky
 # Copyright (C) 2005-2011, Giovanni Bajo
 #
 # Based on previous work under copyright (c) 2002 McMillan Enterprises, Inc.
@@ -27,175 +28,98 @@
 #
 
 
-# TODO Rename this module to  'pyi_os_path.py' Since this module will then only wrap functions from modules 'os' and 'os.path'. And all Owner classes will be removed.
-
-
 ### **NOTE** This module is used during bootstrap.
 ### Import *ONLY* builtin modules.
 ### List of built-in modules: sys.builtin_module_names
+
+
+# Set up 'os' and 'os.path' module replacement functions for use during import
+# bootstrap.
+
+
 import sys
-import imp
 
 
-# TODO Is this function still used anywhere?
-def getDescr(fnm):
-    ext = getpathext(fnm)
-    for (suffix, mode, typ) in imp.get_suffixes():
-        if suffix == ext:
-            return (suffix, mode, typ)
+_builtin_names = sys.builtin_module_names
+_mindirlen = 0
 
 
-# Some helper functions.
+# Wrap os.environ, os.listdir(), os.sep
+
+# We cannot cache the content of os.listdir(). It was found to cause problems
+# with programs that dynamically add python modules to be reimported by that
+# same program (i.e., plugins), because the cache is only built once
+# at the beginning, and never updated. So, we must really list the directory
+# again.
+
+if 'posix' in _builtin_names:  # For Linux, Unix, Mac OS X
+    from posix import environ as os_environ
+    from posix import listdir as os_listdir
+    os_sep = '/'
+    _mindirlen = 1
+elif 'nt' in _builtin_names:  # For Windows
+    from nt import environ as os_environ
+    from nt import listdir as os_listdir
+    os_sep = '\\'
+    _mindirlen = 3
+else:
+    raise ImportError('No os specific module found')
 
 
-def packagename(s):
-    """
-    For package name like 'module.submodule.subsubmodule' returns
-    'module.submodule'. If name does not contain any dots '.',
-    empty string '' is returned.
-    """
-    i = s.rfind('.')
-    if i >= 0:
-        return s[:i]
-    else:
-        return ''
+# Wrap os.path.join()
+def os_path_join(a, b, sep=os_sep):
+    if a == '':
+        return b
+    lastchar = a[-1:]
+    if lastchar == '/' or lastchar == sep:
+        return a + b
+    return a + sep + b
 
 
-def namesplit(s):
-    """
-    Split package name at the position of dot '.'.
-
-    Examples:
-        'module.submodule' =>  ['module', 'submodule']
-        'module'           =>  ['module']
-        ''                 =>  []
-    """
-    rslt = []
-    # Ensure that for empty string '' an empty list is returned.
-    if s:
-        rslt = s.split('.')
-    return rslt
+# Wrap os.path.dirname()
+def os_path_dirname(a, sep=os_sep, mindirlen=_mindirlen):
+    for i in range(len(a) - 1, -1, -1):
+        c = a[i]
+        if c == '/' or c == sep:
+            if i < mindirlen:
+                return a[:i + 1]
+            return a[:i]
+    return ''
 
 
-def getpathext(fnm):
-    i = fnm.rfind('.')
-    if i >= 0:
-        return fnm[i:]
-    else:
-        return ''
-
-
-def pathisdir(pathname):
-    "Local replacement for os.path.isdir()."
-    try:
-        s = _os_stat(pathname)
-    except OSError:
-        return None
-    return (s[0] & 0170000) == 0040000
-
-
-_os_stat = _os_path_join = _os_getcwd = _os_path_dirname = None
-_os_environ = _os_listdir = _os_path_basename = None
-_os_sep = None
-
-
-def _os_bootstrap():
-    """
-    Set up 'os' module replacement functions for use during import bootstrap.
-    """
-
-    global _os_stat, _os_getcwd, _os_environ, _os_listdir
-    global _os_path_join, _os_path_dirname, _os_path_basename
-    global _os_sep
-
-    names = sys.builtin_module_names
-
-    join = dirname = environ = listdir = basename = None
-    mindirlen = 0
-    # Only 'posix' and 'nt' os specific modules are supported.
-    # 'dos', 'os2' and 'mac' (MacOS 9) are not supported.
-    if 'posix' in names:
-        from posix import stat, getcwd, environ, listdir
-        sep = _os_sep = '/'
-        mindirlen = 1
-    elif 'nt' in names:
-        from nt import stat, getcwd, environ, listdir
-        sep = _os_sep = '\\'
-        mindirlen = 3
-    else:
-        raise ImportError('no os specific module found')
-
-    if join is None:
-        def join(a, b, sep=sep):
-            if a == '':
-                return b
-            lastchar = a[-1:]
-            if lastchar == '/' or lastchar == sep:
-                return a + b
-            return a + sep + b
-
-    if dirname is None:
-        def dirname(a, sep=sep, mindirlen=mindirlen):
-            for i in range(len(a) - 1, -1, -1):
-                c = a[i]
-                if c == '/' or c == sep:
-                    if i < mindirlen:
-                        return a[:i + 1]
-                    return a[:i]
-            return ''
-
-    if basename is None:
-        if sys.platform.startswith('win'):
-            # Implementation from ntpath.py module
-            # from standard Python 2.7 Library.
-            def basename(pth):
-                ## Implementation of os.path.splitdrive()
-                if pth[1:2] == ':':
-                    d = pth[0:2]
-                    p = pth[2:]
-                else:
-                    d = ''
-                    p = pth
-                ## Implementation of os.path.split()
-                # set i to index beyond p's last slash
-                i = len(p)
-                while i and p[i - 1] not in '/\\':
-                    i = i - 1
-                head, tail = p[:i], p[i:]  # now tail has no slashes
-                # Windows implementation is based on split(). We need
-                # to return only tail.
-                return tail
+# Wrap os.path.basename()
+if sys.platform.startswith('win'):
+    # Implementation from ntpath.py module
+    # from standard Python 2.7 Library.
+    def os_path_basename(pth):
+        ## Implementation of os.path.splitdrive()
+        if pth[1:2] == ':':
+            d = pth[0:2]
+            p = pth[2:]
         else:
-            # Implementation from ntpath.py module
-            # from standard Python 2.7 Library.
-            def basename(pth):
-                i = pth.rfind('/') + 1
-                return pth[i:]
-
-    def _listdir(dir, cache=None):
-        # The cache is not used. It was found to cause problems
-        # with programs that dynamically add python modules to be
-        # reimported by that same program (i.e., plugins), because
-        # the cache is only built once at the beginning, and never
-        # updated. So, we must really list the directory again.
-        return listdir(dir)
-
-    _os_stat = stat
-    _os_getcwd = getcwd
-    _os_path_join = join
-    _os_path_dirname = dirname
-    _os_environ = environ
-    _os_listdir = _listdir
-    _os_path_basename = basename
+            d = ''
+            p = pth
+        ## Implementation of os.path.split()
+        # set i to index beyond p's last slash
+        i = len(p)
+        while i and p[i - 1] not in '/\\':
+            i = i - 1
+        head, tail = p[:i], p[i:]  # now tail has no slashes
+        # Windows implementation is based on split(). We need
+        # to return only tail.
+        return tail
+else:
+    # Implementation from ntpath.py module
+    # from standard Python 2.7 Library.
+    def os_path_basename(pth):
+        i = pth.rfind('/') + 1
+        return pth[i:]
 
 
-_os_bootstrap()
-
-
-if 'PYTHONCASEOK' not in _os_environ:
+if 'PYTHONCASEOK' not in os_environ:
     def caseOk(filename):
-        files = _os_listdir(_os_path_dirname(filename))
-        return _os_path_basename(filename) in files
+        files = os_listdir(os_path_dirname(filename))
+        return os_path_basename(filename) in files
 else:
     def caseOk(filename):
         return True
