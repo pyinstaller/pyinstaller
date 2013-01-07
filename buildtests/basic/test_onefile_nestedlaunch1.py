170abe745e6a157c6b43e3ab2e4a14398c7c38be
Martin Zibricky <mzibr.public@gmail.com>
2013-01-07 12:30:16 +0100

    Clean: Fix test basic/test_onefile_nestedlaunch after rename.

--- buildtests/basic/test_onefile_nestedlaunch1.py
+++ buildtests/basic/test_onefile_nestedlaunch1.py
@@ -1,13 +1,20 @@
 import os
 import sys
 
-if __name__ == "__main__":
-    fn =  os.path.join(os.path.dirname(sys.executable),
-                       "..", "test_onefile_nestedlaunch0", "test_onefile_nestedlaunch0.exe")
+
+if __name__ == '__main__':
+
+    filename =  os.path.join(os.path.dirname(sys.executable),
+            'test_onefile_nestedlaunch0')
+
+    # On Windows append .exe suffix.
+    if sys.platform.startswith('win'):
+        filename += '.exe'
+
     try:
         import subprocess
     except ImportError:
-        if os.system(fn) != 0:
-            raise RuntimeError("os.system failed: %s" % fn)
+        if os.system(filename) != 0:
+            raise RuntimeError("os.system failed: %s" % filename)
     else:
-        subprocess.check_call([fn])
+        subprocess.check_call([filename])
