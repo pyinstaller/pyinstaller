7ff8f4be426bf8364c07a0bf64b87551110ccbf0
Martin Zibricky <mzibr.public@gmail.com>
2013-01-12 23:44:32 +0100

    Clean: Move hookutils scripts to separate directory.

--- PyInstaller/hooks/hook-enchant.py
+++ PyInstaller/hooks/hook-enchant.py
@@ -1,9 +1,10 @@
+import os
 import sys
 
 from hookutils import eval_script
 
 if sys.platform == 'win32':
-    files = eval_script('enchant-datafiles-finder.py')
+    files = eval_script(os.path.join('utils', 'enchant-datafiles-finder.py'))
     datas = []  # data files in PyInstaller hook format
     for d in files:
         for f in d[1]:
