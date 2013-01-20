1b6b767c875a2d244a2ef2cb90b4e46a45a0398f
Martin Zibricky <mzibr.public@gmail.com>
2013-01-20 23:27:39 +0100

    Issue #642: Include django data files with the frozen app.

--- PyInstaller/hooks/hook-django.py
+++ PyInstaller/hooks/hook-django.py
@@ -23,7 +23,8 @@
 
 import os
 from PyInstaller import log as logging
-from PyInstaller.hooks.hookutils import django_find_root_dir, django_dottedstring_imports
+from PyInstaller.hooks.hookutils import django_find_root_dir, django_dottedstring_imports, \
+        collect_data_files
 
 
 logger = logging.getLogger(__name__)
@@ -37,6 +38,8 @@ if root_dir:
     # Without them the django server won't run.
     package_name = os.path.basename(root_dir)
     hiddenimports += [
+            # TODO Consider including 'mysite.settings.py' in source code as a data files.
+            #      Since users might need to edit this file.
             package_name + '.settings',
             package_name + '.urls',
             package_name + '.wsgi',
@@ -47,10 +50,15 @@ if root_dir:
             'django.template.defaulttags',
             'django.template.loader_tags',
     ]
-    # Other hidden imports to get Django example project working.
+    # Other hidden imports to get Django example startproject working.
     hiddenimports += [
             'django.contrib.messages.storage.fallback',
     ]
+    # Include django data files - localizations, etc.
+    datas = collect_data_files('django')
+
+    # Include data files from your Django project found in your django root package.
+    datas += collect_data_files(package_name)
+
 else:
     logger.warn('No django root directory could be found!')
-
