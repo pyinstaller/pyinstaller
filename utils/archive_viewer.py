3ad406f08f3a7326cb5748592e5ee35cfd506acf
Martin Zibricky <mzibr.public@gmail.com>
2013-02-26 13:53:45 +0100

    Issue #304: Add wrapper scripts for pyinstaller cli utils.

---
+++ utils/archive_viewer.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+
+try:
+    import PyInstaller
+except ImportError:
+    # if importing PyInstaller fails, try to load from parent
+    # directory to support running without installation.
+    import imp
+    import os
+    # Prevent running as superuser (root).
+    if not hasattr(os, "getuid") or os.getuid() != 0:
+        imp.load_module('PyInstaller', *imp.find_module('PyInstaller',
+            [os.path.dirname(os.path.dirname(os.path.abspath(__file__)))]))
+
+
+if __name__ == '__main__':
+    from PyInstaller.cliutils.archive_viewer import run
+    run()
