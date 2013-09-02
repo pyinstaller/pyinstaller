eef81722b0434bea8c2f2881d6787d787dc24aed
Hartmut Goebel <h.goebel@crazy-compilers.com>
2013-09-02 20:25:30 +0200

    Update build-script for eggs and commit built mini-namespace-packages.

--- tests/eggs4testing/make.sh
+++ tests/eggs4testing/make.sh
@@ -10,10 +10,18 @@
 
 # This file is part of the package for testing eggs in `PyInstaller`.
 
+NSPKG_1="nspkg1-aaa nspkg1-bbb nspkg1-ccc nspkg1-empty"
+NSPKG_2="nspkg2-aaa nspkg2-bbb nspkg2-ccc nspkg2-empty"
+
+distdir="$PWD"/dist
 
 export PYTHONDONTWRITEBYTECODE=1
 
-rm -rf build/ dist/ *.egg-info
+rm -rf build/ dist/ *.egg* */*.egg* */build/ */dist/ venv
+rm -rf $NSPKG_1 $NSPKG_2
+
+./build-nspkg-tests.py
+
 
 python setup-zipped.py bdist_egg
 # nedd to clean up build-dir, otherwise stuff from `zipped_egg`
@@ -22,25 +30,60 @@ rm -rf build/
 python setup-unzipped.py bdist_egg
 rm -rf build/
 
+for pkg in $NSPKG_1 ; do
+    cd $pkg
+    python setup.py sdist --dist-dir "$distdir"
+    python setup.py bdist_egg --dist-dir "$distdir"
+    cd -
+done
+
 # setup a virtualenv for testing
 virtualenv venv --distribute
 . venv/bin/activate
-easy_install --zip-ok dist/zipped_egg*.egg
-easy_install --always-unzip dist/unzipped_egg*.egg
-cp ../import/test_eggs*.py venv
+easy_install --zip-ok "$distdir"/zipped_egg*.egg
+easy_install --always-unzip "$distdir"/unzipped_egg*.egg
+easy_install "$distdir"/nspkg1_*.egg
+for pkg in $NSPKG_2 ; do
+    cd $pkg
+    python setup.py install --single-version-externally-managed \
+	--record=install.log
+    cd -
+done
+cp ../import/test_{eggs,nspkg{1,2}}*.py venv
 
 # see if the unpackaged test-case still works
 cd venv
 python test_eggs1.py
 python test_eggs2.py
+python test_nspkg1.py
+python test_nspkg1-bbb-zzz.py
+python test_nspkg1-empty.py
+python test_nspkg2.py
 cd ..
 
 cd venv
-rm -rfv ../../import/zipped.egg ../../import/unzipped.egg
+rm -rfv ../../import/{,un}zipped.egg ../../import/nspkg{1,2}*-pkg
 mv -v lib/python2.7/site-packages/zipped_egg-*.egg ../../import/zipped.egg
 mv -v lib/python2.7/site-packages/unzipped_egg-*.egg ../../import/unzipped.egg
+
+pkgdir=../../import/nspkg1-pkg
+mkdir -p $pkgdir
+for pkg in $NSPKG_1 ; do
+    pkg=${pkg/-/_}
+    mv -v lib/python2.7/site-packages/$pkg-*.egg $pkgdir/$pkg.egg
+done
+
+pkgdir=../../import/nspkg2-pkg
+mkdir -p $pkgdir
+mv -v lib/python2.7/site-packages/nspkg2/ $pkgdir
+for pkg in $NSPKG_2 ; do
+    pkg=${pkg/-/_}
+    mv -v lib/python2.7/site-packages/$pkg-*.egg-info/ $pkgdir/$pkg.egg-info
+    mv -v lib/python2.7/site-packages/$pkg-*-nspkg.pth $pkgdir/$pkg-nspkg.pth
+done
+
 cd ..
 
 deactivate
 
-rm -rf build/ dist/ venv *.egg-info
+#rm -rf build/ dist/ *.egg* */*.egg* */build/ */dist/ venv
