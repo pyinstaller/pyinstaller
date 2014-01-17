9ef5b8c9f543783d8bc952fb90304ec88728910a
Bryan A. Jones <bjones AT ece.msstate.edu>
2014-01-17 17:18:05 -0600

    Fix: Many missing Pygments formatters, highlighters, and lexers now included.

--- PyInstaller/hooks/hook-pygments.py
+++ PyInstaller/hooks/hook-pygments.py
@@ -6,6 +6,12 @@
 #
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
+from hookutils import collect_submodules
 
-
-hiddenimports = ['agile', 'dotnet']
+# Pygments uses a dynamic import for its formatters, lexers,
+# and styles. There's a filter submodule, but nothing there
+# so it's not included in this list.
+hiddenimports = (
+    collect_submodules('pygments.formatters') + 
+    collect_submodules('pygments.lexers') + 
+    collect_submodules('pygments.styles'))
