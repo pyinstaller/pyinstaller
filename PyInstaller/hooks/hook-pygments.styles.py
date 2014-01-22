98a78ed7b1c43551b38fcf9916c93bd5c0318f38
Bryan A. Jones <bjones AT ece.msstate.edu>
2014-01-21 22:24:04 -0600

    Fix: Subdivide imports on a per-module basis, making them finer-grained,        per Hartmut Goebel's suggestion.

---
+++ PyInstaller/hooks/hook-pygments.styles.py
@@ -0,0 +1,13 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+# Because this is pygments.styles, note the .. required in order to get back to the hooks subdirectory.
+from ..hookutils import collect_submodules
+
+# Pygments uses a dynamic import for its formatters, so list them all here.
+hiddenimports = collect_submodules('pygments.styles')
