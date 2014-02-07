e9fc9da5cf229df84b1f7295f081f097370ae56e
Michael Hipp <michael@redmule.com>
2014-02-06 18:45:04 -0600

    Create hook-reportlab.pdfbase._fontdata.py

---
+++ PyInstaller/hooks/hook-reportlab.pdfbase._fontdata.py
@@ -0,0 +1,33 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+hiddenimports = [
+    'reportlab.pdfbase._fontdata_enc_macexpert',
+    'reportlab.pdfbase._fontdata_enc_macroman',
+    'reportlab.pdfbase._fontdata_enc_pdfdoc',
+    'reportlab.pdfbase._fontdata_enc_standard',
+    'reportlab.pdfbase._fontdata_enc_symbol',
+    'reportlab.pdfbase._fontdata_enc_winansi',
+    'reportlab.pdfbase._fontdata_enc_zapfdingbats',
+    'reportlab.pdfbase._fontdata_widths_courier',
+    'reportlab.pdfbase._fontdata_widths_courierbold',
+    'reportlab.pdfbase._fontdata_widths_courierboldoblique',
+    'reportlab.pdfbase._fontdata_widths_courieroblique',
+    'reportlab.pdfbase._fontdata_widths_helvetica',
+    'reportlab.pdfbase._fontdata_widths_helveticabold',
+    'reportlab.pdfbase._fontdata_widths_helveticaboldoblique',
+    'reportlab.pdfbase._fontdata_widths_helveticaoblique',
+    'reportlab.pdfbase._fontdata_widths_symbol',
+    'reportlab.pdfbase._fontdata_widths_timesbold',
+    'reportlab.pdfbase._fontdata_widths_timesbolditalic',
+    'reportlab.pdfbase._fontdata_widths_timesitalic',
+    'reportlab.pdfbase._fontdata_widths_timesroman',
+    'reportlab.pdfbase._fontdata_widths_zapfdingbats',
+]
+    
