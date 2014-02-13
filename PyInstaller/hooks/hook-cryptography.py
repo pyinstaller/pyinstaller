8807f313448bb4e3f881b9af8fa5fe2d6dbe8b69
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2014-02-13 00:42:03 -0800

    use collect_submodules to find hidden bindings

--- PyInstaller/hooks/hook-cryptography.py
+++ PyInstaller/hooks/hook-cryptography.py
@@ -3,13 +3,10 @@ Hook for cryptography module from the Python Cryptography Authority.
 """
 import os.path
 import glob
-from functools import partial
-from operator import concat
-from cryptography.hazmat.bindings.openssl.binding import Binding as _SSLBinding
+from hookutils import collect_submodules
 
-# add the FFI bindings' modules as hidden imports
-hiddenimports = map(partial(concat, _SSLBinding._module_prefix), _SSLBinding._modules)
-
+# add the OpenSSL FFI binding modules as hidden imports
+hiddenimports = collect_submodules('cryptography.hazmat.bindings.openssl')
 
 # include the cffi extensions as binaries
 def hook(mod):
