39bfc8fabc967f3a4f51c6c59684534b47485f51
Bryan A. Jones <bjones AT ece.msstate.edu>
2014-02-18 17:02:20 -0600

    Docs: Tweak working to clarify the use of collect_submodules.

--- PyInstaller/hooks/hook-pygments.styles.py
+++ PyInstaller/hooks/hook-pygments.styles.py
@@ -9,5 +9,5 @@
 # Because this is pygments.styles, note the .. required in order to get back to the hooks subdirectory.
 from ..hookutils import collect_submodules
 
-# Pygments uses a dynamic import for its formatters, so list them all here.
+# Pygments uses a dynamic import for its formatters, so gather them all here.
 hiddenimports = collect_submodules('pygments.styles')
