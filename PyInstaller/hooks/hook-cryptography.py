13582da6a940ed4a0d9c3bb020dc911493ad3850
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2014-02-19 06:07:03 -0800

    add comment explaining need for hook

--- PyInstaller/hooks/hook-cryptography.py
+++ PyInstaller/hooks/hook-cryptography.py
@@ -20,6 +20,12 @@ from hookutils import collect_submodules, get_module_file_attribute
 hiddenimports = collect_submodules('cryptography.hazmat.bindings.openssl')
 
 def hook(mod):
+    """
+    Include the cffi extensions as binaries in a subfolder named like the package.
+    The cffi verifier expects to find them inside the package directory for 
+    the main module. We cannot use hiddenimports because that would add the modules
+	outside the package.
+    """
     cryptography_dir = os.path.dirname(get_module_file_attribute('cryptography'))
     for ext in ('pyd', 'so'):
         ffimods = glob.glob(os.path.join(cryptography_dir, '_cffi_*.%s*' % ext))
