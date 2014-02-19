f13f7c5ba2963b7a6a493297a1dfe700b6414106
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2014-02-19 06:06:35 -0800

    use get_module_file_attribute instead of mod.__file__

--- PyInstaller/hooks/hook-cryptography.py
+++ PyInstaller/hooks/hook-cryptography.py
@@ -14,14 +14,13 @@ Hook for cryptography module from the Python Cryptography Authority.
 
 import os.path
 import glob
-from hookutils import collect_submodules
+from hookutils import collect_submodules, get_module_file_attribute
 
 # add the OpenSSL FFI binding modules as hidden imports
 hiddenimports = collect_submodules('cryptography.hazmat.bindings.openssl')
 
-# include the cffi extensions as binaries
 def hook(mod):
-    cryptography_dir = os.path.dirname(mod.__file__)
+    cryptography_dir = os.path.dirname(get_module_file_attribute('cryptography'))
     for ext in ('pyd', 'so'):
         ffimods = glob.glob(os.path.join(cryptography_dir, '_cffi_*.%s*' % ext))
         for f in ffimods:
