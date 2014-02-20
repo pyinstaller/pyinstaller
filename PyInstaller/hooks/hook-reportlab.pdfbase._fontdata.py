b0c470c185e46e3872e824a8cc757c6b142a27f9
Michael Hipp <michael@redmule.com>
2014-02-20 13:33:51 -0600

    Update hook-reportlab.pdfbase._fontdata.py
    Changed to use 'collect_submodules' instead of hard-coded file names.
    Added comment that it has been tested on Windows 7 x64 and Python 2.7.6 with ReportLab 3.0 and does not work on RL 2.7.

--- PyInstaller/hooks/hook-reportlab.pdfbase._fontdata.py
+++ PyInstaller/hooks/hook-reportlab.pdfbase._fontdata.py
@@ -7,27 +7,12 @@
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
 
-hiddenimports = [
-    'reportlab.pdfbase._fontdata_enc_macexpert',
-    'reportlab.pdfbase._fontdata_enc_macroman',
-    'reportlab.pdfbase._fontdata_enc_pdfdoc',
-    'reportlab.pdfbase._fontdata_enc_standard',
-    'reportlab.pdfbase._fontdata_enc_symbol',
-    'reportlab.pdfbase._fontdata_enc_winansi',
-    'reportlab.pdfbase._fontdata_enc_zapfdingbats',
-    'reportlab.pdfbase._fontdata_widths_courier',
-    'reportlab.pdfbase._fontdata_widths_courierbold',
-    'reportlab.pdfbase._fontdata_widths_courierboldoblique',
-    'reportlab.pdfbase._fontdata_widths_courieroblique',
-    'reportlab.pdfbase._fontdata_widths_helvetica',
-    'reportlab.pdfbase._fontdata_widths_helveticabold',
-    'reportlab.pdfbase._fontdata_widths_helveticaboldoblique',
-    'reportlab.pdfbase._fontdata_widths_helveticaoblique',
-    'reportlab.pdfbase._fontdata_widths_symbol',
-    'reportlab.pdfbase._fontdata_widths_timesbold',
-    'reportlab.pdfbase._fontdata_widths_timesbolditalic',
-    'reportlab.pdfbase._fontdata_widths_timesitalic',
-    'reportlab.pdfbase._fontdata_widths_timesroman',
-    'reportlab.pdfbase._fontdata_widths_zapfdingbats',
-]
-    
+from hookutils import collect_submodules
+hiddenimports = []
+
+# Tested on Windows 7 x64 with Python 2.7.6 x32 using ReportLab 3.0
+# This has been observed to *not* work on ReportLab 2.7
+
+for x in collect_submodules('reportlab.pdfbase'):
+    if x.startswith('reportlab.pdfbase._fontdata_'):
+        hiddenimports.append(x)
