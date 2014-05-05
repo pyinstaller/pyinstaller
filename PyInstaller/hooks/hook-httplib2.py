612edbab9d75cede59c14009a9326b253004ebd6
Michael Sverdlik <m1keil@users.noreply.github.com>
2014-05-05 14:05:49 +0300

    Update hook-httplib2.py

--- PyInstaller/hooks/hook-httplib2.py
+++ PyInstaller/hooks/hook-httplib2.py
@@ -7,7 +7,6 @@
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
 
-
 #this is needed to bundle cacerts.txt that comes with httplib2 module
 from PyInstaller.hooks.hookutils import collect_data_files
 datas = collect_data_files('httplib2')
