669f4432d8a15ba94fdaed3a22c6dd5e114114bb
kz26 <kz26@users.noreply.github.com>
2014-05-12 14:13:36 -0500

    Update hook-pyexcelerate.py

--- PyInstaller/hooks/hook-pyexcelerate.py
+++ PyInstaller/hooks/hook-pyexcelerate.py
@@ -1,14 +1,15 @@
-"""
-PyInstaller hook for PyExcelerate
-(C) 2014 Kevin Zhang
-"""
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
 
 import os
 
-try:
-	from pyexcelerate.Writer import _TEMPLATE_PATH
-	datas = [
-		(os.path.join(_TEMPLATE_PATH, '*'), 'pyexcelerate/templates')
-	]
-except ImportError:
-	pass
+from pyexcelerate.Writer import _TEMPLATE_PATH
+datas = [
+	(os.path.join(_TEMPLATE_PATH, '*'), os.path.join('pyexcelerate', 'templates'))
+]
