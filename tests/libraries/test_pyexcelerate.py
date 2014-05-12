63f3e5150cc4b2691a5069bea3006288d2063c7d
kz26 <kz26@users.noreply.github.com>
2014-05-12 14:15:48 -0500

    Update test_pyexcelerate.py

--- tests/libraries/test_pyexcelerate.py
+++ tests/libraries/test_pyexcelerate.py
@@ -7,4 +7,7 @@
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
 
+# Requires PyExcelerate 0.6.1 or higher
+# Tested on Windows 7 x64 SP1 with CPython 2.7.6
+
 import pyexcelerate
