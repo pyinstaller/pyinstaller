9524c5396bf5b4b4a2307cd250315584c2eb2bd2
Hartmut Goebel <h.goebel@crazy-compilers.com>
2014-06-04 18:56:33 +0200

    Add a comment to the fake-file required for nspkg.pth-style namespaces.

---
+++ PyInstaller/fake/namespace/__init__.py
@@ -0,0 +1,3 @@
+# This is an empty source file, required for faking
+# namespace-packages. It has to be named __init__.py, since
+# pyi_archive.zlibArchive.add() takes that as a marker for packages.
