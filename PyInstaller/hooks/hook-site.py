8eb40c371a84091da5788110269acc019037bf73
Martin Zibricky <mzibr.public@gmail.com>
2014-06-21 17:59:11 +0200

    Issue #85: Fix running PyInstaller inside virtualenv. Use real site.py module and not the one from virtualenv.

---
+++ PyInstaller/hooks/hook-site.py
@@ -0,0 +1,32 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+
+# In virtualenv, site.py module is overriden. We need to bundle the real
+# site.py module.
+
+import os
+import sys
+
+from PyInstaller.compat import is_virtualenv, is_win
+from PyInstaller.utils.hooks.hookutils import exec_statement
+
+
+def hook(mod):
+    if is_virtualenv:
+        # Workaround to get real path of site.py module.
+        if is_win:
+            mod_path = os.path.join(sys.real_prefix, 'Lib', 'site.py')
+        else:
+            mod_pth = os.path.join(sys.real_prefix, 'lib', 'python'+sys.version[:3], 'site.py')
+            mod64_path = os.path.join(sys.real_prefix, 'lib64', 'python'+sys.version[:3], 'site.py')
+            if os.path.exists(mod64_path):
+                mod_pth = mod64_path
+        mod.retarget(mod_path)
+    return mod
