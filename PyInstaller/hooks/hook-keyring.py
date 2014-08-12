b2bf99b8e839f49533170c4a9d804aa2d8dd987f
Ryan Branche <rbranche@apple.com>
2014-08-12 13:28:33 -0700

    Add license to hook file.

--- PyInstaller/hooks/hook-keyring.py
+++ PyInstaller/hooks/hook-keyring.py
@@ -1,3 +1,12 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2014, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
 hiddenimports = ['keyring.backends.file',
                  'keyring.backends.Gnome',
                  'keyring.backends.Google',
