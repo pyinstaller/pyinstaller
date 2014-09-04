651202dc0dadbe68969c5812a92ff49fdecae7b9
Lorenzo Villani <lorenzo@villani.me>
2014-09-04 14:47:56 +0200

    Ensure we are pulling QtWebkitWidgets

---
+++ PyInstaller/hooks/hook-PyQt5.QtWebKitWidgets.py
@@ -0,0 +1,20 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2014, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+
+hiddenimports = ["sip",
+                 "PyQt5.QtCore",
+                 "PyQt5.QtGui",
+                 "PyQt5.QtNetwork",
+                 "PyQt5.QtQuick",
+                 "PyQt5.QtQml",
+                 "PyQt5.QtSql",
+                 "PyQt5.QtSensors",
+                 "PyQt5.QtWebKit"
+                 ]
