2056c384b8397a7495a62363387d4e602c63eae0
bryancort <bryancort@gmail.com>
2015-01-14 10:51:44 -0500

    Updated with testing environment

--- PyInstaller/hooks/hook-psychopy.py
+++ PyInstaller/hooks/hook-psychopy.py
@@ -7,5 +7,7 @@
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
 
+#Tested on Windows 7 64bit with python 2.7.6 and PsychoPy 1.81.03
+
 from PyInstaller.hooks import hookutils
 datas = hookutils.collect_data_files('psychopy')
