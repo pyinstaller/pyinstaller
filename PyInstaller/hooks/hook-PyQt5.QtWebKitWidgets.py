7b48ea77d98a67524509d1921f8a43375ff411a0
Nat Picker <tallforasmurf@yahoo.com>
2015-05-01 16:03:56 -0700

    Remove unnecessary hidden imports for QtWebKitWidgets from the hook.

--- PyInstaller/hooks/hook-PyQt5.QtWebKitWidgets.py
+++ PyInstaller/hooks/hook-PyQt5.QtWebKitWidgets.py
@@ -7,14 +7,11 @@
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
 
-
+# Shorten list of hidden imports to what PyQt5.QtWebKitWidgets actually
+# does link to. It does not need Qml, QSql, or QtQuick.
 hiddenimports = ["sip",
                  "PyQt5.QtCore",
                  "PyQt5.QtGui",
                  "PyQt5.QtNetwork",
-                 "PyQt5.QtQuick",
-                 "PyQt5.QtQml",
-                 "PyQt5.QtSql",
-                 "PyQt5.QtSensors",
                  "PyQt5.QtWebKit"
                  ]
