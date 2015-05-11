740d4415bd1f49ab94a72556cfaedb1f10306a3c
Hartmut Goebel <h.goebel@crazy-compilers.com>
2015-05-11 23:02:24 +0200

    Import error in hook-PyQt4, hook-PyQt5 and hook-PySide.
    This fixes #1257.

--- PyInstaller/hooks/hook-PySide.py
+++ PyInstaller/hooks/hook-PySide.py
@@ -18,7 +18,7 @@ from PyInstaller.utils.hooks.hookutils import qt4_menu_nib_dir
 # The PySide directory contains Qt dlls. We need to avoid including different
 # version of Qt libraries when there is installed another application (e.g. QtCreator)
 if is_win:
-    from PyInstaller.utils.winutils import extend_system_path
+    from PyInstaller.utils.win32.winutils import extend_system_path
     extend_system_path([os.path.join(x, 'PySide') for x in getsitepackages()])
 
 
