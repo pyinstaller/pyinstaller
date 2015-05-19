6f84a5ac1a511c192379197aab7dc8756c05caec
Hartmut Goebel <h.goebel@crazy-compilers.com>
2015-05-19 22:43:42 +0200

    Add a comment.

--- PyInstaller/hooks/hook-rawpy.py
+++ PyInstaller/hooks/hook-rawpy.py
@@ -7,4 +7,7 @@
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
 
+# Python wrapper for LibRaw (https://pypi.python.org/pypi/rawpy)
+# Tested with version 0.3.5
+
 hiddenimports = ['numpy', 'enum']
