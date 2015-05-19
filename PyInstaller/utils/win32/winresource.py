pyinstaller/pyinstaller

commit 713b169953553406e13751a9d224b1b8bce85823

Minor cleanup: Use xx.decode(...) instead of bytes(xx, ...).

PyInstaller/utils/win32/winresource.py
@@ -202,7 +202,8 @@ def UpdateResources(dstpath, data, type_, names=None, languages=None):
             for language in res[type_][name]:
                 logger.info("Updating resource type %s name %s language %s",
                             type_, name, language)
-                win32api.UpdateResource(hdst, type_, name, bytes(data, 'UTF-8'), language)
+                win32api.UpdateResource(hdst, type_, name,
+                                        data.decode('UTF-8'), language)
     win32api.EndUpdateResource(hdst, 0)
 
 

commit 9b00c2ccde3475888cfefeb8e86f44a31aac6548

Fix #1275: string decode error in utils/win32/winresource.py
This as introduced by myself in 713b1699.

PyInstaller/utils/win32/winresource.py
@@ -203,7 +203,7 @@ def UpdateResources(dstpath, data, type_, names=None, languages=None):
                 logger.info("Updating resource type %s name %s language %s",
                             type_, name, language)
                 win32api.UpdateResource(hdst, type_, name,
-                                        data.decode('UTF-8'), language)
+                                        data.encode('UTF-8'), language)
     win32api.EndUpdateResource(hdst, 0)
 
 