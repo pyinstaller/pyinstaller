681a8378050a499dace6e1c20e3ada6198b86673
Bryan A. Jones <bjones AT ece.msstate.edu>
2015-07-01 08:55:27 -0500

    Fix: Provide correct encoding for Windows based on querying chcp.

--- tests/basic/test_stdout_encoding.py
+++ tests/basic/test_stdout_encoding.py
@@ -9,21 +9,31 @@
 
 
 import sys
+import subprocess
 
 
 frozen_encoding = str(sys.stdout.encoding)
 
-
-# For various OS encoding is different.
-# On Windows it should be still cp850.  ## FIXME: Is this correct???  I don't have Windows!
-# On Linux, MAC OS X, and other unixes it should be mostly 'UTF-8'.
-encoding = 'cp850' if sys.platform.startswith('win') else 'UTF-8'
-
+# For various OSes the encoding is different.
+if sys.platform.startswith('win'):
+    # The default Windows encoding is based on the active console code page,
+    # which varies from PC to PC and may be changed by the user. For more info,
+    # see https://technet.microsoft.com/en-us/library/bb490874.aspx.
+    # Therefore, ask for the current encoding using chcp (see link above
+    # for details). The expected output: 'Active code page: nnn\r\n'.
+    # We want only the nnn (a number which specifies the code page).
+    chcp_out = subprocess.check_output(['chcp'], shell=True)
+    # The encoding consists of the string `cpnnn`, where the nnn is replaced
+    # by the active code page.
+    encoding = 'cp' + chcp_out.split()[-1]
+else:
+    # On Linux, MAC OS X, and other Unixes it should be mostly 'UTF-8'.
+    encoding = 'UTF-8'
 
 print('Encoding expected: ' + encoding)
 print('Encoding current: ' + frozen_encoding)
 
-
 if not frozen_encoding == encoding:
     raise SystemExit('Frozen encoding %s is not the same as unfrozen %s.' %
                      (frozen_encoding, encoding))
+
