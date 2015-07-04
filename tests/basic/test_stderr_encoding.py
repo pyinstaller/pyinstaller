c3809171864a8278904ecc5447ea8073fd88d392
Bryan A. Jones <bjones AT ece.msstate.edu>
2015-07-04 15:24:31 -0500

    Refactor: Place common code to detect platform-specific encoding in a separate file.

--- tests/basic/test_stderr_encoding.py
+++ tests/basic/test_stderr_encoding.py
@@ -7,28 +7,11 @@
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
 
-
 import sys
-import subprocess
-
+# Get the expected stdout/stderr encoding for this platform.
+from expected_stdxxx_encoding import encoding
 
 frozen_encoding = str(sys.stderr.encoding)
-
-# For various OSes the encoding is different.
-if sys.platform.startswith('win'):
-    # The default Windows encoding is based on the active console code page,
-    # which varies from PC to PC and may be changed by the user. For more info,
-    # See https://technet.microsoft.com/en-us/library/bb490874.aspx.
-    # Therefore, ask for the current encoding using chcp (see link above
-    # for details). The expected output: 'Active code page: nnn\r\n'.
-    # We want only the nnn (a number which specifies the code page).
-    chcp_out = subprocess.check_output(['chcp'], shell=True)
-    # The encoding consists of the string `cpnnn`, where the nnn is replaced
-    # by the active code page.
-    encoding = 'cp' + chcp_out.split()[-1]
-else:
-    # On Linux, MAC OS X, and other Unixes it should be mostly 'UTF-8'.
-    encoding = 'UTF-8'
 
 print('Encoding expected: ' + encoding)
 print('Encoding current: ' + frozen_encoding)
