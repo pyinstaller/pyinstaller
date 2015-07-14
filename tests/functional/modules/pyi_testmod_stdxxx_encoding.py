68a3ee4b0756ccbe88056499c39f56eb407d9a48
Martin Zibricky <mzibr.public@gmail.com>
2015-07-14 14:30:19 +0200

    Pull request #1319: Adapt test_stdxxx_encoding for Python 3.

--- tests/functional/modules/pyi_testmod_stdxxx_encoding.py
+++ tests/functional/modules/pyi_testmod_stdxxx_encoding.py
@@ -12,16 +12,28 @@ import subprocess
 
 # For various OSes the encoding is different.
 if sys.platform.startswith('win'):
-    # The default Windows encoding is based on the active console code page,
-    # which varies from PC to PC and may be changed by the user. For more info,
-    # See https://technet.microsoft.com/en-us/library/bb490874.aspx.
-    # Therefore, ask for the current encoding using chcp (see link above
-    # for details). The expected output: 'Active code page: nnn\r\n'.
-    # We want only the nnn (a number which specifies the code page).
-    chcp_out = subprocess.check_output(['chcp'], shell=True)
-    # The encoding consists of the string `cpnnn`, where the nnn is replaced
-    # by the active code page.
-    encoding = 'cp' + chcp_out.split()[-1]
+    if sys.version_info[0] == 2:
+        # The default Windows encoding is based on the active console code page,
+        # which varies from PC to PC and may be changed by the user. For more info,
+        # See https://technet.microsoft.com/en-us/library/bb490874.aspx.
+        # Therefore, ask for the current encoding using chcp (see link above
+        # for details). The expected output: 'Active code page: nnn\r\n'.
+        # We want only the nnn (a number which specifies the code page).
+        chcp_out = subprocess.check_output(['chcp'])
+        # The encoding consists of the string `cpnnn`, where the nnn is replaced
+        # by the active code page.
+        encoding = 'cp' + chcp_out.split()[-1]
+    else:
+        # For Python 3 the code page is different if it runs on the active console
+        # or not.
+        # In the interactive console MS-DOS code pages are used (cp852) otherwise
+        # ansi code pages are used (e.g. cp1250).
+        # The test suite runs this test in a non-interactive console.
+        #
+        # The app created by PyInstaller reports the same encoding as from
+        # locale.getpreferredencoding()
+        import locale
+        encoding = locale.getpreferredencoding(False)
 else:
     # On Linux, MAC OS X, and other Unixes it should be mostly 'UTF-8'.
     encoding = 'UTF-8'
