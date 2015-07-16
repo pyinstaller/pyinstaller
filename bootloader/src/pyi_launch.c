54590dffdb867081614e5ffb9a1fe86f1ffd4ac2
Hartmut Goebel <h.goebel@crazy-compilers.com>
2015-07-16 10:30:35 +0200

    In pyi_launch_run_scripts set `__file__` with Python 2, too.

--- bootloader/src/pyi_launch.c
+++ bootloader/src/pyi_launch.c
@@ -336,8 +336,11 @@ int pyi_launch_run_scripts(ARCHIVE_STATUS *status)
 			strcpy(buf, ptoc->name);
 			strcat(buf, ".py");
 			//VS("LOADER: Running %s\n", buf);
-            //__file__ = PI_PyUnicode_FromStringAndSize(buf, strlen(buf));
-            __file__ = PI_PyUnicode_FromString(buf);
+            if (is_py2) {
+	      __file__ = PI_PyString_FromString(buf);
+	    } else {
+	      __file__ = PI_PyUnicode_FromString(buf);
+	    };
             PI_PyObject_SetAttrString(__main__, "__file__", __file__);
             Py_DECREF(__file__);
 			/* Run it */
