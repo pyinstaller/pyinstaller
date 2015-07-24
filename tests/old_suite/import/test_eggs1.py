a6d67297215f3fc629fa4557a0e9d91997e3f034
Hartmut Goebel <h.goebel@crazy-compilers.com>
2015-07-24 09:57:59 +0200

    Minor cleanup for two test-cases.
    Just define the expected data once, since it has to be encoded
    to become bytes.

--- tests/old_suite/import/test_eggs1.py
+++ tests/old_suite/import/test_eggs1.py
@@ -16,10 +16,12 @@ sys.path.append(os.path.join(os.path.dirname(__file__), 'unzipped.egg'))
 
 import pkg_resources
 
+expected_data = 'This is data file for `unzipped`.'.encode('ascii')
+
 t = pkg_resources.resource_string('unzipped_egg', 'data/datafile.txt')
-assert t.rstrip() == 'This is data file for `unzipped`.'
+assert t.rstrip() == expected_data
 
 import unzipped_egg
-assert unzipped_egg.data == 'This is data file for `unzipped`.'
+assert unzipped_egg.data == expected_data
 
 print('Okay.')
