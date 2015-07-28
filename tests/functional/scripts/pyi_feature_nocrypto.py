eb460470c2a65ee889644e47d655523b1a5e4705
Martin Zibricky <mzibr.public@gmail.com>
2015-07-28 12:19:39 +0200

    Issue #1299: Migrate crypto tests to py.test.

--- tests/functional/scripts/pyi_feature_nocrypto.py
+++ tests/functional/scripts/pyi_feature_nocrypto.py
@@ -7,17 +7,15 @@
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
 
-import imp
-
 
 try:
-    imp.find_module('pyi_crypto')
+    import pyi_crypto
 except ImportError:
     raise AssertionError('The pyi_crypto module should be there even if crypto is disabled.')
 
 
 try:
-    imp.find_module('pyi_crypto_key')
+    import pyimod00_crypto_key
 
     raise AssertionError('The pyimod00_crypto_key module must NOT be there if crypto is disabled.')
 except ImportError:
