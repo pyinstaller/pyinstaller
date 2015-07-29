cfa6f3fd2675fa26256431307822b77b14498f65
Martin Zibricky <mzibr.public@gmail.com>
2015-07-29 10:41:49 +0200

    Issue #1299: For Appveyor fix msvc detection on Windows in test test_load_dll_using_ctypes.

--- tests/functional/test_basic.py
+++ tests/functional/test_basic.py
@@ -122,8 +122,13 @@ def test_load_dll_using_ctypes(tmpdir, monkeypatch, pyi_builder):
     if is_win:
         ret = subprocess.call('gcc -shared ctypes_dylib-win.c -o ctypes_dylib.dll', shell=True)
         if ret != 0:
+            # Find path to cl.exe file.
+            from distutils.msvccompiler import MSVCCompiler
+            comp = MSVCCompiler()
+            comp.initialize()
+            cl_path = comp.cc
             # Fallback to msvc.
-            ret = subprocess.call('cl /LD ctypes_dylib-win.c', shell=True)
+            ret = subprocess.call('%s /LD ctypes_dylib-win.c' % cl_path, shell=True)
     elif is_darwin:
         # On Mac OS X we need to detect architecture - 32 bit or 64 bit.
         arch = 'i386' if architecture() == '32bit' else 'x86_64'
