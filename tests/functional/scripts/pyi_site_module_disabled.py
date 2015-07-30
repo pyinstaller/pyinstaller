c45d929ae3af4f4961b6984538dfe10065708359
David Vierra <codewarrior@hawaii.rr.com>
2015-07-30 13:42:44 -1000

    Rewrite test_site_module_disabled after investigating site module behavior and -S flag
    (-S sets the global Py_NoSiteFlag which we set in the bootloader.)
    
    -S flag tells Python not to import site. Check that site has not been imported by checking sys.modules.
    On import, site initializes USER_SITE and USER_BASE, so just check that they aren't in sys.path.

--- tests/functional/scripts/pyi_site_module_disabled.py
+++ tests/functional/scripts/pyi_site_module_disabled.py
@@ -11,19 +11,39 @@
 # Test that the Pythons 'site' module is disabled and Python is not searching
 # for any user-specific site directories.
 
-# Check that option -S is passed to Python interpreter.
+# Check that option -S is passed to Python interpreter and that sys.path has
+# not been modified.
 
 
-import site
 import sys
 
+# The option -S tells Python not to import `site` on startup.
+# If site has been imported already, that's instant failure.
+if 'site' in sys.modules:
+    raise SystemExit('site module already imported')
+
+import site
 
 # Check it is really disabled.
 if not sys.flags.no_site:
     raise SystemExit('site module is enabled!')
 
 # Default values 'site' module when it is disabled.
-if not site.ENABLE_USER_SITE == False:
-    raise SystemExit('ENABLE_USER_SITE not False.')
-if site.USER_SITE is not None and site.USER_BASE is not None:
-    raise SystemExit('USER_SITE or USER_BASE not None.')
+# On Py2, ENABLE_USER_SITE should be False; on Py3, it should be None.
+if site.ENABLE_USER_SITE not in (None, False):
+    raise SystemExit('ENABLE_USER_SITE is %s, expected %s.' %
+                     (site.ENABLE_USER_SITE, (None, False)))
+
+
+# Since we import `site` here in the test, this causes USER_SITE and USER_BASE to be
+# initialized on Py2, so all we can do is confirm that the paths aren't in sys.path
+
+if site.USER_SITE is not None:
+    if site.USER_SITE in sys.path:
+        raise SystemExit('USER_SITE found in sys.path')
+
+# This should never happen, USER_BASE isn't a site-modules folder and is only used by
+# distutils for installing module datas.
+if site.USER_BASE is not None:
+    if site.USER_SITE in sys.path:
+        raise SystemExit('USER_BASE found in sys.path')
