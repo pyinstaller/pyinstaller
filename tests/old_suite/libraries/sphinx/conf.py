34330898963154d1066d3b2a1b564a5e6a81c8c7
Hartmut Goebel <h.goebel@crazy-compilers.com>
2015-07-31 15:23:45 +0200

    Make test_sphinx run even if (optional) theme is not installed.

--- tests/old_suite/libraries/sphinx/conf.py
+++ tests/old_suite/libraries/sphinx/conf.py
@@ -108,7 +108,12 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+try:
+    # try to use optional theme
+    import alabaster
+    html_theme = 'alabaster'
+except ImportError:
+    html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
