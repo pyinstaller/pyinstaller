33cc8f328efc7c762583641551313941c2304808
Jonathan Springer <springermac@gmail.com>
2015-08-12 22:41:36 -0400

    Add hook and runtime hook for ssl

---
+++ PyInstaller/hooks/hook-ssl.py
@@ -0,0 +1,10 @@
+from PyInstaller.compat import is_darwin
+from PyInstaller.utils.hooks.hookutils import exec_statement
+
+if is_darwin:  # TODO check if this is needed on linux
+    datas = []
+    files = exec_statement("""
+    import ssl
+    print(ssl.get_default_verify_paths().cafile)""").strip().split()
+    for file in files:
+        datas.append((file, 'lib'))  # TODO find a way to make sure the bundled cafile is always named 'cert.pem'
