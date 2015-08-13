465b78523f2c1eb661538979273b9e333c049182
Jonathan Springer <springermac@gmail.com>
2015-08-12 23:10:06 -0400

    Fix indent issue

--- PyInstaller/hooks/hook-ssl.py
+++ PyInstaller/hooks/hook-ssl.py
@@ -4,7 +4,7 @@ from PyInstaller.utils.hooks.hookutils import exec_statement
 if is_darwin:  # TODO check if this is needed on linux
     datas = []
     files = exec_statement("""
-    import ssl
-    print(ssl.get_default_verify_paths().cafile)""").strip().split()
+import ssl
+print(ssl.get_default_verify_paths().cafile)""").strip().split()
     for file in files:
         datas.append((file, 'lib'))  # TODO find a way to make sure the bundled cafile is always named 'cert.pem'
