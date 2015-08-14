496b6ec9c3d8cd2412bfadcc5d3986e797bfb372
Martin Zibricky <mzibr.public@gmail.com>
2015-08-14 17:14:22 +0200

    Issue #1404: Fix hook-zmq.py to properly bundle libzmq and libsodium.

--- PyInstaller/hooks/hook-zmq.py
+++ PyInstaller/hooks/hook-zmq.py
@@ -1,5 +1,5 @@
 #-----------------------------------------------------------------------------
-# Copyright (c) 2013, PyInstaller Development Team.
+# Copyright (c) 2005-2015, PyInstaller Development Team.
 #
 # Distributed under the terms of the GNU General Public License with exception
 # for distributing bootloader.
@@ -12,30 +12,15 @@
 Hook for PyZMQ. Cython based Python bindings for messaging library ZeroMQ.
 http://www.zeromq.org/
 """
-import glob
-import os
-import itertools
-from PyInstaller.utils.hooks.hookutils import collect_submodules
+from PyInstaller.utils.hooks.hookutils import collect_dynamic_libs, collect_submodules
 
 hiddenimports = ['zmq.utils.garbage']
 hiddenimports.extend(collect_submodules('zmq.backend'))
 
-def hook(mod):
-    # If PyZMQ provides its own copy of libzmq or libsodium, add it to the
-    # extension-modules TOC so zmq/__init__.py can load it at runtime.
-    # For predictable behavior, the libzmq search here must be equivalent
-    # to the search in zmq/__init__.py.
-    zmq_directory = os.path.dirname(mod.__file__)
-    # search for libzmq*.{pyd,so,dll,dylib}* and libsodium*.{pyd,so,dll,dylib}*
-    for libname, ext in itertools.product(('libzmq', 'libsodium'),
-                                          ('pyd', 'so', 'dll', 'dylib')):
-        bundled = glob.glob(os.path.join(zmq_directory, libname + '.' + ext))
-        if bundled:
-            # zmq/__init__.py will look in os.join(sys._MEIPASS, 'zmq'),
-            # so libzmq has to land there.
-            name = os.path.join('zmq', os.path.basename(bundled[0]))
-            mod.add_binary((
-                (name, bundled[0], 'BINARY'),
-            ))
-
-    return mod
+# If PyZMQ provides its own copy of libzmq or libsodium, add it to the
+# extension-modules TOC so zmq/__init__.py can load it at runtime.
+# For predictable behavior, the libzmq search here must be equivalent
+# to the search in zmq/__init__.py.
+# zmq/__init__.py will look in os.join(sys._MEIPASS, 'zmq'),
+# so libzmq has to land there.
+binaries = collect_dynamic_libs('zmq')
