e432508965776c3efd25475a2318a0ba429b919f
Bryan A. Jones <bjones AT ece.msstate.edu>
2015-08-28 14:22:49 -0500

    Fix: Properly locate the data/ subdirectory relative to scripts/.

--- tests/functional/scripts/pyi_get_datadir.py
+++ tests/functional/scripts/pyi_get_datadir.py
@@ -18,8 +18,8 @@ import os.path
 # Globals
 # -------
 # Directory storing test-specific data.
-DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
-
+DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                        '..', 'data')
 
 # Functions
 # ---------
