89b4ee69fb3a8a073a2a1d2271a955caf66a8b56
Hartmut Goebel <h.goebel@crazy-compilers.com>
2015-09-02 14:34:49 +0200

    Add (draft) method to get the distribution a module belongs to.

--- PyInstaller/building/toc_conversion.py
+++ PyInstaller/building/toc_conversion.py
@@ -7,6 +7,8 @@
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
 
+import pkg_resources
+from ..depend.utils import get_path_to_egg
 
 class DependencyProcessor(object):
     """
@@ -16,6 +18,8 @@ class DependencyProcessor(object):
     def __init__(self, graph, additional_files):
         self._binaries = set()
         self._datas = set()
+        self._distributions = set()
+        self.__seen_distribution_paths = set()
         # Include files that were found by hooks.
         # graph.flatten() should include only those modules that are reachable
         # from top-level script.
@@ -25,6 +29,46 @@ class DependencyProcessor(object):
             if name in additional_files:
                 self._binaries.update(additional_files.binaries(name))
                 self._datas.update(additional_files.datas(name))
+            # Any module can belong to a single distribution
+            self._distributions.update(self._get_distribution_for_node(node))
+
+
+    def _get_distribution_for_node(self, node):
+        """Get the distribution a module belongs to.
+
+        Bug: This currently only handles packages in eggs.
+        """
+        # TODO add support for single modules in eggs (e.g. mock-1.0.1)
+        # TODO add support for egg-info:
+        # TODO add support for wheels (dist-info)
+        #
+        # TODO add support for unpacked eggs and for new .whl packages.
+        # Wheels:
+        #  .../site-packages/pip/  # It seams this has to be a package
+        #  .../site-packages/pip-6.1.1.dist-info
+        # Unzipped Eggs:
+        #  .../site-packages/mock.py   # this may be a single module, too!
+        #  .../site-packages/mock-1.0.1-py2.7.egg-info
+        # Unzipped Eggs (I asume: multiple-versions externaly managed):
+        #  .../site-packages/pyPdf-1.13-py2.6.egg/pyPdf/
+        #  .../site-packages/pyPdf-1.13-py2.6.egg/EGG_INFO
+        # Zipped Egg:
+        #  .../site-packages/zipped.egg/zipped_egg/
+        #  .../site-packages/zipped.egg/EGG_INFO
+        modpath = node.filename
+        if not modpath:
+            # e.g. namespace-package
+            return []
+        # TODO: add other ways to get a distribution path
+        distpath = get_path_to_egg(modpath)
+        if not distpath or distpath in self.__seen_distribution_paths:
+            # no egg or already handled
+            return []
+        self.__seen_distribution_paths.add(distpath)
+        dists = list(pkg_resources.find_distributions(distpath))
+        assert len(dists) == 1
+        return dists
+
 
     # Public methods.
 
