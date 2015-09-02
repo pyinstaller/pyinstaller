aa39df3f3a8b25cbbecc20fb1bda517f1b83d168
Bryan A. Jones <bjones AT ece.msstate.edu>
2015-09-02 14:08:06 -0500

    Refactor: Remove duplicated code, using get_data_dir instead.

--- tests/functional/scripts/pyi_load_dll_using_ctypes.py
+++ tests/functional/scripts/pyi_load_dll_using_ctypes.py
@@ -7,11 +7,15 @@
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
 
-
+# Library imports
+# ---------------
 import os
-
 import sys
 from ctypes import CDLL
+
+# Local imports
+# -------------
+from pyi_get_datadir import get_data_dir
 
 # Library name based on platform.
 if sys.platform.startswith('win32'):
@@ -20,38 +24,15 @@ elif sys.platform.startswith("darwin"):
     name = 'ctypes_dylib.dylib'
 else:
     name = 'ctypes_dylib.so'
-exec_dir = os.path.dirname(sys.executable)
-
-# onedir mode:
-# tmpdir
-# ├── ctypes_dylib.dll
-# ├── ctypes_dylib.so
-# ├── ctypes_dylib.dylib
-# ├── build
-# └── dist
-#     └── appname
-#         └── appname.exe
-pth_onedir = os.path.join(exec_dir, '..', '..', 'data', 'load_dll_using_ctypes', name)
-# onefile mode:
-# tmpdir
-# ├── ctypes_dylib.dll
-# ├── ctypes_dylib.so
-# ├── ctypes_dylib.dylib
-# ├── build
-# └── dist
-#     └── appname.exe
-pth_onefile = os.path.join(exec_dir, '..', 'data', 'load_dll_using_ctypes', name)
-lib_filename = pth_onedir if os.path.exists(pth_onedir) else pth_onefile
-
 
 def dummy(arg):
     """
     Test loading ctypes library and passing an argument to it.
     """
-    tct = CDLL(lib_filename)
+    tct = CDLL(os.path.join(get_data_dir(), 'load_dll_using_ctypes', name))
     return tct.dummy(arg)
-
 
 # Test resolving dynamic libraries loaded in Python code at runtime
 # by Python module 'ctypes'
 assert dummy(42) == 42
+
