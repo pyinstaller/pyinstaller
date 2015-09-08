f38488d7749565f3e51fbfd506c95bfdf8fb68fe
Martin Zibricky <mzibr.public@gmail.com>
2015-09-08 23:54:52 +0200

    Issue #967: In hook-enchant.py bundle libenchant from macports.

--- PyInstaller/hooks/hook-enchant.py
+++ PyInstaller/hooks/hook-enchant.py
@@ -15,9 +15,11 @@ Tested with PyEnchatn 1.6.6.
 """
 
 import os
+import sys
 
 from PyInstaller.compat import is_darwin
-from PyInstaller.utils.hooks.hookutils import exec_statement, collect_data_files, eval_script
+from PyInstaller.utils.hooks.hookutils import exec_statement, collect_data_files, \
+    collect_dynamic_libs, eval_script
 
 
 # TODO Add Linux support
@@ -26,28 +28,37 @@ from PyInstaller.utils.hooks.hookutils import exec_statement, collect_data_files
 # - Windows: libenchat-1.dll, libenchat_ispell.dll, libenchant_myspell.dll, other
 #            dependent dlls and dictionaries for several languages (de, en, fr)
 # - Mac OS X: usually libenchant.dylib and several dictionaries when installed via pip.
+binaries = collect_dynamic_libs('enchant')
 datas = collect_data_files('enchant')
 
 
 # On OS X try to find files from Homebrew or Macports environments.
 if is_darwin:
-    # Collecting dictionaries only works when pyenchant is installed via pip since the dictionaries installed via pip
-    #
-    binaries = []
-    files = exec_statement("""
+    libenchant = exec_statement("""
 from enchant._enchant import e
-print(e._name)""").strip().split()
-    for f in files:  # Put the enchant library in lib/ so the enchant plugins can find it
-        binaries.append((f, 'lib'))
-    files = exec_statement("""
-from enchant import Broker
-for provider in Broker().describe():
-    print(provider.file)""").strip().split()
-    for f in files:  # Put enchant plugins in lib/enchant/ so the enchant library can find them
-        binaries.append((f, os.path.join('lib', 'enchant')))
+print(e._name)
+""").strip()
 
-    datas = []
-    files = collect_data_files('enchant')  # Only works if pyenchant is installed via pip
-    for file in files:
-        if 'share' in file[0] and not file[0].endswith('.zip') and 'man' not in file[0]:
-            datas.append((file[0], os.sep.join(file[1].split(os.sep)[1:])))
+    # Check libenchant was not installed via pip but is somewhere on disk.
+    if not libenchant.startswith(sys.prefix):
+        # 'libenchant' was not installed via pip - append it to 'binaries'.
+        binaries.append((libenchant, 'enchant'))
+
+        # Collect enchant backends from Macports.
+        # Collect all available dictionaries from Macports.
+
+#
+#     for f in files:  # Put the enchant library in lib/ so the enchant plugins can find it
+#         binaries.append((f, 'lib'))
+#     files = exec_statement("""
+# from enchant import Broker
+# for provider in Broker().describe():
+#     print(provider.file)""").strip().split()
+#     for f in files:  # Put enchant plugins in lib/enchant/ so the enchant library can find them
+#         binaries.append((f, os.path.join('lib', 'enchant')))
+#
+#     datas = []
+#     files = collect_data_files('enchant')  # Only works if pyenchant is installed via pip
+#     for file in files:
+#         if 'share' in file[0] and not file[0].endswith('.zip') and 'man' not in file[0]:
+#             datas.append((file[0], os.sep.join(file[1].split(os.sep)[1:])))
