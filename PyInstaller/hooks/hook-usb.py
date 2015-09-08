b283b2819abdf1ec93d39f4243597847bb9c2289
Hartmut Goebel <h.goebel@crazy-compilers.com>
2015-09-08 21:36:33 +0200

    Fix NameError in hook-usb.py.
    NameError: name 'bins' is not defined - in case of libname is None
    and not is_cygwin
    
    I introduced this in 292138b75.

--- PyInstaller/hooks/hook-usb.py
+++ PyInstaller/hooks/hook-usb.py
@@ -42,9 +42,12 @@ if libname is not None:
     # Use basename here because Python returns full library path
     # on Mac OSX when using ctypes.util.find_library.
     bins = [os.path.basename(libname)]
+    binaries = _resolveCtypesImports(bins)
 elif is_cygwin:
     bins = ['cygusb-1.0-0.dll', 'cygusb0.dll']
-binaries = _resolveCtypesImports(bins)[:1]  # use only the first one if any
+    binaries = _resolveCtypesImports(bins)[:1]  # use only the first one if any
+else:
+    binaries = []
 if binaries:
     # `_resolveCtypesImports` returns a 3-tuple, but `binaries` are only
     # 2-tuples, so remove the last element:
