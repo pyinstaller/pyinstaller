51cee6fdce5b32f2c3c9fb829f67a5a8dcce5d81
Martin Zibricky <mzibr.public@gmail.com>
2015-09-08 20:09:32 +0200

    Issue #967: Fix test_enchant to fail when no dictionary is available.

--- tests/functional/scripts/pyi_lib_enchant.py
+++ tests/functional/scripts/pyi_lib_enchant.py
@@ -8,7 +8,7 @@
 #-----------------------------------------------------------------------------
 
 
-# Enchant hook test.
+# Enchant hook test. Tested with PyEnchant 1.6.6.
 
 import sys
 import enchant
@@ -23,21 +23,24 @@ dicts = [x[0] for x in enchant.list_dicts()]
 if len(backends) < 1:
     raise SystemExit('Error: No dictionary backend available')
 
+# Usually en_US dictionary should be bundled.
 if len(dicts) < 1:
-    print('W: No dictionary available')
+    raise SystemExit('No dictionary available')
 
-print((80 * '-'))
-print(('PYTHONPATH: %s' % sys.path))
-print((80 * '-'))
-print(('Backends: ' + ', '.join(backends)))
-print(('Languages: %s' % ', '.join(langs)))
-print(('Dictionaries: %s' % dicts))
-print((80 * '-'))
+
+print(80 * '-')
+print('PYTHONPATH: %s' % sys.path)
+print(80 * '-')
+print('Backends: ' + ', '.join(backends))
+print('Languages: %s' % ', '.join(langs))
+print('Dictionaries: %s' % dicts)
+print(80 * '-')
+
 
 # Try spell checking if English is availale
 l = 'en_US'
 if l in langs:
     d = enchant.Dict(l)
-    print(('d.check("hallo") %s' % d.check('hallo')))
-    print(('d.check("halllo") %s' % d.check('halllo')))
-    print(('d.suggest("halllo") %s' % d.suggest('halllo')))
+    print('d.check("hallo") %s' % d.check('hallo'))
+    print('d.check("halllo") %s' % d.check('halllo'))
+    print('d.suggest("halllo") %s' % d.suggest('halllo'))
