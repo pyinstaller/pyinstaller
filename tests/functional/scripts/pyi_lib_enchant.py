830b06ed5b1a2a7f89c09a461c971ccde81de4b9
Martin Zibricky <mzibr.public@gmail.com>
2015-09-09 12:45:32 +0200

    Issue #967: Improve verbosity of test_enchant.

--- tests/functional/scripts/pyi_lib_enchant.py
+++ tests/functional/scripts/pyi_lib_enchant.py
@@ -14,24 +14,22 @@ import sys
 import enchant
 
 
-backends = [x.name for x in enchant.Broker().describe()]
-langs = enchant.list_languages()
-dicts = [x[0] for x in enchant.list_dicts()]
-
-
-# At least one backend should be available
-if len(backends) < 1:
-    raise SystemExit('Error: No dictionary backend available')
-
-# Usually en_US dictionary should be bundled.
-if len(dicts) < 1:
-    raise SystemExit('No dictionary available')
-
-
 print(80 * '-')
 print('PYTHONPATH: %s' % sys.path)
+
+# At least one backend should be available
+backends = [x.name for x in enchant.Broker().describe()]
+if len(backends) < 1:
+    raise SystemExit('Error: No dictionary backend available')
 print(80 * '-')
 print('Backends: ' + ', '.join(backends))
+
+# Usually en_US dictionary should be bundled.
+langs = enchant.list_languages()
+dicts = [x[0] for x in enchant.list_dicts()]
+if len(dicts) < 1:
+    raise SystemExit('No dictionary available')
+print(80 * '-')
 print('Languages: %s' % ', '.join(langs))
 print('Dictionaries: %s' % dicts)
 print(80 * '-')
