780d7b5aba39806655e2f96f20f247a1b6f5c997
Hartmut Goebel <h.goebel@crazy-compilers.com>
2015-09-13 03:39:43 -1000

    Make pyi_interact_pyside close the window automatically.
    This addresses #1480: Appveyor tests are waiting on something (times out).
    
    (auto-formatted to fix indentation error)

--- tests/functional/scripts/pyi_interact_pyside.py
+++ tests/functional/scripts/pyi_interact_pyside.py
@@ -14,17 +14,21 @@ import sys
 from PySide import QtCore
 from PySide import QtGui
 
-class MyDialog(QtGui.QDialog):
 
+class MyDialog(QtGui.QDialog):
     def __init__(self):
         super(MyDialog, self).__init__()
 
         self.label = QtGui.QLabel(
-            u'Press <ESC> to exit. Some non-ascii chars: řčšěíáŘ',
+            u'Press <ESC> to exit. Some non-ascii chars: řčšěíáŘ'
+            u'\nor wait some seconds',
             self)
         self.setWindowTitle('Hello World from PySide')
         self.resize(400, 200)
         self.show()
+
+        # close window after 1.5 seconds
+        QtCore.QTimer.singleShot(1500, self.close)
 
     def sizeHint(self):
         return self.label.sizeHint()
@@ -37,10 +41,11 @@ class MyDialog(QtGui.QDialog):
 def main():
     app = QtGui.QApplication(sys.argv)
     read_formats = ', '.join([str(format).lower() \
-        for format in QtGui.QImageReader.supportedImageFormats()])
+                              for format in QtGui.QImageReader.supportedImageFormats()])
     print('Qt4 plugin paths: ' + str(list(app.libraryPaths())))
     print('Qt4 image read support: ' + read_formats)
-    print('Qt4 Libraries path: ' + str(QtCore.QLibraryInfo.location(QtCore.QLibraryInfo.LibrariesPath)))
+    print('Qt4 Libraries path: ' + str(
+        QtCore.QLibraryInfo.location(QtCore.QLibraryInfo.LibrariesPath)))
     ex = MyDialog()
     app.exec_()
 
