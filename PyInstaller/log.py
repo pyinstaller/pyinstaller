6970c1cefdb21e7aa46f2cd11934bd0ed1a54b35
David Vierra <codewarrior@hawaii.rr.com>
2015-09-12 16:00:30 -1000

    Logging: Defer initialization until __process_options is called.
    This solves a problem where logging was initialized for the test suites as soon as `conftest.py` was imported, and if it was imported while pytest output capturing is not enabled, all the logging was sent to stderr instead of being captured.
    
    Now, logging is only initialized upon a call to `__main__.run()` (or to the `run()` function of the cliutils that use the logging options).
    
    Related to https://github.com/pytest-dev/pytest-xdist/issues/4

--- PyInstaller/log.py
+++ PyInstaller/log.py
@@ -18,10 +18,6 @@ __all__ = ['getLogger', 'INFO', 'WARN', 'DEBUG', 'ERROR', 'FATAL']
 import logging
 from logging import getLogger, INFO, WARN, DEBUG, ERROR, FATAL
 
-FORMAT = '%(relativeCreated)d %(levelname)s: %(message)s'
-logging.basicConfig(format=FORMAT, level=logging.INFO)
-logger = getLogger('PyInstaller')
-
 def __add_options(parser):
     levels = ('DEBUG', 'INFO', 'WARN', 'ERROR', 'CRITICAL')
     parser.add_option('--log-level',
@@ -34,8 +30,13 @@ def __add_options(parser):
                       )
 
 def __process_options(parser, opts):
+    FORMAT = '%(relativeCreated)d %(levelname)s: %(message)s'
+    logging.basicConfig(format=FORMAT, level=logging.INFO)
+    logger = getLogger('PyInstaller')
+
     try:
         level = getattr(logging, opts.loglevel.upper())
     except AttributeError:
         parser.error('Unknown log level `%s`' % opts.loglevel)
-    logger.setLevel(level)
+    else:
+        logger.setLevel(level)
