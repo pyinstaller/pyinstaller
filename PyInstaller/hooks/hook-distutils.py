62642f3bb08d5bf357524d7d2ebd5e87e879dd72
leycec <leycec@gmail.com>
2015-09-20 03:37:48 -0400

    Distutils pre-find module path hook added.
    When freezing under a venv, this hook retargets the "distutils" package from the
    venv-isolated version of that package to the system-wide version of that
    package. The "distutils" post-graph hook no longer contains such functionality.

--- PyInstaller/hooks/hook-distutils.py
+++ PyInstaller/hooks/hook-distutils.py
@@ -7,13 +7,16 @@
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
 
+"""
+`distutils`-specific post-import hook.
 
-# The 'distutils' module requires Makefile and pyconfig.h files from Python
-# installation. 'distutils.sysconfig' parses these files to get some
-# information from them.
+This hook freezes the external `Makefile` and `pyconfig.h` files bundled with
+the active Python interpreter, which the `distutils.sysconfig` module parses at
+runtime for platform-specific metadata.
+"""
+
 # TODO Verify that bundling Makefile and pyconfig.h is still required for Python 3.
 
-import distutils
 import os
 import sysconfig
 
@@ -32,15 +35,3 @@ datas = [(_CONFIG_H, relpath_to_config_or_make(_CONFIG_H))]
 # The Makefile does not exist on all platforms, eg. on Windows
 if os.path.exists(_MAKEFILE):
     datas.append((_MAKEFILE, relpath_to_config_or_make(_MAKEFILE)))
-
-def hook(mod):
-    """
-    This hook checks for the distutils hacks present when using the
-    virtualenv package.
-    """
-    # Non-empty  means PyInstaller is running inside virtualenv.
-    # Virtualenv overrides real distutils modules.
-    if hasattr(distutils, 'distutils_path'):
-        mod_path = os.path.join(distutils.distutils_path, '__init__.py')
-        mod.retarget(mod_path)
-    return mod
