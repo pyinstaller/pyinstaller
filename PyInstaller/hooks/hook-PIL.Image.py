9bd18f1931a1ddc67a56deab58cce5d2ea4019fa
Martin Zibricky <mzibr.public@gmail.com>
2015-09-23 13:59:20 +0200

    Issue #830: Remove PIL rthook - no longer necessary.

--- PyInstaller/hooks/hook-PIL.Image.py
+++ PyInstaller/hooks/hook-PIL.Image.py
@@ -22,30 +22,3 @@ excludedimports = [modname_tkinter, 'FixTk']
 # Include all PIL image plugins - module names containing 'ImagePlugin'.
 # e.g.  PIL.JpegImagePlugin
 hiddenimports = collect_submodules('PIL', pattern='ImagePlugin')
-
-
-# def hook(mod):
-#     # `PIL.Image` may be imported as `PIL.Image` or as `Image`
-#     # (without the prefix). We need to use the same module name to
-#     # avoid the same module under two different names.
-#     # We cannot import modules directly in PyInstaller.
-#     statement = """
-# import sys
-# __import__('%(modname)s')
-# image_mod = sys.modules['%(modname)s']
-# # PIL uses lazy initialization.
-# # first import the default stuff ...
-# image_mod.preinit()
-# # ... then every available plugin
-# image_mod.init()
-# for name in sys.modules:
-#     if name.endswith('ImagePlugin'):
-#         # Modules are printed to stdout and the output is then parsed.
-#         print(name)
-# """ % {'modname': mod.name}
-#     out = hookutils.exec_statement(statement)
-#     mod.add_import(out.strip().splitlines())
-#
-#     mod.del_import('FixTk')
-#
-#     return mod
