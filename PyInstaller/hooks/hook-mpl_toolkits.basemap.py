9e98530a6b79f6da493b34a3f8185600003c992d
giumas <giumas@yahoo.it>
2015-09-24 12:05:54 -0400

    Add hook for `mpl_toolkits.basemap` that is shipped with auxiliary data to plot maps

---
+++ PyInstaller/hooks/hook-mpl_toolkits.basemap.py
@@ -0,0 +1,16 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2015, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+
+from PyInstaller.utils.hooks import (collect_data_files)
+
+
+# mpl_toolkits.basemap (tested with v.1.0.7) is shipped with auxiliary data,
+# usually stored in mpl_toolkits\basemap\data and used to plot maps
+datas = collect_data_files('mpl_toolkits.basemap')
