ba213e043a0fdb8cf57fee6cdc38863c4efed903
Jonathan Springer <springermac@gmail.com>
2015-09-24 13:51:36 -0400

    Add copyright headers and comments to hooks

--- PyInstaller/hooks/graph/hook-gi.repository.GLib.py
+++ PyInstaller/hooks/graph/hook-gi.repository.GLib.py
@@ -1,5 +1,16 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2005-2015, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
 from PyInstaller.lib.modulegraph.modulegraph import RuntimeModule
 
 
 def hook(module_graph):
+    # PyGObject modules loaded through the gi repository are marked as MissingModules by modulegraph
+    # so we convert them to RuntimeModules so their hooks are loaded and run.
     module_graph.add_module(RuntimeModule('gi.repository.GLib'))
---
+++ test.py
@@ -0,0 +1,4 @@
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import Gst
+print(Gst)
