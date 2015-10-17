f9e6ef692e73a4c9c3b377012f9f36e9e5e6a189
Hartmut Goebel <h.goebel@crazy-compilers.com>
2015-10-17 16:50:17 +0200

    Issue #1564: Move cli-utils to base dir and eliminate need to extend path.

---
+++ archive_viewer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013-2015 PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+# This utility is primary meant to be used when PyInstaller is not
+# installed, eg. when be run by a git checkout.
+
+from PyInstaller.utils.cliutils.archive_viewer import run
+run()
