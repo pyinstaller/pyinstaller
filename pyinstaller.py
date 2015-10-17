5a9a83e63503bcefc7919d2e6e7b365b2a52d9de
Hartmut Goebel <h.goebel@crazy-compilers.com>
2015-10-17 16:50:58 +0200

    Align pyinstaller.py with the other scripts.

--- pyinstaller.py
+++ pyinstaller.py
@@ -1,6 +1,6 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 #-----------------------------------------------------------------------------
-# Copyright (c) 2013, PyInstaller Development Team.
+# Copyright (c) 2013-2015, PyInstaller Development Team.
 #
 # Distributed under the terms of the GNU General Public License with exception
 # for distributing bootloader.
@@ -8,11 +8,8 @@
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
 
+# This utility is primary meant to be used when PyInstaller is not
+# installed, eg. when be run by a git checkout.
 
-"""
-Main command-line interface to PyInstaller.
-"""
-
-if __name__ == '__main__':
-    from PyInstaller.__main__ import run
-    run()
+from PyInstaller.__main__ import run
+run()
