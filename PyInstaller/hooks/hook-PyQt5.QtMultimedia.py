3133d565f782d2a35a148a7fb11f01e744ce2f81
Nat Picker <tallforasmurf@yahoo.com>
2015-10-23 13:09:44 -0700

    Fix issue #1625, provide hook for PyQt5.QtMultimedia

---
+++ PyInstaller/hooks/hook-PyQt5.QtMultimedia.py
@@ -0,0 +1,20 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+# QtMultimedia tries to pull in QtNetwork
+
+hiddenimports = [ 'PyQt5.QtNetwork' ]
+
+# QtMultimedia needs some plugins
+
+from PyInstaller.utils.hooks import qt5_plugins_binaries
+
+binaries = []
+binaries.extend( qt5_plugins_binaries( 'audio' ) )
+binaries.extend( qt5_plugins_binaries( 'mediaservice' ) )
