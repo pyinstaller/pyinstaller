267ef98aeab5dc56ee8357c926762e844ec15a90
Martin Zibricky <mzibr.public@gmail.com>
2015-10-31 20:59:09 +0100

    Issue #1643: Collect data files for hook-IPython.

--- PyInstaller/hooks/hook-IPython.py
+++ PyInstaller/hooks/hook-IPython.py
@@ -10,9 +10,13 @@
 
 # Tested with IPython 4.0.0.
 
+from PyInstaller.utils.hooks import collect_data_files
+
 # Ignore 'matplotlib'. IPython contains support for matplotlib.
 # Ignore GUI libraries. IPython supports integration with GUI frameworks.
 # Assume that it will be imported by any other module when the user really
 # uses it.
 # TODO IPython uses 'tkinter' for clipboard acces on Linux/Unix. Ignore it conditionally.
 excludedimports = ['gtk', 'matplotlib', 'PyQt4', 'PyQt5', 'PySide']
+
+datas = collect_data_files('IPython')
