6bcc11cf2aac992a0004348c4e9892b6fd8070b1
Bryan A. Jones <bjones AT ece.msstate.edu>
2015-12-04 10:27:45 -0600

    Add: Test case showing wxPython import failure. See issue #1704.

---
+++ tests/functional/modules/pyi_testmod_path/__init__.py
@@ -0,0 +1,13 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2015, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+import os.path
+# Insert a/ at the beginning of __path__.
+__path__.insert(0, os.path.join(__path__[0], 'a'))
+# Import b, which should now find a/b.py, no ./b.py.
+import b
