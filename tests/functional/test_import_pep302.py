1183a1e6584de11465cd67bf46ebc7b7094b7fed
Hartmut Goebel <h.goebel@crazy-compilers.com>
2015-12-06 13:18:42 +0100

    Port old PEP 302 test cases to new test framework - and fix them.
    The tests did not test what was expected to be tested, since they
    imported the resp. modules prior to using the `pkgutil`-functions. Thus
    `pkgutil` did take a different execution-branch.
    
    Thanks to Dustin Spicuzza for his test-cases, see issue #1689.

---
+++ tests/functional/test_import_pep302.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+#-----------------------------------------------------------------------------
+# Copyright (c) 2005-2015, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+import pytest
+
+# PEP-302 import hooks specification contain section 'Optional
+# Extensions to the Importer Protocol'
+#
+# This section is meant to be optional but the reality is different.
+# Some Python modules (e.g. Flask) depends on implementation of these
+# optional functions:
+#
+#   loader.is_package(fullmodname)
+#   loader.get_code(fullmodname)
+#   loader.get_source(fullmodname)
+#
+# This test-cases test the return values of these functions for
+# importers from pyimod03_importers module.
+
+# Note: The modules should be at the end of the resp. code. Otherwise
+#       pkgutil-functions take a fery different branch (since the
+#       module is already in sys.modules).
+
+
+def test_pep302_loader_builtin(pyi_builder):
+    pyi_builder.test_source(
+        """
+        mod = 'sys'
+        import pkgutil
+        ldr = pkgutil.get_loader(mod)
+        assert ldr
+        assert ldr.is_package(mod) == False
+        assert ldr.get_code(mod) is None
+        assert ldr.get_source(mod) is None
+        """)
+
+
+def test_pep302_loader_frozen_module(pyi_builder):
+    pyi_builder.test_source(
+        """
+        mod = 'compileall'
+        import pkgutil
+        ldr = pkgutil.get_loader(mod)
+        assert ldr
+        assert ldr.is_package(mod) == False
+        assert ldr.get_code(mod) is not None
+        assert ldr.get_source(mod) is None
+        # Import at the very end, just to get the module frozen.
+        import compileall
+        """)
+
+
+def test_pep302_loader_frozen_package(pyi_builder):
+    pyi_builder.test_source(
+        """
+        mod = 'distutils'
+        import pkgutil
+        ldr = pkgutil.get_loader(mod)
+        assert ldr
+        assert ldr.is_package(mod) == True
+        assert ldr.get_code(mod) is not None
+        assert ldr.get_source(mod) is None
+        # Import at the very end, just to get the module frozen.
+        import distutils
+        """)
+
+
+def test_pep302_loader_frozen_submodule(pyi_builder):
+    pyi_builder.test_source(
+        """
+        mod = 'distutils.config'
+        import pkgutil
+        ldr = pkgutil.get_loader(mod)
+        assert ldr
+        assert ldr.is_package(mod) == False
+        assert ldr.get_code(mod) is not None
+        assert ldr.get_source(mod) is None
+        # Import at the very end, just to get the module frozen.
+        import distutils.config
+        """)
+
+
+def test_pep302_loader_cextension(pyi_builder):
+    pyi_builder.test_source(
+        """
+        mod = '_sqlite3'
+        import pkgutil
+        ldr = pkgutil.get_loader(mod)
+        assert ldr
+        assert ldr.is_package(mod) == False
+        assert ldr.get_code(mod) is None
+        assert ldr.get_source(mod) is None
+        # Import at the very end, just to get the module frozen.
+        import sqlite3
+        """)
