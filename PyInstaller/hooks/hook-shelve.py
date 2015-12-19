a640f42a4f94eb20ec0f3e9a25cb00ce60d1cab3
Penaz <Penaz91@users.noreply.github.com>
2015-12-19 21:23:57 +0100

    Update hook-shelve.py

--- PyInstaller/hooks/hook-shelve.py
+++ PyInstaller/hooks/hook-shelve.py
@@ -1,3 +1,12 @@
 # Hidden Imports for Shelve Module
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+# Tested on Windows 7 with Python 3.5
 
 hiddenimports = ["dbm.ndbm", "dbm.dumb", "dbm.gnu"]
