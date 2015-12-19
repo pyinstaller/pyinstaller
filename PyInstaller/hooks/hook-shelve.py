aa3a68bf8a31f8d8228ba584f01d1fed4b1c91b9
Penaz <Penaz91@users.noreply.github.com>
2015-12-19 22:20:56 +0100

    Update hook-shelve.py

--- PyInstaller/hooks/hook-shelve.py
+++ PyInstaller/hooks/hook-shelve.py
@@ -6,5 +6,6 @@
 #
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
+# Tested on Windows 7 x64 With Pyhton 3.5
 
 hiddenimports = ["dbm.ndbm", "dbm.dumb", "dbm.gnu"]
