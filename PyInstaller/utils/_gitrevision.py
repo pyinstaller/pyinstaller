83d3ffaf7c1bcb12a62d70243ac8277a3ce2c1ae
Hartmut Goebel <h.goebel@crazy-compilers.com>
2015-12-24 14:11:46 +0100

    Add comments.

--- PyInstaller/utils/_gitrevision.py
+++ PyInstaller/utils/_gitrevision.py
@@ -1,6 +1,11 @@
-rev = "$Format:%h$"
-commit = "$Format:%H$"
-date = "$Format:%ci$"
+#
+# The content of this file will be filled in with meaningful data
+# when creating an archive using `git archive` or by downloading an
+# archive from github, e.g. from github.com/.../archive/develop.zip
+#
+rev = "$Format:%h$"     # abbreviated commit hash
+commit = "$Format:%H$"  # commit hash
+date = "$Format:%ci$"   # commit date
 author = "$Format:%an$ <$Format:%ae$>"
-ref_names = "$Format:%D$"
+ref_names = "$Format:%D$"  # incl. current branch
 commit_message = """$Format:%B$"""
