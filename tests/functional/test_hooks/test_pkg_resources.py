41f07eb9a071cb553262527397f8e5c7f1a459c0
Hartmut Goebel <h.goebel@crazy-compilers.com>
2016-01-26 11:37:48 +0100

    Cleanup: Move small test-script into the test module.

--- tests/functional/test_hooks/test_pkg_resources.py
+++ tests/functional/test_hooks/test_pkg_resources.py
@@ -5,4 +5,8 @@ def test_pkg_resources_importable(pyi_builder):
     """
     Check that a trivial example using pkg_resources does build.
     """
-    pyi_builder.test_script('pyi_hooks/use_pkg_resources.py')
+    pyi_builder.test_source(
+        """
+        import pkg_resources
+        pkg_resources.working_set.require()
+        """)
