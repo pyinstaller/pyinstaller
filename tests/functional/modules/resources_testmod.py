6e5c489aded925100e18ea1e44a8d4e9f00f99a8
Hartmut Goebel <h.goebel@crazy-compilers.com>
2016-01-29 16:28:45 +0100

    Issue #1780: Create functional test for basemodules regex.
    # Conflicts:
    #	tests/functional/test_basic.py

---
+++ tests/functional/modules/resources_testmod.py
@@ -0,0 +1,14 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2005-2016, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+"""
+This is a module whose name begins with 're'. It will be used to verify that the
+regex for excluding modules listed in PY3_BASE_MODULES (which includes 're') does
+not exclude this module.
+"""
