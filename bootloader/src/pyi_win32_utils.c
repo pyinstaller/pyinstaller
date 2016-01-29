cf5b60f38591bb11ff3f47ef0cddc8f8ce402c60
David Vierra <codewarrior@hawaii.rr.com>
2016-01-29 09:04:58 -1000

    Issue #1799: Fix fallback when GetShortPathNameW fails

--- bootloader/src/pyi_win32_utils.c
+++ bootloader/src/pyi_win32_utils.c
@@ -201,7 +201,7 @@ char * pyi_win32_wcs_to_mbs_sfn(const wchar_t *wstr) {
         VS("Failed to get short path name for filename. GetShortPathNameW: \n%s",
                    GetWinErrorString()
                    );
-        str = pyi_win32_wcs_to_mbs(wstr_sfn);
+        str = pyi_win32_wcs_to_mbs(wstr);
     }
     return str;
 }
