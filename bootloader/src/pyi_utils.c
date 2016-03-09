16b8cc8dde1a7a5549828d0bb27078045ec21b29
Kenneth Zhao <kenneth.zhao@gmail.com>
2016-03-09 08:47:54 -0800

    Add new lines to EOF in order to compile on RedHat 5 derivative systems.  In addition, check fwrite() return to make the GCC happy.

--- bootloader/src/pyi_utils.c
+++ bootloader/src/pyi_utils.c
@@ -444,8 +444,8 @@ int pyi_copy_file(const char *src, const char *dst, const char *filename)
                 break;
             }
         } else {
-            fwrite(buf, 4096, 1, out);
-            if (ferror(out)) {
+            int rc = fwrite(buf, 4096, 1, out);
+            if (rc <= 0 || ferror(out)) {
                 clearerr(out);
                 error = -1;
                 break;
