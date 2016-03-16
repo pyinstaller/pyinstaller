3a037735dddc13f91c0853ae4980adb4482df3ba
Jonathan Springer <springermac@gmail.com>
2016-03-15 20:03:46 -0400

    Fix osx_overide_info_plist test
    Re add removed file need for osx_override_info_plist test

---
+++ tests/functional/scripts/pyi_helloworld.py
@@ -0,0 +1,11 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2015-2016, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+# ----------------------------------------------------------------------------
+
+
+print('Hello Python!')
