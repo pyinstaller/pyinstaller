b01b8b56f64ee73eddfa51d85718f23f1e414927
Jonathan Springer <springermac@gmail.com>
2016-03-23 16:56:34 -0400

    Split hook utils into separate modules

---
+++ PyInstaller/utils/hooks/django.py
@@ -0,0 +1,72 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2005-2016, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+# ----------------------------------------------------------------------------
+import os
+
+from ..hooks import eval_script
+from ...utils import misc
+
+
+def django_dottedstring_imports(django_root_dir):
+    """
+    Get all the necessary Django modules specified in settings.py.
+
+    In the settings.py the modules are specified in several variables
+    as strings.
+    """
+    pths = []
+    # Extend PYTHONPATH with parent dir of django_root_dir.
+    pths.append(misc.get_path_to_toplevel_modules(django_root_dir))
+    # Extend PYTHONPATH with django_root_dir.
+    # Many times Django users do not specify absolute imports in the settings module.
+    pths.append(django_root_dir)
+
+    package_name = os.path.basename(django_root_dir) + '.settings'
+    env = {'DJANGO_SETTINGS_MODULE': package_name,
+           'PYTHONPATH': os.pathsep.join(pths)}
+    ret = eval_script('django_import_finder.py', env=env)
+
+    return ret
+
+
+def django_find_root_dir():
+    """
+    Return path to directory (top-level Python package) that contains main django
+    files. Return None if no directory was detected.
+
+    Main Django project directory contain files like '__init__.py', 'settings.py'
+    and 'url.py'.
+
+    In Django 1.4+ the script 'manage.py' is not in the directory with 'settings.py'
+    but usually one level up. We need to detect this special case too.
+    """
+    # 'PyInstaller.config' cannot be imported as other top-level modules.
+    from ...config import CONF
+    # Get the directory with manage.py. Manage.py is supplied to PyInstaller as the
+    # first main executable script.
+    manage_py = CONF['main_script']
+    manage_dir = os.path.dirname(os.path.abspath(manage_py))
+
+    # Get the Django root directory. The directory that contains settings.py and url.py.
+    # It could be the directory containing manage.py or any of its subdirectories.
+    settings_dir = None
+    files = set(os.listdir(manage_dir))
+    if 'settings.py' in files and 'urls.py' in files:
+        settings_dir = manage_dir
+    else:
+        for f in files:
+            if os.path.isdir(os.path.join(manage_dir, f)):
+                subfiles = os.listdir(os.path.join(manage_dir, f))
+                # Subdirectory contains critical files.
+                if 'settings.py' in subfiles and 'urls.py' in subfiles:
+                    settings_dir = os.path.join(manage_dir, f)
+                    break  # Find the first directory.
+
+    return settings_dir
+
+__all__ = ('django_dottedstring_imports', 'django_find_root_dir')
---
+++ PyInstaller/utils/hooks/win32.py
@@ -0,0 +1,54 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2005-2016, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+# ----------------------------------------------------------------------------
+from ..hooks import exec_statement
+
+# NOTE: This function requires PyInstaller to be on the default "sys.path" for
+# the called Python process. Running py.test changes the working dir to a temp
+# dir, so PyInstaller should be installed via either "setup.py install" or
+# "setup.py develop" before running py.test.
+
+
+def get_pywin32_module_file_attribute(module_name):
+    """
+    Get the absolute path of the PyWin32 DLL specific to the PyWin32 module
+    with the passed name.
+
+    On import, each PyWin32 module:
+
+    * Imports a DLL specific to that module.
+    * Overwrites the values of all module attributes with values specific to
+      that DLL. This includes that module's `__file__` attribute, which then
+      provides the absolute path of that DLL.
+
+    This function safely imports that module in a PyWin32-aware subprocess and
+    returns the value of that module's `__file__` attribute.
+
+    Parameters
+    ----------
+    module_name : str
+        Fully-qualified name of that module.
+
+    Returns
+    ----------
+    str
+        Absolute path of that DLL.
+
+    See Also
+    ----------
+    `PyInstaller.utils.win32.winutils.import_pywin32_module()`
+        For further details.
+    """
+    statement = """
+        from PyInstaller.utils.win32 import winutils
+        module = winutils.import_pywin32_module('%s')
+        print(module.__file__)
+    """
+    return exec_statement(statement % module_name)
+
+__all__ = ('get_pywin32_module_file_attribute', )
