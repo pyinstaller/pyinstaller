8b28f584abe2430170dba8777762756b4838fe26
Hartmut Goebel <h.goebel@crazy-compilers.com>
2016-03-27 21:12:21 +0200

    Include a own CSS stylesheet.

--- doc/conf.py
+++ doc/conf.py
@@ -112,6 +112,8 @@ todo_include_todos = False
 html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
 
+html_style = 'css/pyinstaller.css'
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
