3f96dde4315115544ef0508e6f723694cc70f3ef
Hartmut Goebel <h.goebel@crazy-compilers.com>
2016-03-27 19:45:41 +0200

    Add logo and favicon.

--- doc/conf.py
+++ doc/conf.py
@@ -128,12 +128,12 @@ html_theme = 'alabaster'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = '_static/pyinstaller-draft1a-100_trans.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = '_static/pyinstaller-draft1a.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
