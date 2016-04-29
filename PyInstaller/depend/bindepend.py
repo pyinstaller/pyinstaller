e78bb550f113a0a032b9d3a94335568692472c4f
Mansour Moufid <mansourmoufid@gmail.com>
2016-04-29 19:07:53 -0400

    Replace run-time search path keywords.

--- PyInstaller/depend/bindepend.py
+++ PyInstaller/depend/bindepend.py
@@ -626,9 +626,14 @@ def _getImports_macholib(pth):
                 # Remove trailing '\x00' characters.
                 # e.g. '../lib\x00\x00'
                 rpath = rpath.rstrip('\x00')
+                # Replace the @executable_path and @loader_path keywords
+                # with the actual path to the binary.
+                executable_path = os.path.dirname(pth)
+                rpath = rpath.replace('@executable_path', executable_path)
+                rpath = rpath.replace('@loader_path', executable_path)
                 # Make rpath absolute. According to Apple doc LC_RPATH
                 # is always relative to the binary location.
-                rpath = os.path.normpath(os.path.join(os.path.dirname(pth), rpath))
+                rpath = os.path.normpath(os.path.join(executable_path, rpath))
                 run_paths.update([rpath])
             else:
                 # Frameworks that have this structure Name.framework/Versions/N/Name
