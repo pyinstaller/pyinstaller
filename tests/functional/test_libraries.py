4814b2e6112942692801a29bf0845e243d094a45
David Vierra <codewarrior@hawaii.rr.com>
2016-04-30 11:36:43 -1000

    Issue #1935: Add test case exercising much of the `future` module
    Test code taken from examples in `future` documentation.

--- tests/functional/test_libraries.py
+++ tests/functional/test_libraries.py
@@ -77,6 +77,12 @@ def test_cherrypy(pyi_builder):
 def test_enchant(pyi_builder):
     pyi_builder.test_script('pyi_lib_enchant.py')
 
+
+@skipif(is_py3, reason="Only tests Python 2.7 feature")
+def test_future(pyi_builder):
+    pyi_builder.test_script('pyi_future.py')
+
+
 @skipif(is_py3, reason="Only tests Python 2.7 feature")
 def test_future_queue(pyi_builder):
     pyi_builder.test_source(
@@ -85,6 +91,7 @@ def test_future_queue(pyi_builder):
         queue.Queue()
         """
     )
+
 
 @importorskip('gevent')
 def test_gevent(pyi_builder):
