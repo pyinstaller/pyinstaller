fa9dffcd24b68f878a5f79a3e848a84ef8f87791
Jonathan Stewmon <jstewmon@rmn.com>
2016-06-30 00:17:05 -0500

    use pv to show progress in case binary install takes > 10 minutes

--- travis-build.sh
+++ travis-build.sh
@@ -8,15 +8,15 @@ envsubst '$PY_VERSION' <Dockerfile.tmpl > Dockerfile
 docker build -t pyinstaller .
 
 docker run --rm -i \
-  -e PIP_ACCEL_LOG_FORMAT \
-  -v $HOME/.cache/pip:/root/.cache/pip \
-  -v $HOME/.pip-accel:/var/cache/pip-accel \
-  pyinstaller bash - <<EOF
-    set -e
-    (cd bootloader && python waf distclean all --no-lsb)
-    pip install -e . | cat
-    pip install pip-accel | cat
-    pip-accel install -r tests/requirements-tools.txt | cat
-    pip-accel install -r tests/requirements-linux.txt | cat
-    py.test -n 5 --maxfail 3 tests/unit/ tests/
+    -e PIP_ACCEL_LOG_FORMAT \
+    -v $HOME/.cache/pip:/root/.cache/pip \
+    -v $HOME/.pip-accel:/var/cache/pip-accel \
+    pyinstaller bash - <<EOF
+        set -e
+        (cd bootloader && python waf distclean all --no-lsb)
+        pip install -e . | cat
+        pip install pip-accel | cat
+        pip-accel install -r tests/requirements-tools.txt | cat
+        pip-accel install -r tests/requirements-linux.txt | pv -ft -i 60
+        py.test -n 5 --maxfail 3 tests/unit/ tests/
 EOF
