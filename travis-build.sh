c79e79114783baa6f372cf1641bf9400c9238cb7
Jonathan Stewmon <jstewmon@rmn.com>
2016-06-30 13:12:25 -0500

    use docker cp to mitigate pip cache ownership issues
    travis will change the owner of the cache dir back to the travis
    user, preventing container's pip from using the cache

--- travis-build.sh
+++ travis-build.sh
@@ -5,18 +5,48 @@ find . -name '*.pyc' -delete
 
 envsubst '$PY_VERSION' <Dockerfile.tmpl > Dockerfile
 
-docker build -t pyinstaller .
+echo 'building pyinstaller image'
+docker build --pull -t pyinstaller .
 
-docker run --rm -i \
+echo 'creating pyinstaller container'
+2>/dev/null docker rm -v pyinstaller || true
+docker create -i --name pyinstaller \
     -e PIP_ACCEL_LOG_FORMAT \
-    -v $HOME/.cache/pip:/root/.cache/pip \
-    -v $HOME/.pip-accel:/var/cache/pip-accel \
-    pyinstaller bash - <<EOF
-        set -e
-        (cd bootloader && python waf distclean all --no-lsb)
-        pip install -e . | cat
-        pip install pip-accel | cat
-        pip-accel install -r tests/requirements-tools.txt | cat
-        pip-accel install -r tests/requirements-linux.txt | pv -ft -i 60
-        py.test -n 5 --maxfail 3 tests/unit/ tests/
+    pyinstaller bash -
+
+trap 'docker rm -v pyinstaller' EXIT
+
+echo 'initializing container cache dirs'
+docker start -i pyinstaller < <(echo 'mkdir -p /root/.cache/pip /var/cache/pip-accel')
+
+echo 'loading pip cache'
+docker cp $HOME/.cache/pip/ pyinstaller:/root/.cache/
+
+echo 'loading pip-accel cache'
+docker cp $HOME/.pip-accel/. pyinstaller:/var/cache/pip-accel/
+
+docker start -i pyinstaller <<EOF
+    set -e
+    (cd bootloader && python waf distclean all --no-lsb)
+
+    echo 'installing pyinstaller'
+    pip install -e . | cat
+
+    echo 'installing pip-accel'
+    pip install pip-accel | cat
+
+    echo 'installing requirements-tools'
+    pip-accel install -r tests/requirements-tools.txt | cat
+
+    echo 'installing requirements-linux'
+    pip-accel install -r tests/requirements-linux.txt | pv -ft -i 60
+
+    echo 'running tests'
+    # py.test -n 5 --maxfail 3 tests/unit/ tests/
 EOF
+
+echo 'extracting pip cache'
+docker cp pyinstaller:/root/.cache/pip/ $HOME/.cache/
+
+echo 'extracting pip-accel cache'
+docker cp pyinstaller:/var/cache/pip-accel/. $HOME/.pip-accel/
