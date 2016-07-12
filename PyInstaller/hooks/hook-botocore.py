fce9bf3361ff96c5ae7fe9051cf55366e611d7d3
Jesse Suen <jessesuen@gmail.com>
2016-07-11 19:05:22 -0700

    Issue #2094: Fix hook for botocore

--- PyInstaller/hooks/hook-botocore.py
+++ PyInstaller/hooks/hook-botocore.py
@@ -16,8 +16,14 @@
 #
 # https://botocore.readthedocs.org/en/latest/
 #
-# Tested with botocore 1.3.0
+# Tested with botocore 1.4.36
 
 from PyInstaller.utils.hooks import collect_data_files
+from PyInstaller.compat import is_py2
+
+if is_py2:
+    hiddenimports = ['HTMLParser']
+else:
+    hiddenimports = ['html.parser']
 
 datas = collect_data_files('botocore')
