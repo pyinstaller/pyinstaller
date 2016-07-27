39ad4bc0df88d4039aa0ac4481458b06e24aa3b6
Bruno Oliveira <nicoddemus@gmail.com>
2016-07-26 22:12:30 -0300

    Add hook for pytest modules

---
+++ tests/functional/test_hooks/test_pytest.py
@@ -0,0 +1,12 @@
+
+
+def test_pytest_runner(pyi_builder):
+    """
+    Check if pytest runner builds correctly.
+    """
+    pyi_builder.test_source(
+        """
+        import pytest
+        import sys
+        sys.exit(pytest.main(['--help']))
+        """)
