8e3e6a51946d3ad309d128f8cbb6ec74284266fd
Bryan A. Jones <bjones@ece.msstate.edu>
2016-08-19 15:57:55 -0500

    Fix: Eliminate run-time hook by placing Qt files in the correct directory.

--- PyInstaller/hooks/hook-PyQt5.py
+++ PyInstaller/hooks/hook-PyQt5.py
@@ -6,25 +6,18 @@
 #
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
-
-
 import os
 
-from PyInstaller.utils.hooks import (
-    get_module_attribute, is_module_satisfies, qt_menu_nib_dir)
-from PyInstaller.compat import getsitepackages, is_darwin, is_win
+from PyInstaller.utils.hooks import (get_module_attribute,
+    is_module_satisfies, qt_menu_nib_dir, collect_data_files)
+from PyInstaller.compat import is_darwin
 
-
-# On Windows system PATH has to be extended to point to the PyQt5 directory.
-# The PySide directory contains Qt dlls. We need to avoid including different
-# version of Qt libraries when there is installed another application (e.g. QtCreator)
-if is_win:
-    from PyInstaller.utils.win32.winutils import extend_system_path
-    extend_system_path([os.path.join(x, 'PyQt5') for x in getsitepackages()])
-
-
-# In the new consolidated mode any PyQt depends on _qt
+# In the new consolidated mode any PyQt depends on Qt.
 hiddenimports = ['sip', 'PyQt5.Qt']
+
+# Collect just the qt.conf file.
+datas = [x for x in collect_data_files('PyQt5', False, os.path.join('Qt', 'bin')) if
+         x[0].endswith('qt.conf')]
 
 
 # For Qt<5.4 to work on Mac OS X it is necessary to include `qt_menu.nib`.
