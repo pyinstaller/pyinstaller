026bf39f9ff449db4a827e05183000cac9641923
Charles Duffy <charles@dyfis.net>
2016-08-26 16:12:48 -0500

    Hooks: Add hook for pymssql.
    Honor dependency on `_mssql` module.
    
    [skip ci] no test-case for this.

---
+++ PyInstaller/hooks/hook-pymssql.py
@@ -0,0 +1,10 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2017, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+hiddenimports = ['_mssql']
