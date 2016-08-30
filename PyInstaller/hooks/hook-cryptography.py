0729a2b61f700edddbf061ec843faf2689a1909c
Stephen Rauch <stephen.rauch+github@gmail.com>
2016-08-29 18:41:04 -0700

    cryptography module missing backend

--- PyInstaller/hooks/hook-cryptography.py
+++ PyInstaller/hooks/hook-cryptography.py
@@ -17,9 +17,16 @@ import glob
 
 from PyInstaller.compat import EXTENSION_SUFFIXES
 from PyInstaller.utils.hooks import collect_submodules, get_module_file_attribute
+from PyInstaller.utils.hooks import copy_metadata
+
+# get the package data so we can load the backends
+datas = copy_metadata('cryptography')
+
+# Add the backends as hidden imports
+hiddenimports = collect_submodules('cryptography.hazmat.backends')
 
 # Add the OpenSSL FFI binding modules as hidden imports
-hiddenimports = collect_submodules('cryptography.hazmat.bindings.openssl') + ['_cffi_backend']
+hiddenimports += collect_submodules('cryptography.hazmat.bindings.openssl') + ['_cffi_backend']
 
 
 # Include the cffi extensions as binaries in a subfolder named like the package.
