7527baf903ae43c450c7dc9b638b20b48a19b468
Cheain <842217191@qq.com>
2016-09-10 01:23:30 +0800

    make versioninfo.py is suit for Python3.x
    make versioninfo.py is suit for Python3.x. make command
    'pyi-grab_version' is useful for Python3.x

--- PyInstaller/utils/cliutils/grab_version.py
+++ PyInstaller/utils/cliutils/grab_version.py
@@ -35,7 +35,7 @@ def run():
         import PyInstaller.utils.win32.versioninfo
         vs = PyInstaller.utils.win32.versioninfo.decode(args.exe_file)
         fp = codecs.open(args.out_filename, 'w', 'utf-8')
-        fp.write(unicode(vs))
+        fp.write(str(vs))
         fp.close()
         print(('Version info written to: %s' % args.out_filename))
     except KeyboardInterrupt:
