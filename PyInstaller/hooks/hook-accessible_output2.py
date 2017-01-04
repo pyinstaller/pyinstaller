13945895b9a44749d74ac4b4df9242f874b899c8
Chris Norman <chris.norman2@googlemail.com>
2017-01-04 11:09:33 +0000

    Added copyright notice to hook-accessible_output2.

--- PyInstaller/hooks/hook-accessible_output2.py
+++ PyInstaller/hooks/hook-accessible_output2.py
@@ -1,3 +1,12 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013-2016, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
 """
 accessible_output2: http://hg.q-continuum.net/accessible_output2
 """
