8f0fafc0df19c2c652eed1dfb6113c91e6d6beec
y2kbugger <git@y2kbugger.com>
2017-01-05 13:54:06 -0500

    use smartypants

--- doc/conf.py
+++ doc/conf.py
@@ -193,7 +193,7 @@ html_static_path = ['_static']
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
-#html_use_smartypants = False
+#html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
 #html_sidebars = {}
