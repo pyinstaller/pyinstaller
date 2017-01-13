702c9c2f5419e8a3ce2a6effc71431fa93ff9ea8
xoviat <email@system.com>
2017-01-13 09:14:08 -0600

    Loader: Make FrozenImporter comply to PEP 451 (MakeSpec).
    This implements PEP 451, the MakeSpec type import system, for FrozenImporter.
    Adds all three methods defined in PEP 451: find_spec, create_module and
    exec_module.
    
    Co-Authored-By: Hartmut Goebel <h.goebel@crazy-compilers.com>

--- PyInstaller/loader/pyimod03_importers.py
+++ PyInstaller/loader/pyimod03_importers.py
@@ -468,7 +468,7 @@ class FrozenImporter(object):
         module.__file__ (or pkg.__path__ items)
         """
         assert path.startswith(SYS_PREFIX + pyi_os_path.os_sep)
-        fullname = path[len(SYS_PREFIX)+1:]
+        fullname = path[SYS_PREFIXLEN+1:]
         if fullname in self.toc:
             # If the file is in the archive, return this
             return self._pyz_archive.extract(fullname)[1]
@@ -497,6 +497,135 @@ class FrozenImporter(object):
             filename = pyi_os_path.os_path_join(SYS_PREFIX,
                 fullname.replace('.', pyi_os_path.os_sep) + '.pyc')
         return filename
+
+    def find_spec(self, fullname, path=None, target=None):
+        """
+        PEP-451 finder.find_spec() method for the ``sys.meta_path`` hook.
+
+        fullname     fully qualified name of the module
+        path         None for a top-level module, or package.__path__ for
+                     submodules or subpackages.
+        target       unused by this Finder
+
+        Finders are still responsible for identifying, and typically creating,
+        the loader that should be used to load a module. That loader will now
+        be stored in the module spec returned by find_spec() rather than
+        returned directly. As is currently the case without the PEP-452, if a
+        loader would be costly to create, that loader can be designed to defer
+        the cost until later.
+
+        Finders must return ModuleSpec objects when find_spec() is called.
+        This new method replaces find_module() and find_loader() (in the
+        PathEntryFinder case). If a loader does not have find_spec(),
+        find_module() and find_loader() are used instead, for
+        backward-compatibility.
+        """
+        entry_name = None  # None means - no module found in this importer.
+
+        if fullname in self.toc:
+            entry_name = fullname
+            trace("import %s # PyInstaller PYZ", fullname)
+        elif path is not None:
+            # Try to handle module.__path__ modifications by the modules themselves
+            # Reverse the fake __path__ we added to the package module to a
+            # dotted module name and add the tail module from fullname onto that
+            # to synthesize a new fullname
+            modname = fullname.rsplit('.')[-1]
+
+            for p in path:
+                p = p[SYS_PREFIXLEN+1:]
+                parts = p.split(pyi_os_path.os_sep)
+                if not parts: continue
+                if not parts[0]:
+                    parts = parts[1:]
+                parts.append(modname)
+                entry_name = ".".join(parts)
+                if entry_name in self.toc:
+                    trace("import %s as %s # PyInstaller PYZ (__path__ override: %s)",
+                          entry_name, fullname, p)
+                    break
+            else:
+                entry_name = None
+
+        if entry_name is None:
+            trace("# %s not found in PYZ", fullname)
+            return None
+
+        # origin has to be the filename
+        origin = self.get_filename(entry_name)
+        is_pkg = self.is_package(entry_name)
+
+        spec =  _frozen_importlib.ModuleSpec(
+            fullname, self,
+            is_package=is_pkg, origin=origin,
+            # Provide the entry_name for the loader to use during loading
+            loader_state = entry_name)
+
+        # Make the import machinery set __file__.
+        # PEP 451 says: "has_location" is true if the module is locatable. In
+        # that case the spec's origin is used as the location and __file__ is
+        # set to spec.origin. If additional location information is required
+        # (e.g. zipimport), that information may be stored in
+        # spec.loader_state.
+        spec.has_location = True
+        return spec
+
+    def create_module(self, spec):
+        """
+        PEP-451 loader.create_module() method for the ``sys.meta_path`` hook.
+
+        Loaders may also implement create_module() that will return a new
+        module to exec. It may return None to indicate that the default module
+        creation code should be used. One use case, though atypical, for
+        create_module() is to provide a module that is a subclass of the
+        builtin module type. Most loaders will not need to implement
+        create_module(),
+
+        create_module() should properly handle the case where it is called
+        more than once for the same spec/module. This may include returning
+        None or raising ImportError.
+        """
+        return None
+        return imp_new_module(spec.name)
+
+    def exec_module(self, module):
+        """
+        PEP-451 loader.exec_module() method for the ``sys.meta_path`` hook.
+
+        Loaders will have a new method, exec_module(). Its only job is to
+        "exec" the module and consequently populate the module's namespace. It
+        is not responsible for creating or preparing the module object, nor
+        for any cleanup afterward. It has no return value. exec_module() will
+        be used during both loading and reloading.
+
+        exec_module() should properly handle the case where it is called more
+        than once. For some kinds of modules this may mean raising ImportError
+        every time after the first time the method is called. This is
+        particularly relevant for reloading, where some kinds of modules do
+        not support in-place reloading.
+        """
+        spec = module.__spec__
+        bytecode = self.get_code(spec.loader_state)
+
+        # Set by the import machinery
+        assert hasattr(module, '__file__')
+
+        # If `submodule_search_locations` is not None, this is a package;
+        # set __path__.
+        if spec.submodule_search_locations is not None:
+            # Since PYTHONHOME is set in bootloader, 'sys.prefix' points to
+            # the correct path where PyInstaller should find bundled dynamic
+            # libraries. In one-file mode it points to the tmp directory where
+            # bundled files are extracted at execution time.
+            #
+            # __path__ cannot be empty list because 'wx' module prepends
+            # something to it. It cannot contain value 'sys.prefix' because
+            # 'xml.etree.cElementTree' fails otherwise.
+            #
+            # Set __path__ to point to 'sys.prefix/package/subpackage'.
+            module.__path__ = [pyi_os_path.os_path_dirname(module.__file__)]
+
+        exec(bytecode, module.__dict__)
 
 
 class CExtensionImporter(object):
