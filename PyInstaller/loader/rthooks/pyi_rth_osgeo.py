3ff83834c75aa8ae77813d56dca7d9c9f33bdedb
gmasetti <gmasetti@ccom.unh.edu>
2017-01-20 13:24:56 -0500

    added additional places to check for auxiliary data (conda specific)

--- PyInstaller/loader/rthooks/pyi_rth_osgeo.py
+++ PyInstaller/loader/rthooks/pyi_rth_osgeo.py
@@ -14,7 +14,15 @@ import sys
 
 is_win = sys.platform.startswith('win')
 if is_win:
-    gdal_data = os.path.join(sys._MEIPASS, 'Library', 'data')
+
+    gdal_data = os.path.join(sys._MEIPASS, 'data', 'gdal')
+    if not os.path.exists(gdal_data):
+
+        gdal_data = os.path.join(sys._MEIPASS, 'Library', 'share', 'gdal')
+        # last attempt, check if one of the required file is in the generic folder Library/data
+        if not os.path.exists(os.path.join(gdal_data, 'gcs.csv')):
+            gdal_data = os.path.join(sys._MEIPASS, 'Library', 'data')
+
 else:
     gdal_data = os.path.join(sys._MEIPASS, 'share', 'gdal')
 
