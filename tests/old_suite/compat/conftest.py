9165fb18deb7f00b5e00af4a9ec6de86b8d6554b
xoviat <xoviat@users.noreply.github.com>
2017-01-26 17:22:55 -0600

    Tests: MERGE: restore compat for py27.

--- tests/old_suite/compat/conftest.py
+++ tests/old_suite/compat/conftest.py
@@ -1,4 +1,7 @@
 def pytest_collection_modifyitems(session, config, items):
     sorted_items = sorted(items, key=lambda item: item.name)
-    items.clear()
+    try:
+        items.clear()
+    except AttributeError:
+        del items[:]
     items.extend(sorted_items)
