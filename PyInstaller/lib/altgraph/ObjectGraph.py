ffbe35a1fab20b4a357aaf90118c83ca2b539780
Hartmut Goebel <h.goebel@crazy-compilers.com>
2017-02-12 14:30:03 +0100

    altgraph: Fix and adjust imports.
    * Fix imports in modules PyInstaller does not use.
    * Use relative imports.

--- PyInstaller/lib/altgraph/ObjectGraph.py
+++ PyInstaller/lib/altgraph/ObjectGraph.py
@@ -6,9 +6,9 @@ A graph of objects that have a "graphident" attribute.
 graphident is the key for the object in the graph
 """
 
-from PyInstaller.lib.altgraph import GraphError
-from PyInstaller.lib.altgraph.Graph import Graph
-from PyInstaller.lib.altgraph.GraphUtil import filter_stack
+from . import GraphError
+from .Graph import Graph
+from .GraphUtil import filter_stack
 
 class ObjectGraph(object):
     """
