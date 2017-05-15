fcb07e6b82b0861127b7ec035e77ce91b1a07e7b
Dustin Spicuzza <dustin@virtualroadside.com>
2017-05-15 00:27:50 -0400

    Hooks: GStreamer: add additional gstreamer typelibs

---
+++ PyInstaller/hooks/pre_safe_import_module/hook-gi.repository.GstAudio.py
@@ -0,0 +1,15 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2005-2017, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+
+def pre_safe_import_module(api):
+    # PyGObject modules loaded through the gi repository are marked as
+    # MissingModules by modulegraph so we convert them to
+    # RuntimeModules so their hooks are loaded and run.
+    api.add_runtime_module(api.module_name)
