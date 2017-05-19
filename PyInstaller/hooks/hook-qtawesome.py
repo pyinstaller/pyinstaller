e424eec7dbd22cd170b5f1eb5a6672c10e670c94
Till Bey <tillbey@gmail.com>
2017-05-19 14:52:58 +0200

    Hooks: Add hook for qtawesome.
    [skip ci] no test-case for this.

---
+++ PyInstaller/hooks/hook-qtawesome.py
@@ -0,0 +1,18 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2017, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+"""
+Hook for QtAwesome (https://github.com/spyder-ide/qtawesome).
+Font files and charmaps need to be included with module.
+Tested with QtAwesome 0.4.4 and Python 3.6 on macOS 10.12.4.
+"""
+
+from PyInstaller.utils.hooks import collect_data_files
+
+datas = collect_data_files('qtawesome')
