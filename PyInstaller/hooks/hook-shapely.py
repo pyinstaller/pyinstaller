6e1bd69789773d70164c704bbb60ea8d13b8bbe7
Luke Lee <durdenmisc@gmail.com>
2017-06-19 17:28:54 +0200

    Add copyright header to shapely hook

--- PyInstaller/hooks/hook-shapely.py
+++ PyInstaller/hooks/hook-shapely.py
@@ -1,3 +1,12 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2017, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
 import os
 
 from PyInstaller.utils.hooks import get_package_paths
