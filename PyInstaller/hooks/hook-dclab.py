c9dcd185cc4439b9f00d36a41f8e0b2e5ddab337
Paul Müller <paulmueller@users.noreply.github.com>
2017-06-20 10:16:33 +0200

    Hooks: Add hook for dclab.
    dclab is a library for deformability cytometry.
    
    skip ci] no test-case for this.

---
+++ PyInstaller/hooks/hook-dclab.py
@@ -0,0 +1,14 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2017, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+# Hook for dclab: https://pypi.python.org/pypi/dclab
+
+from PyInstaller.utils.hooks import collect_data_files
+
+datas = collect_data_files('dclab')
