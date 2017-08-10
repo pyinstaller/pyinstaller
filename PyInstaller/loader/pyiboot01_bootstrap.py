99bea2e6594639388a7d9b2a3fe1ccdea7e9ab2f
Hartmut Goebel <htgoebel@users.noreply.github.com>
2017-08-10 15:07:07 +0200

    Update pyiboot01_bootstrap.py
    Add a todo-commtn

--- PyInstaller/loader/pyiboot01_bootstrap.py
+++ PyInstaller/loader/pyiboot01_bootstrap.py
@@ -63,6 +63,7 @@ for pth in sys.path:
     if not os.path.isabs(pth):
         # careful about using abspath with non-unicode path,
         # it breaks multibyte character that contain slash under win32/Python 2
+        # TODO: Revert when dropping suport for is_py2.
         pth = os.path.abspath(pth)
     python_path.append(pth)
     sys.path = python_path
