fc4013d1e20a61c3fcd8be9807f0b9a2ae840338
Chris Norman <chris.norman2@googlemail.com>
2017-08-10 14:12:17 +0100

    Hooks: Add hook for the libaudioverse.
    [skip ci] no test-case for this

---
+++ PyInstaller/hooks/hook-libaudioverse.py
@@ -0,0 +1,16 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2016-2017, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+"""
+Libaudioverse: https://github.com/libaudioverse/libaudioverse
+"""
+
+from pyinstaller.utils.hooks import collect_dynamic_libs
+
+binaries = collect_dynamic_libs('libaudioverse')
