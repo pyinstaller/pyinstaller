fada29f82c65ed5210f8ff5ffeb1507257c191ad
xoviat <xoviat@users.noreply.github.com>
2017-08-10 11:41:06 -0500

    modulegraph: refactor compat
    move get_instrructiosn and friends out of util and modulegraph into compat.

--- PyInstaller/lib/modulegraph/_compat.py
+++ PyInstaller/lib/modulegraph/_compat.py
@@ -1,9 +1,33 @@
 import sys
 
 if sys.version_info[0] == 2:
+    PY2 = True
+
+    from StringIO import StringIO as BytesIO
+    from StringIO import StringIO
+    from urllib import pathname2url
+    _cOrd = ord
+
+    # File open mode for reading (univeral newlines)
+    _READ_MODE = "rU"
+
     def Bchr(value):
         return chr(value)
-
 else:
+    PY2 = False
+    
+    from urllib.request import pathname2url
+    from io import BytesIO, StringIO
+    _cOrd = int
+    _READ_MODE = "r"
+
     def Bchr(value):
         return value
+
+if sys.version_info < (3, 4):
+    try:
+        from xdis.std import get_instructions
+    except ImportError:
+        from dis3 import get_instructions
+else:
+    from dis import get_instructions
