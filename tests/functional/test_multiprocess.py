7a2e9706906be8bf3bf8501b18ab9256cf1a5ee3
Brian Teague <bpteague@gmail.com>
2017-09-04 18:44:07 -0400

    Tests: add multiprocessing tests to exercise semaphores

--- tests/functional/test_multiprocess.py
+++ tests/functional/test_multiprocess.py
@@ -15,7 +15,7 @@ import sys
 
 # Local imports
 # -------------
-from PyInstaller.compat import is_py34
+from PyInstaller.compat import is_py34, is_win
 from PyInstaller.utils.tests import importorskip, skipif
 
 
@@ -34,18 +34,20 @@ def test_multiprocess_pool(pyi_builder):
     pyi_builder.test_script('pyi_multiprocess_pool.py')
 
 
-@skipif(not is_py34, reason='Spawn was introduced in python 3.4+')
+@skipif(not is_py34, reason='set_start_method introduced in python 3.4+')
 @importorskip('multiprocessing')
-def test_multiprocess_spawn(pyi_builder, capfd):
+def test_multiprocess_spawn_semaphore(pyi_builder, capfd):
     pyi_builder.test_source("""
         import sys 
 
-        from multiprocessing import set_start_method, Process
+        from multiprocessing import set_start_method, Process, Semaphore
         from multiprocessing import freeze_support
         from multiprocessing.util import log_to_stderr
 
-        def test():
+        def test(s):
+            s.acquire()
             print('In subprocess')
+            s.release()
 
         if __name__ == '__main__':
             log_to_stderr()
@@ -54,8 +56,11 @@ def test_multiprocess_spawn(pyi_builder, capfd):
 
             print('In main')
             sys.stdout.flush()
-            proc = Process(target=test)
+            s = Semaphore()
+            s.acquire()
+            proc = Process(target=test, args = [s])
             proc.start()
+            s.release()
             proc.join()
         """)
 
@@ -70,4 +75,99 @@ def test_multiprocess_spawn(pyi_builder, capfd):
     assert os.linesep.join(expected) in out
     for substring in expected:
         assert out.count(substring) == 1
+
+
+@skipif(not is_py34, reason='set_start_method introduced in python 3.4+')
+@skipif(is_win, reason='fork is not available on windows')
+@importorskip('multiprocessing')
+def test_multiprocess_fork_semaphore(pyi_builder, capfd):
+    pyi_builder.test_source("""
+        import sys 
+
+        from multiprocessing import set_start_method, Process, Semaphore
+        from multiprocessing import freeze_support
+        from multiprocessing.util import log_to_stderr
+
+        def test(s):
+            s.acquire()
+            print('In subprocess')
+            s.release()
+
+        if __name__ == '__main__':
+            log_to_stderr()
+            freeze_support()
+            set_start_method('fork')
+
+            print('In main')
+            sys.stdout.flush()
+            s = Semaphore()
+            s.acquire()
+            proc = Process(target=test, args = [s])
+            proc.start()
+            s.release()
+            proc.join()
+        """)
+
+    out, err = capfd.readouterr()
+
+    # Print the captured output and error so that it will show up in the test output.
+    sys.stderr.write(err)
+    sys.stdout.write(out)
+
+    expected = ["In main", "In subprocess"]
+
+    assert os.linesep.join(expected) in out
+    for substring in expected:
+        assert out.count(substring) == 1
+
+
+
+
+@skipif(not is_py34, reason='set_start_method introduced in python 3.4+')
+@skipif(is_win, reason='forkserver is not available on windows')
+@importorskip('multiprocessing')
+def test_multiprocess_forkserver_semaphore(pyi_builder, capfd):
+    pyi_builder.test_source("""
+        import sys 
+
+        from multiprocessing import set_start_method, Process, Semaphore
+        from multiprocessing import freeze_support
+        from multiprocessing.util import log_to_stderr
+
+        def test(s):
+            s.acquire()
+            print('In subprocess')
+            s.release()
+
+        if __name__ == '__main__':
+            log_to_stderr()
+            freeze_support()
+            set_start_method('forkserver')
+
+            print('In main')
+            sys.stdout.flush()
+            s = Semaphore()
+            s.acquire()
+            proc = Process(target=test, args = [s])
+            proc.start()
+            s.release()
+            proc.join()
+        """)
+
+    out, err = capfd.readouterr()
+
+    # Print the captured output and error so that it will show up in the test output.
+    sys.stderr.write(err)
+    sys.stdout.write(out)
+
+    expected = ["In main", "In subprocess"]
+
+    assert os.linesep.join(expected) in out
+    for substring in expected:
+        assert out.count(substring) == 1
+
+
+
+
+
 
