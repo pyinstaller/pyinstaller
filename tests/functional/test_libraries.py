aec9e54e07da541760936cdad013168b07231f24
Nejc Habjan <hab.nejc@gmail.com>
2017-10-05 00:18:13 +0200

    Tests: Add test for uvloop hook.

--- tests/functional/test_libraries.py
+++ tests/functional/test_libraries.py
@@ -822,3 +822,9 @@ def test_unidecode(pyi_builder):
         # Unidecode should not skip non-ASCII chars if mappings for them exist.
         assert unidecode(u"kožušček") == "kozuscek"
         """)
+
+
+@importorskip('uvloop')
+@skipif(is_win or not is_py35, reason='Windows, or py < 3.5 not supported')
+def test_uvloop(pyi_builder):
+    pyi_builder.test_source("import uvloop")
