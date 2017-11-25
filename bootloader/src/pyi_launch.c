e183b3e3b94166cfe67d430418bb963e5ae3f2dd
Hartmut Goebel <h.goebel@crazy-compilers.com>
2017-11-25 13:07:43 +0100

    WIP Bootloader: Create a new module '__main__' for each script.
    This will ensure each scripts will get its own global variables.
    
    - Move the creation of the '__main__' module into the loop and remove
      this module from sys.modules after the script has been executed.
    - Use PyImport_ExecCodeModule() for executing the scripts.
    - No longer set the __file__ attribute (now done by PyImport_ExecCodeModule).
    
    Using PyImport_AddModule within the loop did not work: For the second script,
    '__builtins__' have not been set, so even a simple import failed. (See
    pull-request #3038 for details.)

--- bootloader/src/pyi_launch.c
+++ bootloader/src/pyi_launch.c
@@ -366,22 +366,19 @@ pyi_launch_run_scripts(ARCHIVE_STATUS *status)
     char buf[PATH_MAX];
     size_t namelen;
     TOC * ptoc = status->tocbuff;
-    PyObject *__main__;
-    PyObject *__file__;
-    PyObject *main_dict;
-    PyObject *code, *retval;
+    PyObject *__main__name;
+    PyObject *sys_modules;
+    PyObject *code, *module;
 
-    __main__ = PI_PyImport_AddModule("__main__");
-
-    if (!__main__) {
-        FATALERROR("Could not get __main__ module.");
-        return -1;
+    sys_modules = PI_PyImport_GetModuleDict(); /* borrowed reference */
+    if (is_py2) {
+        __main__name = PI_PyString_FromString("__main__");
     }
-
-    main_dict = PI_PyModule_GetDict(__main__);
-
-    if (!main_dict) {
-        FATALERROR("Could not get __main__ module's dict.");
+    else {
+        __main__name = PI_PyUnicode_FromString("__main__");
+    }
+    if (__main__name == NULL) {
+        FATALERROR("Could not get object for string '__main__'.");
         return -1;
     }
 
@@ -402,41 +399,41 @@ pyi_launch_run_scripts(ARCHIVE_STATUS *status)
             strcat(buf, ".py");
             VS("LOADER: Running %s\n", buf);
 
-            if (is_py2) {
-                __file__ = PI_PyString_FromString(buf);
-            }
-            else {
-                __file__ = PI_PyUnicode_FromString(buf);
-            };
-            PI_PyObject_SetAttrString(__main__, "__file__", __file__);
-            Py_DECREF(__file__);
 
             /* Unmarshall code object */
             code = PI_PyMarshal_ReadObjectFromString((const char *) data, ntohl(ptoc->ulen));
-
             if (!code) {
                 FATALERROR("Failed to unmarshal code object for %s\n", ptoc->name);
                 PI_PyErr_Print();
                 return -1;
             }
             /* Run it */
-            retval = PI_PyEval_EvalCode(code, main_dict, main_dict);
+            module = PI_PyImport_ExecCodeModule("__main__", code);
 
             /* If retval is NULL, an error occured. Otherwise, it is a Python object.
              * (Since we evaluate module-level code, which is not allowed to return an
              * object, the Python object returned is always None.) */
-            if (!retval) {
+            if (!module) {
                 PI_PyErr_Print();
                 /* If the error was SystemExit, PyErr_Print calls exit() without
                  * returning. So don't print "Failed to execute" on SystemExit. */
                 FATALERROR("Failed to execute script %s\n", ptoc->name);
                 return -1;
             }
+            Py_DECREF(module);
+
             free(data);
+
+            /* remove '__main__' from sys.modules */
+            if (PI_PyObject_DelItem(sys_modules, __main__name) != 0) {
+                FATALERROR("Failed to remove '__main__' from sys.modules\n");
+                return -1;
+            }
         }
 
         ptoc = pyi_arch_increment_toc_ptr(status, ptoc);
     }
+    Py_DECREF(__main__name);
     return 0;
 }
 
