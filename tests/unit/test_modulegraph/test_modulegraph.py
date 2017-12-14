c7772f8a2bdd263180578d22bdb18aa5d3b7df81
xoviat <xoviat@users.noreply.github.com>
2017-12-14 11:24:44 -0600

    [squash]

--- tests/unit/test_modulegraph/test_modulegraph.py
+++ tests/unit/test_modulegraph/test_modulegraph.py
@@ -467,7 +467,7 @@ class TestNode (unittest.TestCase):
         if '__dict__' in d:
             # New in Python 3.4
             del d['__dict__']
-        if '__slotnames__ in d:
+        if '__slotnames__' in d:
             del d['__slotnames__']
         self.assertEqual(d, {})
 
