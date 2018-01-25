84e40a759f3c7c77f06fbafbd0537c0f58f99594
Bryan A. Jones <bjones@ece.msstate.edu>
2018-01-24 23:05:53 -0600

    Fix: Simplify run-time hook for PySide2 only, reverting to its functionality in 4e8e0ff7a5dbf1370f57f734bcbd37f6157ce532. Move: Place non-plugins code in pyi_rth_qt5.py.

--- PyInstaller/loader/rthooks/pyi_rth_qt5plugins.py
+++ PyInstaller/loader/rthooks/pyi_rth_qt5plugins.py
@@ -7,7 +7,6 @@
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
 
-
 # Qt5 plugins are bundled as data files (see hooks/hook-PyQt5*),
 # within a "qt5_plugins" directory.
 # We add a runtime hook to tell Qt5 where to find them.
@@ -15,37 +14,8 @@
 import os
 import sys
 
-# We remove QT_PLUGIN_PATH variable, because we want Qt5 to load
-# plugins only from one path.
-if 'QT_PLUGIN_PATH' in os.environ:
-    # On some platforms (e.g. AIX) 'os.unsetenv()' is not available and then
-    # deleting the var from os.environ does not delete it from the environment.
-    # In those cases we cannot delete the variable but only set it to the
-    # empty string.
-    os.environ['QT_PLUGIN_PATH'] = ''
-    del os.environ['QT_PLUGIN_PATH']
+from PySide2.QtCore import QCoreApplication
 
-try:
-    import sip
-except ImportError:
-    # The packaging system may name sip PyQt5.sip, so make it available as "sip"
-    try:
-        import PyQt5.sip as sip
-        sys.modules['sip'] = sip
-    except ImportError:
-        pass  # if this didn't work, there will be a NameError later
-
-
-# We cannot use QT_PLUGIN_PATH here, because it would not work when
-# PyQt5 is compiled with a different CRT from Python (eg: it happens
-# with Riverbank's GPL package).
-try:
-    from PyQt5.QtCore import QCoreApplication
-except ImportError:
-    from PySide2.QtCore import QCoreApplication
-
-# Self-compiled versions of PyQt5 don't have this set correctly, while wheel
-# versions do. Set this correctly regardless. See
-# https://github.com/pyinstaller/pyinstaller/issues/3175.
-d = os.path.join(sys._MEIPASS, 'PyQt5', 'Qt', 'plugins')
-QCoreApplication.setLibraryPaths([os.path.abspath(d)])
+# Set "qt5_plugins" as the only path for Qt5 plugins.
+QCoreApplication.setLibraryPaths(
+    [os.path.abspath(os.path.join(sys._MEIPASS, "qt5_plugins"))])
