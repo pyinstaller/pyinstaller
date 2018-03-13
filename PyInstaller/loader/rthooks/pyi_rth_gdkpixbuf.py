404879ae3f68117be6211c4c6bf096aa8cf33c68
Dustin Spicuzza <dustin@virtualroadside.com>
2018-03-12 22:58:25 -0400

    Hooks: fix GdkPixbuf to work on newer msys2
    - The loaders used to be static, now it seems to be copied to a weird location
    - Move the output directory to match what GdkPixbuf installs to

--- PyInstaller/loader/rthooks/pyi_rth_gdkpixbuf.py
+++ PyInstaller/loader/rthooks/pyi_rth_gdkpixbuf.py
@@ -12,7 +12,8 @@ import os
 import tempfile
 import sys
 
-pixbuf_file = os.path.join(sys._MEIPASS, 'lib', 'gdk-pixbuf', 'loaders.cache')
+pixbuf_file = os.path.join(sys._MEIPASS, 'lib', 'gdk-pixbuf-2.0', '2.10.0',
+                           'loaders.cache')
 
 # If we're not on Windows we need to rewrite the cache
 # -> we rewrite on OSX to support --onefile mode
@@ -25,8 +26,8 @@ if os.path.exists(pixbuf_file) and sys.platform != 'win32':
     # we injected with the actual path
     fd, pixbuf_file = tempfile.mkstemp()
     with os.fdopen(fd, 'wb') as fp:
-        fp.write(contents.replace(b'@executable_path/lib',
-                                  os.path.join(sys._MEIPASS, 'lib').encode('utf-8')))
+        libpath = os.path.join(sys._MEIPASS, 'lib').encode('utf-8')
+        fp.write(contents.replace(b'@executable_path/lib', libpath))
 
     try:
         atexit.register(os.unlink, pixbuf_file)
