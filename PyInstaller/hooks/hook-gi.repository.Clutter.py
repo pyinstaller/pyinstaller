87c605015743a1ad663359ba0a8a21bfce2a3276
Benoît Vinot <benoit.vinot@inria.fr>
2018-04-09 09:14:40 +0200

    Hooks: New hooks for Clutter and Champlain

---
+++ PyInstaller/hooks/hook-gi.repository.Clutter.py
@@ -0,0 +1,15 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2005-2018, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+"""
+Import hook for PyGObject's "gi.repository.Clutter" package.
+"""
+
+from PyInstaller.utils.hooks import get_gi_typelibs
+
+binaries, datas, hiddenimports = get_gi_typelibs('Clutter', '1.0')
