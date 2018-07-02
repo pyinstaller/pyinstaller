51b9359e04fa0c945691a93e397a340b0822db98
Hartmut Goebel <htgoebel@users.noreply.github.com>
2018-07-02 22:08:37 +0200

    FIXUP

--- PyInstaller/hooks/hook-jedi.py
+++ PyInstaller/hooks/hook-jedi.py
@@ -7,11 +7,8 @@
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
 
-
-"""
-Hook for https://pypi.org/project/jedi/
-"""
-
+# Hook for Jedi, a static analysis tool https://pypi.org/project/jedi/
 
 from PyInstaller.utils.hooks import collect_data_files
+
 datas = collect_data_files('jedi')
