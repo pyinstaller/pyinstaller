0ad06e3375b48a5cfcc24d040c5eeb647c75df07
brightening-eyes <amir.ramezani1370@gmail.com>
2018-09-01 17:10:20 +0430

    added a hook for nltk for PyInstaller/hooks/hook-nltk.py

---
+++ PyInstaller/hooks/hook-nltk.py
@@ -0,0 +1,18 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013-2018, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+import nltk
+
+#loop through the nltk data path, then add them as the data directory
+datas=[]
+for p in nltk.data.path:
+	datas.append((p, "nltk_data"))
+
+#nltk.chunk.named_entity should be included as name entity chunking doesnt work without it
+hiddenimports=["nltk.chunk.named_entity"]
