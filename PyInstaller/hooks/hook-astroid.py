36f36f3317d31cd4868e221bbb54e08e7c8b9c48
Bryan A. Jones <bjones@ece.msstate.edu>
2018-09-01 23:19:55 -0500

    Hooks: Fix test failure with astroid, in which `six` isn't packaged.

--- PyInstaller/hooks/hook-astroid.py
+++ PyInstaller/hooks/hook-astroid.py
@@ -40,6 +40,6 @@ datas = collect_data_files('astroid', True, 'brain')
 # everything in brain/ is dynamically imported, these are hidden imports. For
 # simplicity, include everything in astroid. Exclude all the test/ subpackage
 # contents and the test_util module.
-hiddenimports = collect_submodules('astroid',
+hiddenimports = ['six'] + collect_submodules('astroid',
   lambda name: (not is_module_or_submodule(name, 'astroid.tests')) and
                (not name == 'test_util'))
