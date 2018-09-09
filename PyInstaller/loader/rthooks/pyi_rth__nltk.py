5116e7965b48cc0e9ab7394f6af3ee6bc0f90a90
Amir Ramezani <amir.ramezani1370@gmail.com>
2018-09-09 14:28:25 +0430

    Hooks: Add hook for nltk.
    [skip ci] no test-case for this hook

---
+++ PyInstaller/loader/rthooks/pyi_rth__nltk.py
@@ -0,0 +1,16 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013-2018, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+
+import sys
+import os
+import nltk
+
+#add the path to nltk_data
+nltk.data.path.append(os.path.join(sys._MEIPASS, "nltk_data"))
