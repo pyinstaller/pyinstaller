130d8d3e1421546aeab16edbcdcff61febf3a305
Hartmut Goebel <h.goebel@crazy-compilers.com>
2018-09-09 13:29:08 +0200

    Fix release-helper script update-readme-versions.sh.
    - Fix replacement for pyinstaller.readthedocs.io.
    - Remove outdated changes regarding removed badges.

--- scripts/update-readme-versions.sh
+++ scripts/update-readme-versions.sh
@@ -14,10 +14,5 @@ fi
 
 INFILE="$(dirname "$0")/../README.rst"
 
-sed -e '/img\.shields\.io.*travis/   s/\<develop\>/v'$VERSION'/' \
-    -e '/img\.shields\.io.*appveyor/ s/\<develop\>/v'$VERSION'/' \
-    -e '/ci\.appveyor/  s/\<develop\>/v'$VERSION'/' \
-    -e '/landscape\.io/ s/\<develop\>/master/' \
-    -e '/img\.shields\.io\/badge\// s/-latest-/-v'$VERSION'-/' \
-    -e '/pyinstaller\.readthedocs\.io\// s/\<latest\>/v'$VERSION'/' \
+sed -e '/pyinstaller\.readthedocs\.io\// s!/latest!/v'$VERSION'!' \
     -i "$INFILE"
