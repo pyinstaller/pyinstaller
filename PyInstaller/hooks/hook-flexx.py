eb04269e9cc3b8c3a5e9c5dff976c15a4c21be8e
Korijn van Golen <korijn@gmail.com>
2018-12-07 16:53:05 +0100

    Update hook-flexx.py

--- PyInstaller/hooks/hook-flexx.py
+++ PyInstaller/hooks/hook-flexx.py
@@ -1,13 +1,11 @@
 import os
 from PyInstaller.utils.hooks import collect_data_files, get_module_file_attribute
 
-datas = collect_data_files('flexx', include_py_files=True)
-
 # We must also collect flexx source code manually and add it to data to
 # allow transpilation of py->js.
 flexx_py_path = os.path.dirname(get_module_file_attribute('flexx'))
 
-datas = datas + [
+datas = collect_data_files('flexx', include_py_files=True) + [
     (flexx_py_path, 'flexx'),
     (flexx_py_path, 'site-packages/flexx'),
 ]
