069da1685c9e1153d6a48421cf5868b2de3eb39a
Amo Godwin <amohgyebigodwin@gmail.com>
2019-01-03 04:02:46 +0000

    # Hooks: add the qml files imported by Qml engine and all its imports
    * Extract the main qml file and all other qml files used in the application
    
    * Add all the imports including the JavaScripts and folders
    
    * Package all images with extensions .png, .jpg, .jpeg,
    .tiff, .gif, .ico in qml source code

--- PyInstaller/hooks/hook-PyQt5.QtQml.py
+++ PyInstaller/hooks/hook-PyQt5.QtQml.py
@@ -1,5 +1,5 @@
 #-----------------------------------------------------------------------------
-# Copyright (c) 2013-2019, PyInstaller Development Team.
+# Copyright (c) 2013-2018, PyInstaller Development Team.
 #
 # Distributed under the terms of the GNU General Public License with exception
 # for distributing bootloader.
@@ -202,7 +202,10 @@ class QmlImports():
 
         # Here we read the line to see if there is
         # a genuine qml filepath.
+<<<<<<< HEAD
+=======
 
+>>>>>>> 2272b0030893e2d4a9e003223bbae4c309a2e4bb
         if re.findall('(\+|,|\))', raw) or not re.findall('(\'|")', raw):
             # It is a variable
             pass
