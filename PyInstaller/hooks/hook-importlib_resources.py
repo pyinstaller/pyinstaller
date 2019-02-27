33ce6edd4b9c831cf030205b6c53be173d556450
TeamSpen210 <spencerb21@live.com>
2019-02-27 11:11:55 +1000

    Add hook for importlib_resources

---
+++ PyInstaller/hooks/hook-importlib_resources.py
@@ -0,0 +1,31 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013-2019, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+import os
+from PyInstaller.compat import is_py2, is_py3, is_py37
+from PyInstaller.utils.hooks import get_module_file_attribute
+# importlib_resources is a backport of 3.7+ importlib.resources.
+
+# Include this version file, loaded to set __version__.
+res_loc = os.path.dirname(get_module_file_attribute('importlib_resources'))
+
+datas = [
+    (os.path.join(res_loc, 'version.txt'), 'importlib_resources'),
+]
+
+# Replicate the module's version checks to exclude unused modules.
+if is_py37:
+    # Stdlib now has the implmentation of this, so the backports aren't used.
+    excludedmodules = [
+        'importlib_resources._py2',
+        'importlib_resources._py3',
+    ]
+elif is_py3:
+    excludedmodules = ['importlib_resources._py2']
+elif is_py2:
+    excludedmodules = ['importlib_resources._py3']
