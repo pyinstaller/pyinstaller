51b469ea9322b47e89161699048ce7461a6ab392
Addison Elliott <addison.elliott@gmail.com>
2019-03-12 15:57:38 -0500

    add pywt hook

---
+++ PyInstaller/hooks/hook-pywt.py
@@ -0,0 +1,11 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2019-2019, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+
+hiddenimports = ['pywt._extensions._cwt']
