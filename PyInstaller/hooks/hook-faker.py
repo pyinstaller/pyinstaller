e8c9b70035e8d05a2a63b63f160391a5d4d51481
Amir Rossert <amir.rossert@safebreach.com>
2019-03-26 16:31:55 +0200

    moved "+" to the next line

--- PyInstaller/hooks/hook-faker.py
+++ PyInstaller/hooks/hook-faker.py
@@ -10,5 +10,5 @@
 from PyInstaller.utils.hooks import collect_submodules, collect_data_files
 
 hiddenimports = collect_submodules('faker.providers')
-datas = (collect_data_files('text_unidecode') +
-         collect_data_files('faker.providers', include_py_files=True))
+datas = (collect_data_files('text_unidecode')
+         + collect_data_files('faker.providers', include_py_files=True))
