cd2edc9c5ed55724d32417af87d5a6944724f4d0
Amir Rossert <amir.rossert@safebreach.com>
2019-03-28 12:06:33 +0200

    fixed lint errors

--- PyInstaller/hooks/hook-faker.py
+++ PyInstaller/hooks/hook-faker.py
@@ -10,5 +10,4 @@
 from PyInstaller.utils.hooks import collect_submodules, collect_data_files
 
 hiddenimports = collect_submodules('faker.providers')
-datas = (collect_data_files('text_unidecode')
-         + collect_data_files('faker.providers', include_py_files=True))
+datas = (collect_data_files('text_unidecode') + collect_data_files('faker.providers', include_py_files=True))
