5775458a7006c8c1a1391ad5f86a32e6cbb32d9b
Amir Rossert <amir.rossert@safebreach.com>
2019-03-30 18:39:56 +0300

    try to fix lint errors

--- PyInstaller/hooks/hook-faker.py
+++ PyInstaller/hooks/hook-faker.py
@@ -10,5 +10,7 @@
 from PyInstaller.utils.hooks import collect_submodules, collect_data_files
 
 hiddenimports = collect_submodules('faker.providers')
-datas = (collect_data_files('text_unidecode')
-         + collect_data_files('faker.providers', include_py_files=True))  # noqa: W503
+datas = (
+        collect_data_files('text_unidecode') +
+        collect_data_files('faker.providers', include_py_files=True)
+        )
