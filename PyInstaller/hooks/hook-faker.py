2b7ab63e245fe37834a1f6e4215d7fc998682fba
Amir Rossert <amir.rossert@safebreach.com>
2019-04-04 15:12:54 +0300

    lint

--- PyInstaller/hooks/hook-faker.py
+++ PyInstaller/hooks/hook-faker.py
@@ -10,6 +10,6 @@
 from PyInstaller.utils.hooks import collect_submodules, collect_data_files
 
 hiddenimports = collect_submodules('faker.providers')
-datas = (collect_data_files('text_unidecode') +
+datas = (collect_data_files('text_unidecode') +  # noqa: W504
          collect_data_files('faker.providers',
-                            include_py_files=True))  # noqa: W504
+                            include_py_files=True))
