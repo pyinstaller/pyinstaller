7f98ee028cda92bd248cfd263321027bf0c5276b
Hartmut Goebel <htgoebel@users.noreply.github.com>
2019-07-09 14:47:11 +0200

    FIXUP
    [skip-ci]

--- PyInstaller/hooks/hook-astropy.py
+++ PyInstaller/hooks/hook-astropy.py
@@ -1,3 +1,12 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013-2019, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
 from PyInstaller.utils.hooks import collect_data_files, collect_submodules
 
 # Astropy includes a number of non-Python files that need to be present
