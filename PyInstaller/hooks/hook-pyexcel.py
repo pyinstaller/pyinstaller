3a769730ebee6c71287cdf20db76b2bb133eff71
HoLuLuLu <schmolli.christian@googlemail.com>
2019-07-16 14:31:28 +0200

    Update hook-pyexcel.py

--- PyInstaller/hooks/hook-pyexcel.py
+++ PyInstaller/hooks/hook-pyexcel.py
@@ -11,4 +11,14 @@
 # This hook was tested with pyexcel 0.5.13:
 # https://github.com/pyexcel/pyexcel
 
-hiddenimports = ['pyexcel.plugins.renderers.sqlalchemy', 'pyexcel.plugins.renderers.django', 'pyexcel.plugins.renderers.excel', 'pyexcel.plugins.renderers._texttable', 'pyexcel.plugins.parsers.excel', 'pyexcel.plugins.parsers.sqlalchemy', 'pyexcel.plugins.sources.http', 'pyexcel.plugins.sources.file_input', 'pyexcel.plugins.sources.memory_input', 'pyexcel.plugins.sources.file_output', 'pyexcel.plugins.sources.output_to_memory', 'pyexcel.plugins.sources.pydata.bookdict', 'pyexcel.plugins.sources.pydata.dictsource', 'pyexcel.plugins.sources.pydata.arraysource', 'pyexcel.plugins.sources.pydata.records', 'pyexcel.plugins.sources.django', 'pyexcel.plugins.sources.sqlalchemy', 'pyexcel.plugins.sources.querysets']
+hiddenimports = ['pyexcel.plugins.renderers.sqlalchemy',
+'pyexcel.plugins.renderers.django', 'pyexcel.plugins.renderers.excel',
+'pyexcel.plugins.renderers._texttable', 'pyexcel.plugins.parsers.excel',
+'pyexcel.plugins.parsers.sqlalchemy', 'pyexcel.plugins.sources.http',
+'pyexcel.plugins.sources.file_input', 'pyexcel.plugins.sources.memory_input',
+'pyexcel.plugins.sources.file_output', 'pyexcel.plugins.sources.output_to_memory',
+'pyexcel.plugins.sources.pydata.bookdict',
+'pyexcel.plugins.sources.pydata.dictsource',
+'pyexcel.plugins.sources.pydata.arraysource',
+'pyexcel.plugins.sources.pydata.records', 'pyexcel.plugins.sources.django',
+'pyexcel.plugins.sources.sqlalchemy', 'pyexcel.plugins.sources.querysets']
