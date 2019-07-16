ba951f0ac4fff9e80c2b2bcd17dae7ecf3e37a7e
strangemind <50242282+strangemind@users.noreply.github.com>
2019-07-16 09:11:25 -0500

    hooks: add hook-firestore.py
    'google-cloud-firestore'  hook is found when file is named 'hook-firestore.py' not 'google-cloud-firestore.py'

--- PyInstaller/hooks/hook-firestore.py
+++ PyInstaller/hooks/hook-firestore.py
@@ -6,9 +6,11 @@
 #
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
+
 # from https://github.com/MartinSahlen/cloud-functions-python/issues/56
 
 from PyInstaller.utils.hooks import copy_metadata
+
 datas = copy_metadata('google-cloud-core')
 datas += copy_metadata('google-cloud-firestore')
 datas += copy_metadata('google-api-core')
