d3a2a203720bba8446f5f3e00dd2689d2d4800cb
Rémy Roy <remyroy@remyroy.com>
2019-08-06 09:43:40 -0400

    Hooks: Exclude imports for pkg_resources to fix bundling issue.

--- PyInstaller/hooks/hook-pkg_resources.py
+++ PyInstaller/hooks/hook-pkg_resources.py
@@ -11,3 +11,5 @@ from PyInstaller.utils.hooks import collect_submodules
 # pkg_resources keeps vendored modules in its _vendor subpackage, and does
 # sys.meta_path based import magic to expose them as pkg_resources.extern.*
 hiddenimports = collect_submodules('pkg_resources._vendor')
+
+excludedimports = ['__main__']
