22c036ca1d4607945ff2521c09fa3e59119e40d5
Amir Rossert <amir.rossert@safebreach.com>
2019-10-18 13:07:11 +0300

    add nnpy hook

---
+++ PyInstaller/hooks/hook-nnpy.py
@@ -0,0 +1,16 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013-2019, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+
+"""
+Hook for https://pypi.org/project/nnpy/
+"""
+
+
+hiddenimports = ['_cffi_backend']
