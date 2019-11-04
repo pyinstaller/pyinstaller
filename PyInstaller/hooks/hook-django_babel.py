212e2b0823b70f47a40cdc490e0fc2a2548c9336
Hugo Martins <caramelo.martins@gmail.com>
2019-11-04 11:22:18 +0000

    Add hook for django-babel. Fixes #4281

---
+++ PyInstaller/hooks/hook-django_babel.py
@@ -0,0 +1,15 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2005-2019, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+# Tested with django-babel v0.6.2.
+# Hook for https://github.com/python-babel/django-babel
+
+from PyInstaller.utils.hooks import copy_metadata
+datas = copy_metadata('django-babel')
+
