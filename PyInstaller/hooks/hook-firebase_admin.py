9f73d19ab669900819ada26728ce039fd475e5c0
Tim Davis <tdavis@palcare.com>
2019-11-11 14:11:02 -0600

    hook for google firebase admin #4209

---
+++ PyInstaller/hooks/hook-firebase_admin.py
@@ -0,0 +1,14 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013-2018, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+# hook for google firestore: https://github.com/MartinSahlen/cloud-functions-python/issues/56
+
+from PyInstaller.utils.hooks import copy_metadata
+
+datas = copy_metadata('firebase-admin')
