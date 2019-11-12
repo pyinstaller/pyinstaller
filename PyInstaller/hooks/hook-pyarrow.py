42157ec00ca85ada1880ef9b4197e1fcca2833d2
Hartmut Goebel <h.goebel@crazy-compilers.com>
2019-11-12 15:04:32 +0100

    Fix lint errors.

--- PyInstaller/hooks/hook-pyarrow.py
+++ PyInstaller/hooks/hook-pyarrow.py
@@ -1,11 +1,11 @@
- #-----------------------------------------------------------------------------
-+# Copyright (c) 2005-2019, PyInstaller Development Team.
- #
- # Distributed under the terms of the GNU General Public License with exception
- # for distributing bootloader.
- #
- # The full license is in the file COPYING.txt, distributed with this software.
- #-----------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
+# Copyright (c) 2005-2019, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
 
 # Hook for https://pypi.org/project/pyarrow/
 
@@ -18,6 +18,4 @@ hiddenimports = [
 ]
 
 datas = collect_data_files('pyarrow')
-
 binaries = collect_dynamic_libs('pyarrow')
-
