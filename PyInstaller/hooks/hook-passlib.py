9aea5049e508f4af403d89e26298fe22e7114d31
Hartmut Goebel <h.goebel@crazy-compilers.com>
2019-11-12 15:15:55 +0100

    Fix lint errors, add comment.

--- PyInstaller/hooks/hook-passlib.py
+++ PyInstaller/hooks/hook-passlib.py
@@ -7,10 +7,11 @@
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
 
-
+# Handlers are imported by a lazy-load proxy, based on a
+# name-to-package mapping. Collect all handlers to ease packaging.
+# If you want to reduce the size of your application, used
+# `--exclude-module` to remove unused ones.
 hiddenimports = [
     "passlib.handlers",
     "passlib.handlers.digests",
 ]
-
-
