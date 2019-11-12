a0c185a3360ce3c235ad95c456a501abc0d9844d
Matt Khan <mattkhan@gmail.com>
2019-11-12 09:48:54 +0000

    Hooks: Make the soundfile hook platform aware.
    The hook exists to package the libsndfile binary when required.
    However pysoundfile only bundles libsndfile on OSX and Windows,
    on Linux it relies on a system package. Attempting to include
    _soundfile_data on Linux therefore results in an unexpected
    exception as per #4325.
    
    Furthermore the bundled files are different on OSX and Windows
    so the data required has to be platform specific.

--- PyInstaller/hooks/hook-soundfile.py
+++ PyInstaller/hooks/hook-soundfile.py
@@ -13,11 +13,20 @@ https://github.com/bastibe/SoundFile
 """
 
 import os
+
+from PyInstaller.compat import is_win, is_darwin
 from PyInstaller.utils.hooks import get_package_paths
 
 # get path of soundfile
 sfp = get_package_paths('soundfile')
 
-# add the binaries
-bins = os.path.join(sfp[0], "_soundfile_data")
-binaries = [(bins, "_soundfile_data")]
+# add binaries packaged by soundfile on OSX and Windows
+# an external dependency (libsndfile) is used on GNU/Linux
+path = None
+if is_win:
+    path = os.path.join(sfp[0], '_soundfile_data')
+elif is_darwin:
+    path = os.path.join(sfp[0], '_soundfile_data', 'libsndfile.dylib')
+
+if path is not None and os.path.exists(path):
+    binaries = [(path, "_soundfile_data")]
