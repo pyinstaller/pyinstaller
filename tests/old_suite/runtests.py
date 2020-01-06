87b038413e11e132f8690acc47f094b869000cf9
Hartmut Goebel <h.goebel@crazy-compilers.com>
2020-01-07 00:49:25 +0100

    Update license header in all files having one - wording only.
    * Clarify GPL version.
    * Add SPDX-License-Identifier.

--- tests/old_suite/runtests.py
+++ tests/old_suite/runtests.py
@@ -2,10 +2,12 @@
 #-----------------------------------------------------------------------------
 # Copyright (c) 2013-2020, PyInstaller Development Team.
 #
-# Distributed under the terms of the GNU General Public License with exception
-# for distributing bootloader.
+# Distributed under the terms of the GNU General Public License (version 2
+# or later) with exception for distributing the bootloader.
 #
 # The full license is in the file COPYING.txt, distributed with this software.
+#
+# SPDX-License-Identifier: (GPL-2.0-or-later WITH Bootloader-exception)
 #-----------------------------------------------------------------------------
 
 # This program will execute any file with name test*<digit>.py. If your test
