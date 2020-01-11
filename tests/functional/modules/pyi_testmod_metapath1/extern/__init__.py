77d5e7c536fdf83342868947cbf947df93541058
Hartmut Goebel <h.goebel@crazy-compilers.com>
2020-01-11 14:22:09 +0100

    Tests: Add SPDX-License-Identifier to a file.

--- tests/functional/modules/pyi_testmod_metapath1/extern/__init__.py
+++ tests/functional/modules/pyi_testmod_metapath1/extern/__init__.py
@@ -24,6 +24,8 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
+#
+# SPDX-License-Identifier: MIT
 
 #--- Code of pkg_resources/extern/__init__.py starts here
 import sys
