b7dbac29441fa46310adced9a7e8838a9ff0bbda
Hartmut Goebel <h.goebel@crazy-compilers.com>
2020-01-11 14:50:51 +0100

    Tests: Remove some uses of `unicode`.

--- tests/functional/test_basic.py
+++ tests/functional/test_basic.py
@@ -125,11 +125,7 @@ def test_compiled_filenames(pyi_builder):
 def test_decoders_ascii(pyi_builder):
     pyi_builder.test_source(
         """
-        # This import forces Python 2 to handle string as unicode -
-        # as with prefix 'u'.
-        from __future__ import unicode_literals
-
-        # Convert type 'bytes' to type 'str' (Py3) or 'unicode' (Py2).
+        # Convert type 'bytes' to type 'str'.
         assert b'foo'.decode('ascii') == 'foo'
         """)
 
