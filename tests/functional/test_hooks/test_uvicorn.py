27787a8e7c9b2400f31584c7531509af06c087d1
Genadij Razdorov <genadijrazdorov@gmail.com>
2020-02-05 16:04:14 +0100

    Tests: Change port 8000 to 0 for random free port in uvicorn test
    Fixes failed uvicorn testing due to already used port (8000) on localhost.

--- tests/functional/test_hooks/test_uvicorn.py
+++ tests/functional/test_hooks/test_uvicorn.py
@@ -47,7 +47,7 @@ def test_uvicorn(pyi_builder):
             uvicorn.run(
                 "test_source:app",
                 host="127.0.0.1",
-                port=8000,
+                port=0,
                 log_level="info"
             #))
             )
