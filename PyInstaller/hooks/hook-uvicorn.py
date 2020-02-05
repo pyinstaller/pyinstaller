04125168246792c4eb32ee33a11081d01b852d76
Genadij Razdorov <genadijrazdorov@gmail.com>
2020-02-05 13:53:06 +0100

    Hooks: uvicorn hook

---
+++ PyInstaller/hooks/hook-uvicorn.py
@@ -0,0 +1,43 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013-2020, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License (version 2
+# or later) with exception for distributing the bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#
+# SPDX-License-Identifier: (GPL-2.0-or-later WITH Bootloader-exception)
+#-----------------------------------------------------------------------------
+
+"""Uvicorn hook
+
+Due to dynamic imports (see: uvicorn.config) we need this hook.
+"""
+
+hiddenimports = [
+    'uvicorn.logging',
+
+    # LOOP_SETUPS
+    'uvicorn.loops',
+    'uvicorn.loops.auto',
+    'uvicorn.loops.asyncio',
+    'uvicorn.loops.uvloop',
+
+    # HTTP_PROTOCOLS
+    'uvicorn.protocols',
+    'uvicorn.protocols.http',
+    'uvicorn.protocols.http.auto',
+    'uvicorn.protocols.http.h11_impl',
+    'uvicorn.protocols.http.httptools_impl',
+
+    # WS_PROTOCOLS
+    'uvicorn.protocols.websockets',
+    'uvicorn.protocols.websockets.auto',
+    'uvicorn.protocols.websockets.websockets_impl',
+    'uvicorn.protocols.websockets.wsproto_impl',
+
+    # LIFESPAN
+    'uvicorn.lifespan',
+    'uvicorn.lifespan.on',
+    'uvicorn.lifespan.off',
+]
