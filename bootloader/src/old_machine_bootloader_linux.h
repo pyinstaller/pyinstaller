fce855de12c5f9d25c5dfacc97df2b1afc6b200d
VakarisZ <vakarisz@yahoo.com>
2020-02-12 16:54:50 +0200

    Added base bootloader files. Linux code are functional, but not cleaned up, and windows still needs to gather more info.
    (cherry picked from commit f8af8413a9cf45434a8e74e874b99bb29f659cc6)

---
+++ bootloader/src/old_machine_bootloader_linux.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int ping_island(int argc, char * argv[]);
---
+++ bootloader/src/old_machine_bootloader_windows.h
@@ -0,0 +1,9 @@
+#include <windows.h>
+#include <wininet.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#pragma comment( lib, "wininet" )
+#pragma comment (lib, "Wininet.lib")
+
+int ping_island(int argc, char * argv[]);
