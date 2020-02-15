c0e1f36656a2dccf5ebef6a0cc59e0a88b7c5ed2
Legorooj <legorooj@protonmail.com>
2020-02-15 21:27:21 +1100

    Removed parentheses in assert statement: https://travis-ci.org/pyinstaller/pyinstaller/jobs/650751109?utm_medium=notification&utm_source=github_status

--- PyInstaller/depend/imphook.py
+++ PyInstaller/depend/imphook.py
@@ -285,10 +285,8 @@ class ModuleHook(object):
 
         # Safety check, see above
         global HOOKS_MODULE_NAMES
-        assert(
-            self.hook_module_name not in HOOKS_MODULE_NAMES,
+        assert self.hook_module_name not in HOOKS_MODULE_NAMES, \
             'Custom hook conflict. Please remove the following custom hook: {}'.format(self._hook_name)
-        )
         HOOKS_MODULE_NAMES.add(self.hook_module_name)
 
         # Attributes subsequently defined by the _load_hook_module() method.
