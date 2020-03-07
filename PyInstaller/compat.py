9d71a403982cbc7bf590fb835c334634a8983d63
Legorooj <legorooj@protonmail.com>
2020-03-07 19:16:29 +1100

    Cleanup: Remove unnecessary function
    The function existed due to a bug in the python2.4 build for windows.
    I've set the variable straight to platform.system() instead.

--- PyInstaller/compat.py
+++ PyInstaller/compat.py
@@ -204,17 +204,8 @@ def _architecture():
 architecture = _architecture()
 del _architecture
 
-def _system():
-    # On some Windows installation (Python 2.4) platform.system() is
-    # broken and incorrectly returns 'Microsoft' instead of 'Windows'.
-    # http://mail.python.org/pipermail/patches/2007-June/022947.html
-    syst = platform.system()
-    if syst == 'Microsoft':
-        return 'Windows'
-    return syst
+system = platform.system()
 
-system = _system()
-del _system
 
 def _machine():
     """
