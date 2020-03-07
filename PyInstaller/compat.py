ed9aad9a9f1f5f35e43fa9c0f9d6f7cbba0b7cec
Legorooj <legorooj@protonmail.com>
2020-03-07 19:34:21 +1100

    Cleanup: Simplify version checking
    We already have a boolean variable defined that tells us this; use it

--- PyInstaller/compat.py
+++ PyInstaller/compat.py
@@ -717,8 +717,6 @@ def check_requirements():
     Fail hard if any requirement is not met.
     """
     # Fail hard if Python does not have minimum required version
-    if sys.version_info < (3,):
+    if not is_py35:
         raise SystemExit('PyInstaller requires at least Python 3.5 or newer. '
                          'The last version supporting Python 2.7 was v3.6.')
-    elif sys.version_info < (3, 5):
-        raise SystemExit('PyInstaller requires at least Python 3.5 or newer.')
