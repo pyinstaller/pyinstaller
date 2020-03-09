a5c69f0a2568286978adc163b41af5961548f869
Legorooj <legorooj@protonmail.com>
2020-03-09 19:04:06 +1100

    Lint: Remove whitespace on blank lines and fix line length

--- PyInstaller/compat.py
+++ PyInstaller/compat.py
@@ -572,25 +572,28 @@ def expand_path(path):
 def getsitepackages(prefixes=None):
     """Returns a list containing all global site-packages directories.
 
-    For each directory present in prefixes (or site.PREFIXES if prefixes is None),
-    this function will find its `site-packages` subdirectory depending on the
-    system environment, and will return a list of full paths.
+    For each directory present in prefixes - or site.PREFIXES if prefixes is
+    None,this function will find its `site-packages` subdirectory depending on
+    the system environment, and will return a list of full paths.
     """
     sitepackages = []
     seen = set()
-    
+
     if prefixes is None:
         prefixes = site.PREFIXES
-    
+
     for prefix in prefixes:
         if not prefix or prefix in seen:
             continue
         seen.add(prefix)
-        
+
         if os.sep == '/':
-            sitepackages.append(os.path.join(prefix, "lib",
-                                             "python%d.%d" % sys.version_info[:2],
-                                             "site-packages"))
+            sitepackages.append(
+                os.path.join(prefix, "lib",
+                             "python%d.%d" % sys.version_info[:2],
+                             "site-packages"
+                             )
+            )
         else:
             sitepackages.append(prefix)
             sitepackages.append(os.path.join(prefix, "lib", "site-packages"))
