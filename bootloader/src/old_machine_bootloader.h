8114ddf2d36d00df004581b86638772b26b87f2e
Ace Pace <acepace@gmail.com>
2020-03-10 13:23:20 +0200

    add header guard

--- bootloader/src/old_machine_bootloader.h
+++ bootloader/src/old_machine_bootloader.h
@@ -1,2 +1,8 @@
+#ifndef BOOTLOADER_SRC_OLD_MACHINE_BOOTLOADER_H_
+#define BOOTLOADER_SRC_OLD_MACHINE_BOOTLOADER_H_
+
 // Takes argc and argv from main to get parameters such as tunnel or server
 int ping_island(int argc, char * argv[]);
+
+
+#endif  // BOOTLOADER_SRC_OLD_MACHINE_BOOTLOADER_H_
