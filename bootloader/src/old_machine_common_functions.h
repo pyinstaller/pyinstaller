debad32c8f9905819fab3e052a1632640cf8043b
Ace Pace <acepace@gmail.com>
2020-03-10 13:24:39 +0200

    add header guard

--- bootloader/src/old_machine_common_functions.h
+++ bootloader/src/old_machine_common_functions.h
@@ -1,3 +1,6 @@
+#ifndef BOOTLOADER_SRC_OLD_MACHINE_COMMON_FUNCTIONS_H_
+#define BOOTLOADER_SRC_OLD_MACHINE_COMMON_FUNCTIONS_H_
+
 struct requestData {
 	char* IPstring;
 	char* glibcVersion;
@@ -13,3 +16,5 @@ char* replaceSubstringOnce(char* str, char* to_be_replaced, char* replacement);
 char* getRequestDataJson(struct requestData reqData, char* requestFormat, char* systemStr);
 int parseFlags(int argc, char * argv[], int* server_i, int* tunnel_i);
 void error(const char *msg);
+
+#endif  // BOOTLOADER_SRC_OLD_MACHINE_COMMON_FUNCTIONS_H_
