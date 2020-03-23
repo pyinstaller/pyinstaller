1f7112253a7b63c61c6f82cbe0ff9140f2b044af
Daniel Goldberg <danielg@gc.guardicore.com>
2020-03-23 17:09:03 +0200

    Delete old files first

--- build_bootloaders_windows.ps1
+++ build_bootloaders_windows.ps1
@@ -1,3 +1,6 @@
+Remove-Item PyInstaller/bootloader/Windows-32bit/*.exe
+Remove-Item PyInstaller/bootloader/Windows-64bit/*.exe
+
 Set-Location bootloader
 # assume python in path, otherwise, need to enter venv first
 python .\waf all
