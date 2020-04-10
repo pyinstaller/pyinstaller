81f06d23aabfd9b236c2d5af17b0b3a9ad0ba1f4
Legorooj <legorooj@protonmail.com>
2020-04-10 20:27:34 +1000

    Depend/Cleanup: Docstrings should be double quotes, not apostrophes

--- PyInstaller/depend/imphook.py
+++ PyInstaller/depend/imphook.py
@@ -295,7 +295,7 @@ class ModuleHook(object):
 
 
     def __getattr__(self, attr_name):
-        '''
+        """
         Get the magic attribute with the passed name (e.g., `datas`) from this
         lazily loaded hook script if any _or_ raise `AttributeError` otherwise.
 
@@ -313,7 +313,7 @@ class ModuleHook(object):
         See Also
         ----------
         Class docstring for supported magic attributes.
-        '''
+        """
 
         # If this is a magic attribute, initialize this attribute by lazy
         # loading this hook script and then return this attribute. To avoid
@@ -327,7 +327,7 @@ class ModuleHook(object):
 
 
     def __setattr__(self, attr_name, attr_value):
-        '''
+        """
         Set the attribute with the passed name to the passed value.
 
         If this is a magic attribute, this hook script will be lazily loaded
@@ -338,7 +338,7 @@ class ModuleHook(object):
         See Also
         ----------
         Class docstring for supported magic attributes.
-        '''
+        """
 
         # If this is a magic attribute, initialize this attribute by lazy
         # loading this hook script before overwriting this attribute.
