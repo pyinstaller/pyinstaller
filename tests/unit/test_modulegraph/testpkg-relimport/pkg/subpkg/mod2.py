162aad0622a0f7fac097ffd8f91e0e1546c35d01
Legorooj <legorooj@protonmail.com>
2020-04-12 18:15:45 +1000

    Cleanup: Remove __future__ imports, unused imports, and update copyright year
    Also, docstrings should use double-quotes, not single quotes/apostrophes

--- tests/unit/test_modulegraph/testpkg-relimport/pkg/subpkg/mod2.py
+++ tests/unit/test_modulegraph/testpkg-relimport/pkg/subpkg/mod2.py
@@ -1,4 +1,3 @@
 """ pkg.subpkg.mod2 """
-from __future__ import absolute_import
 from ..sub2.mod import __doc__
 from ..sub2 import mod
--- tests/unit/test_modulegraph/testpkg-relimport/pkg/subpkg/relative2.py
+++ tests/unit/test_modulegraph/testpkg-relimport/pkg/subpkg/relative2.py
@@ -1,3 +1,2 @@
 """ pkg.subpkg.relative """
-from __future__ import absolute_import
 from ..relimport import __doc__ as doc
