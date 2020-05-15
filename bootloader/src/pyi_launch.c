16eb0b2b9a73a4a927dbaec16a3b8a2313704e8b
Legorooj <legorooj@protonmail.com>
2020-05-15 13:11:23 +1000

    bootloader: Don't use the traceback added in #4592 when running on macOS
    See #4860.

--- bootloader/src/pyi_launch.c
+++ bootloader/src/pyi_launch.c
@@ -420,7 +420,7 @@ pyi_launch_run_scripts(ARCHIVE_STATUS *status)
             if (!retval) {
                 FATALERROR("Failed to execute script %s\n", ptoc->name);
 
-                #if defined(WINDOWED) && defined(LAUNCH_DEBUG)
+                #if defined(WINDOWED) && defined(LAUNCH_DEBUG) && !defined(__APPLE__)
                 /* Visual C++ requires all variables to be declared before any
                  * statements in a given block. The curly braces below create a
                  * block to make this compiler happy. */
@@ -466,11 +466,11 @@ pyi_launch_run_scripts(ARCHIVE_STATUS *status)
                     Py_DECREF(module);
                 }
 
-                #else /* if defined(WINDOWED) and defined(LAUNCH_DEBUG) */
+                #else /* if defined(WINDOWED) and defined(LAUNCH_DEBUG) and not defined(__APPLE__) */
 
                     PI_PyErr_Print();
 
-                #endif /* if defined(WINDOWED) and defined(LAUNCH_DEBUG) */
+                #endif /* if defined(WINDOWED) and defined(LAUNCH_DEBUG) and not defined(__APPLE__) */
 
                 return -1;
             }
