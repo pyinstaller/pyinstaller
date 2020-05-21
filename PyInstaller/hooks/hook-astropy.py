64c0419fd3b4b1b475e2c9f5637f17bda73b6649
Thomas Robitaille <thomas.robitaille@gmail.com>
2020-05-21 10:42:13 +0100

    hooks: fix bug in astropy hook that caused most data files to be missing

--- PyInstaller/hooks/hook-astropy.py
+++ PyInstaller/hooks/hook-astropy.py
@@ -22,8 +22,8 @@ hiddenimports = collect_submodules('astropy')
 # We now need to include the *_parsetab.py and *_lextab.py files for unit and
 # coordinate parsing, since these are loaded as files rather than imported as
 # sub-modules.
-datas = collect_data_files('astropy', include_py_files=True,
-                           includes=['**/*_parsetab.py', '**/*_lextab.py'])
+datas += collect_data_files('astropy', include_py_files=True,
+                            includes=['**/*_parsetab.py', '**/*_lextab.py'])
 
 # In the Cython code, Astropy imports numpy.lib.recfunctions which isn't
 # automatically discovered by pyinstaller, so we add this as a hidden import.
