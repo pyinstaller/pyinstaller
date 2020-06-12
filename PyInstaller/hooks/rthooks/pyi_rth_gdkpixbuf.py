97d73983e16afda76cbbc2fca561491a6e561081
Hartmut Goebel <h.goebel@crazy-compilers.com>
2020-06-12 19:15:33 +0200

    Change license for run-time hooks to Apache-2.0.
    After PyInstaller now allows packages to provide their own hooks and we
    started the pyinstaller-hooks-contrib repo, we discovered that the currently
    license "GPL with Bootloader-exception" makes it difficult to move existing
    hooks away from the main repo. Also strictly speaking the
    "Bootloader-exception" only applies to the bootloader, not the run-time hooks,
    which go into the frozen package.
    
    So we wanted a new license which fits better, while porting the principle of
    the "Bootloader-exception" to the run-time hooks: To be used without
    restrictions even in your proprietary program.
    
    We decided to use the Apache License 2.0 for the run-time hooks for these
    reasons:
    
    * Creating a new license is to much effort.
    * LGPL is not appropriate: Section 4 for the LGPL requires the application to
      include a copy of the LGPL license and to enable the user to replace the
      LGPL-ed code (the run-time hook) by a different (modified) version of that
      code. This is neither practical nor what the "Bootloader-exception" means.
    * The FSE recommends using Apache-2.0 for short works, which terms prevent
      contributors and distributors from suing for patent infringement.
    
    These authors agreed to the license change:
    
    Amir Ramezani: email, dated Wed, 15 Apr 2020 13:30:08 +0430
    Ben Hagen: email, dated Wed, 15 Apr 2020 10:50:32 +0200
    Brian Teague: Github comment on gh-4810, 10 May 2020
    Bryan A. Jones: email, dated Wed, 15 Apr 2020 07:47:07 -0500
    David Vierra: email, dated Wed, 15 Apr 2020 03:39:15 -1000
    Dustin Spicuzza: email, dated Wed, 15 Apr 2020 10:44:29 -0400
    Giovanni Bajo: email, dated Wed, 15 Apr 2020 10:48:20 +0200
    Glenn Ramsey: email, dated Sat, 18 Apr 2020 10:39:46 +1200
    Hartmut Goebel: It‘s me :-)
    Jonathan Springer: email, dated Sat, 18 Apr 2020 21:03:04 -0400
    KenjiTakahashi: Github comment on gh-4810, 10 May 2020
    Kuisong Tong: email, dated Wed, 15 Apr 2020 07:28:09 -0700
    Lorenzo Berni: Github comment on gh-4810, 10 May 2020
    Lorenzo Villani: email, dated Thu, 16 Apr 2020 19:47:20 +0200
    Lyux: email, dated Wed, 15 Apr 2020 12:19:06 +0000
    Martin Zibricky: email, dated Wed, 15 Apr 2020 08:57:55 +0000
    Merlijn Wajer: email, dated Wed, 15 Apr 2020 15:48:29 +0200
    Min'an: email, dated Wed, 15 Apr 2020 19:06:21 +0800
    Richard Davis: email, dated Wed, 27 May 2020 12:04:13 -0700
    Tim Stumbaugh: email, dated Wed, 15 Apr 2020 13:22:47 -0400
    Zhen Zhang: email, dated Wed, 15 Apr 2020 08:58:49 -0700
    giumas (aka gmasetti): Github comment on gh-1528, 21 Apr 2020
    itsZero (Chien-An Cho): email, dated Wed, 15 Apr 2020 02:31:33 -0700
    leycec: email, dated Sat, 23 May 2020 00:44:05 -0400
    matham: email, dated Sat, 18 Apr 2020 21:03:04 -0400
    melvyn2: email, dated Wed, 15 Apr 2020 07:47:09 -0700
    thiolliere: Github comment gh-4810, 10 May 2020
    
    These two authors we be no reach:
    
    Andrey.Malkov: A single, trivial change on `pyi_rth_django.py` (ae4e663).
    Gendo Ikari: Only merged a now gone rt-hook for OpenGL (84c95cd93), while
    	  		 the shell code shown below reported several changes to existing
    			 files.
    
    For collecting the author names I used this command:
    
    for f in PyInstaller/hooks/rthooks/pyi_rth_*.py ; do \
       git log --follow --format=short $f | \
       grep Author > /tmp/xx/$(basename $f).txt
    done
    
    Closes #4810.

--- PyInstaller/hooks/rthooks/pyi_rth_gdkpixbuf.py
+++ PyInstaller/hooks/rthooks/pyi_rth_gdkpixbuf.py
@@ -1,12 +1,12 @@
 #-----------------------------------------------------------------------------
 # Copyright (c) 2015-2020, PyInstaller Development Team.
 #
-# Distributed under the terms of the GNU General Public License (version 2
-# or later) with exception for distributing the bootloader.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
 #
 # The full license is in the file COPYING.txt, distributed with this software.
 #
-# SPDX-License-Identifier: (GPL-2.0-or-later WITH Bootloader-exception)
+# SPDX-License-Identifier: Apache-2.0
 #-----------------------------------------------------------------------------
 
 import atexit
