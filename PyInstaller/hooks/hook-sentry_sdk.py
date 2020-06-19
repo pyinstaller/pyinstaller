ee98d9b11f311e4136d0e88b5efcbb86cc118f06
SimonIT <simonit.orig@gmail.com>
2020-06-19 18:23:42 +0200

    Fix lint errors

--- PyInstaller/hooks/hook-sentry_sdk.py
+++ PyInstaller/hooks/hook-sentry_sdk.py
@@ -1,1 +1,8 @@
-hiddenimports = ["sentry_sdk.integrations.stdlib", "sentry_sdk.integrations.excepthook", "sentry_sdk.integrations.dedupe", "sentry_sdk.integrations.atexit", "sentry_sdk.integrations.modules", "sentry_sdk.integrations.argv", "sentry_sdk.integrations.logging", "sentry_sdk.integrations.threading"]
+hiddenimports = ["sentry_sdk.integrations.stdlib",
+                 "sentry_sdk.integrations.excepthook",
+                 "sentry_sdk.integrations.dedupe",
+                 "sentry_sdk.integrations.atexit",
+                 "sentry_sdk.integrations.modules",
+                 "sentry_sdk.integrations.argv",
+                 "sentry_sdk.integrations.logging",
+                 "sentry_sdk.integrations.threading"]
