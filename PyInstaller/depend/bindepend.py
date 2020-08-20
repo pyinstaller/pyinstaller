6464e601b54bdc00742a73d0a097325d384d8051
byehack <khosravim861@gmail.com>
2020-08-20 21:28:22 +0430

    Update PyInstaller/depend/bindepend.py
    Co-authored-by: Hartmut Goebel <htgoebel@users.noreply.github.com>

--- PyInstaller/depend/bindepend.py
+++ PyInstaller/depend/bindepend.py
@@ -125,7 +125,8 @@ def _getImports_pe(pth):
                 # sym.forwarder is a bytes object. Convert it to a string.
                 forwarder = winutils.convert_dll_name_to_str(sym.forwarder)
                 # sym.forwarder is for example 'KERNEL32.EnterCriticalSection'
-                dlls.add(forwarder.split('.')[0] + ".dll")
+                dll  = forwarder.split('.')[0]
+                dlls.add(dll + ".dll")
 
     pe.close()
     return dlls
