pyinstaller/pyinstaller

commit 392aba1817b696e3fedd92bfd05dc5266126f018

building: Fix double processing of dependencies.
Fixes: #4125. Fixes double processing of dependencencies when using
COLLECT after EXE. When processing of MINGW Python dll files, this
results in corrupted file paths, for example when processing the _struct
dll the resulting filepath is
dllruct-cpython-37m/_struct-cpython-37m.dll instead of just
_struct-cpython-37m.dll.

Co-authored-by: Jonathan Springer <springermac@gmail.com>
Co-authored-by: Dan Yeaw <dan@yeaw.me>

PyInstaller/building/api.py
@@ -230,9 +230,7 @@ class PKG(Target):
                 # file is contained within python egg, it is added with the egg
                 continue
             if typ in ('BINARY', 'EXTENSION', 'DEPENDENCY'):
-                if self.exclude_binaries and typ != 'DEPENDENCY':
-                    self.dependencies.append((inm, fnm, typ))
-                else:
+                if not self.exclude_binaries:
                     if typ == 'BINARY':
                         # Avoid importing the same binary extension twice. This might
                         # happen if they come from different sources (eg. once from

commit df3072ca1c17213d3934d291abf11af7c3e02278

building: Fix AppImage builds.
AppImage builds were broken since the commit 392aba1.

PyInstaller/building/api.py
@@ -233,7 +233,9 @@ class PKG(Target):
                 # file is contained within python egg, it is added with the egg
                 continue
             if typ in ('BINARY', 'EXTENSION', 'DEPENDENCY'):
-                if not self.exclude_binaries or typ == 'DEPENDENCY':
+                if self.exclude_binaries and typ == 'EXTENSION':
+                    self.dependencies.append((inm, fnm, typ))
+                elif not self.exclude_binaries or typ == 'DEPENDENCY':
                     if typ == 'BINARY':
                         # Avoid importing the same binary extension twice. This might
                         # happen if they come from different sources (eg. once from