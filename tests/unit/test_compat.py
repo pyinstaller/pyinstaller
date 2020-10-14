87762c42ede982b5c2002cb38da8f7465af5a2d2
Rok Mandeljc <rok.mandeljc@gmail.com>
2020-10-14 20:25:50 +0200

    tests: repurpose the incorrect-encoding-in-process-output test
    Update the test to reflect the new behavior of the stdout/stderr
    decoding proces (replacing the invalid characters without raising
    an error). Instead of expecting the UnicodeDecodeError and checking
    for details in stderr, the test now checks whether the stdout is
    still salvageable (is decoded and can be parsed) in spite of it
    originally containing an invalid character.

--- tests/unit/test_compat.py
+++ tests/unit/test_compat.py
@@ -9,20 +9,20 @@
 # SPDX-License-Identifier: (GPL-2.0-or-later WITH Bootloader-exception)
 #-----------------------------------------------------------------------------
 
-from PyInstaller.utils.tests import skipif
-
-import pytest
-
 from PyInstaller import compat
 
 
-def test_exec_command_subprocess_wrong_encoding_reports_nicely(capsys):
-    # Ensure a nice error message is printed if decoding the output of the
-    # subprocess fails.
-    # Actually `exec_python()` is used for running the progam, so we can use a
+def test_exec_command_subprocess_wrong_encoding_stdout_salvageable():
+    # Ensure that even if the stdout of the executed command contains
+    # invalid characters (mismatched encoding or just garbage), the
+    # rest of the output is still salvageable and can be parsed.
+    # `exec_python()` is used for running the progam, so we can use a
     # small Python script.
-    prog = ("""import sys; sys.stdout.buffer.write(b'dfadfadf\\xa0:::::')""")
-    with pytest.raises(UnicodeDecodeError):
-        res = compat.exec_python('-c', prog)
-    out, err = capsys.readouterr()
-    assert 'bytes around the offending' in err
+    prog = (
+        "import sys; "
+        "sys.stdout.buffer.write(b'dfadfadf\\xa0:::::'); "
+        "sys.stdout.buffer.write(b'MessageOfInterest')"
+    )
+
+    out = compat.exec_python('-c', prog)
+    assert 'MessageOfInterest' in out
