8dc60044b8582ea979b3702b6cc95a8622bb7e62
Rok Mandeljc <rok.mandeljc@gmail.com>
2020-10-22 20:03:30 +0200

    tests: add test for pkg_resources package content listing
    The test script tests for behavior of resource_exists(), resource_isdir(),
    and resource_listdir() functions from pkg_resources package (which
    in turn call the methods with same name in the provider class).
    
    The idea is to run the test script twice, once as unfrozen python
    script and once as a frozen program. In both cases, the test package
    is once present as a plain package directory and once as a zipped egg
    (generated on-the-fly from the source directory).
    
    This way, we test behavior of the original provider (DefaultProvider
    or ZipProvider) and the provider used within the frozen application
    (which we will need to implement to replace the currently used
    NullProvider).

---
+++ tests/functional/modules/pyi_pkg_resources_provider/package/pyi_pkgres_testpkg/__init__.py
@@ -0,0 +1,2 @@
+from . import a, b  # noqa: F401
+from . import subpkg1, subpkg2, subpkg3  # noqa: F401
---
+++ tests/functional/modules/pyi_pkg_resources_provider/package/setup.py
@@ -0,0 +1,33 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2020, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License (version 2
+# or later) with exception for distributing the bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#
+# SPDX-License-Identifier: (GPL-2.0-or-later WITH Bootloader-exception)
+#-----------------------------------------------------------------------------
+#
+# This assists in creating an ``.egg`` package for use with the
+# ``test_pkg_resources_provider``. To build the package, execute
+# ``python setup.py bdist_egg``.
+
+import setuptools
+
+setuptools.setup(
+    name='pyi_pkgres_testpkg',
+    version='1.0.0',
+    setup_requires="setuptools >= 40.0.0",
+    author='PyInstaller development team',
+    packages=setuptools.find_packages(),
+    package_data={
+        "pyi_pkgres_testpkg": [
+            "subpkg1/data/*.txt",
+            "subpkg1/data/*.md",
+            "subpkg1/data/*.rst",
+            "subpkg1/data/extra/*.json",
+            "subpkg3/*.json",
+        ],
+    }
+)
