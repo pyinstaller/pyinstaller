0c3112a1d3e541c017dc860de825e12b2f5d13c0
Rok Mandeljc <rok.mandeljc@gmail.com>
2020-12-02 11:58:32 +0100

    bootloader: on linux, fully resolve the executable name while preserving process name (#5232)
    * bootloader: re-introduce executable search via /proc/self/exe on linux
    
    Reverts ebcd49c and re-introduces the resolution of executable
    via readlink() on corresponding /proc/* entry under POSIX OSes
    (Linux, FreeBSD, and solaris).
    
    Not fully resolving symlinks introduces several regressions, as
    outlined in the issues referenced below.
    
    Fixes #4674, #4693, and #5179.
    
    * bootloader: on linux, pass the parent process' name to the child
    
    On linux, use prctl() and _PYI_PROCNAME environment variable to pass
    the name of the parent launcher process to the child.
    
    Keeps the test_symlink_basename_is_kept happy while fully resolving
    the executable symlink.

--- bootloader/src/pyi_path.c
+++ bootloader/src/pyi_path.c
@@ -290,31 +290,47 @@ pyi_path_executable(char *execfile, const char *appname)
     }
 
 #else /* ifdef _WIN32 */
-    char buffer[PATH_MAX];
+    /* On Linux, FreeBSD, and Solaris, we try these /proc paths first
+     */
+    size_t name_len = -1;
 
-    if (appname[0] == PYI_SEP || strchr(appname, PYI_SEP)) {
-        /* Absolute or relative path: Canonicalize directory path,
-         * but keep original basename.
-         */
-        if (pyi_path_fullpath_keep_basename(execfile, appname) == false) {
-            VS("LOADER: Cannot get fullpath for %s\n", execfile);
-            return false;
-        }
-    }
-    else {
-        /* No absolute or relative path, just program name: search $PATH.
-         */
-        if (! pyi_search_path(buffer, appname)) {
-            /* Searching $PATH failed, user is crazy. */
-            VS("LOADER: Searching $PATH failed for %s", appname);
-            if (snprintf(buffer, PATH_MAX, "%s", appname) >= PATH_MAX) {
-                VS("LOADER: Appname too large %s\n", appname);
+    #if defined(__linux__)
+    name_len = readlink("/proc/self/exe", execfile, PATH_MAX-1);  /* Linux */
+    #elif defined(__FreeBSD__)
+    name_len = readlink("/proc/curproc/file", execfile, PATH_MAX-1);  /* FreeBSD */
+    #elif defined(__sun)
+    name_len = readlink("/proc/self/path/a.out", execfile, PATH_MAX-1);  /* Solaris */
+    #endif
+
+    if (name_len != -1) {
+        /* execfile is not yet zero-terminated. result is the byte count. */
+        *(execfile + name_len) = '\0';
+    } else {
+        if (strchr(appname, PYI_SEP)) {
+            /* Absolute or relative path: Canonicalize directory path,
+             * but keep original basename.
+             */
+            if (pyi_path_fullpath_keep_basename(execfile, appname) == false) {
+                VS("LOADER: Cannot get fullpath for %s\n", execfile);
                 return false;
             }
         }
-        if (pyi_path_fullpath_keep_basename(execfile, buffer) == false) {
-            VS("LOADER: Cannot get fullpath for %s\n", execfile);
-            return false;
+        else {
+            /* No absolute or relative path, just program name: search $PATH.
+             */
+            char buffer[PATH_MAX];
+            if (! pyi_search_path(buffer, appname)) {
+                /* Searching $PATH failed, user is crazy. */
+                VS("LOADER: Searching $PATH failed for %s", appname);
+                if (snprintf(buffer, PATH_MAX, "%s", appname) >= PATH_MAX) {
+                    VS("LOADER: Full path to application exceeds PATH_MAX: %s\n", appname);
+                    return false;
+                }
+            }
+            if (pyi_path_fullpath_keep_basename(execfile, buffer) == false) {
+                VS("LOADER: Cannot get fullpath for %s\n", execfile);
+                return false;
+            }
         }
     }
 #endif /* ifdef _WIN32 */
