fcb4966e808e0326f0ae79cccebea23c2151e55c
Rok Mandeljc <rok.mandeljc@gmail.com>
2021-01-26 13:30:53 +0100

    utils/osx: fix_exe_for_code_signing(): add support for universal2 fat binaries
    On macOS, fix_exe_for_code_signing() function is used to make the
    frozen executable Mach-O format compliant by enlarging the LC_SYMTAB
    section/segment to cover the embedded .pkg archive.
    
    The current implementation assumes a single-arch Mach-O thin binary
    (with additional assumptions that hold true only for x86_64), and
    needs to be extended to support fat binaries and arm64 slices:
    * we need to modify the last available header/slice instead of
      the first one. In single-arch binary (thin or fat) this makes
      no difference, but in multi-arch fat binary, the last slice is
      the one that is adjacent to the embedded archive
    * we need to account for the header's offset. The offset always seem
      to be zero for header in the thin binary, but in fat binaries
      it is non-zero even if only single arch slice is present.
    * the total file size needs to be obtained from the file itself
      via stat. In fat binary, the reported header.size does not seem
      to include the appended data. (It makes more sense to do stat,
      anyway).
    * in a fat binary, we need to modify the fat header size for the
      architecture slice that we are modifying. Unfortunately, macholib
      does not seem to support that, so we need to do it ourselves.
    * compute the size delta only once, and use it to adjust the
      size of string table in SYMTAB section and the size of its
      parent __LINKEDIT segment
    * retrieve __LINKEDIT segment by its name instead of hard-coded
      position index. There is no guarantee that there are only four
      segments present even on x86_64 (the toolchain could potentially
      introduce additional segments), and there is in fact an additional
      __DATA_CONST segment present on arm64.
    * do not adjust vmsize on __LINKEDIT segment on any platform. The
      codesign validation seems to be just as happy without it, and it
      seems a waste to have all appended data loaded into executable's
      address space if not necessary.

--- PyInstaller/utils/osx.py
+++ PyInstaller/utils/osx.py
@@ -19,7 +19,8 @@ import shutil
 
 from PyInstaller.compat import base_prefix
 from macholib.MachO import MachO
-from macholib.mach_o import LC_BUILD_VERSION, LC_VERSION_MIN_MACOSX
+from macholib.mach_o import LC_BUILD_VERSION, LC_VERSION_MIN_MACOSX, \
+    LC_SEGMENT_64, LC_SYMTAB, LC_CODE_SIGNATURE
 
 
 def is_homebrew_env():
@@ -140,29 +141,94 @@ def fix_exe_for_code_signing(filename):
       of the table to cover the end of the file.
     - Fix the size of the __LINKEDIT segment.
 
+    Note: the above fix works only if the single-arch thin executable or
+    the last arch slice in a multi-arch fat executable is not signed,
+    because LC_CODE_SIGNATURE comes after LC_SYMTAB, and because modification
+    of headers invalidates the code signature. On modern arm64 macOS, code
+    signature is mandatory, and therefore compilers create a dummy
+    signature when executable is built. In such cases, that signature
+    needs to be removed before this function is called.
+
     Mach-O format specification:
 
     http://developer.apple.com/documentation/Darwin/Reference/ManPages/man5/Mach-O.5.html
     """
-    exe_data = MachO(filename)
-    # Every load command is a tupple: (cmd_metadata, segment, [section1, section2])
-    cmds = exe_data.headers[0].commands  # '0' - Exe contains only one architecture.
-    file_size = exe_data.headers[0].size
+    # Estimate the file size after data was appended
+    file_size = os.path.getsize(filename)
 
-    ## Make the embedded .pkg archive part of the Mach-O 'String Table'.
-    # Data about 'String Table' is in LC_SYMTAB load command.
-    for c in cmds:
-        if c[0].get_cmd_name() == 'LC_SYMTAB':
-            data = c[1]
-            # Increase the size of 'String Table' to cover the embedded .pkg file.
-            new_strsize = file_size - data.stroff
-            data.strsize = new_strsize
-    ## Fix the size of the __LINKEDIT segment.
-    # __LINKEDIT segment data is the 4th item in the executable.
-    linkedit = cmds[3][1]
-    new_segsize = file_size - linkedit.fileoff
-    linkedit.filesize = new_segsize
-    linkedit.vmsize = new_segsize
-    ## Write changes back.
-    with open(exe_data.filename, 'rb+') as fp:
-        exe_data.write(fp)
+    # Take the last available header. A single-arch thin binary contains a
+    # single slice, while a multi-arch fat binary contains multiple, and we
+    # need to modify the last one, which is adjacent to the appended data.
+    executable = MachO(filename)
+    header = executable.headers[-1]
+
+    # Sanity check: ensure the executable slice is not signed (otherwise
+    # signature's section comes last in the __LINKEDIT segment).
+    sign_sec = [cmd for cmd in header.commands
+                if cmd[0].cmd == LC_CODE_SIGNATURE]
+    assert len(sign_sec) == 0, "Executable contains code signature!"
+
+    # Find __LINKEDIT segment by name (16-byte zero padded string)
+    __LINKEDIT_NAME = b'__LINKEDIT\x00\x00\x00\x00\x00\x00'
+    linkedit_seg = [cmd for cmd in header.commands
+                    if cmd[0].cmd == LC_SEGMENT_64
+                    and cmd[1].segname == __LINKEDIT_NAME]
+    assert len(linkedit_seg) == 1, "Expected exactly one __LINKEDIT segment!"
+    linkedit_seg = linkedit_seg[0][1]  # Take the segment command entry
+    # Find SYMTAB section
+    symtab_sec = [cmd for cmd in header.commands
+                  if cmd[0].cmd == LC_SYMTAB]
+    assert len(symtab_sec) == 1, "Expected exactly one SYMTAB section!"
+    symtab_sec = symtab_sec[0][1]  # Take the symtab command entry
+    # Sanity check; the string table is located at the end of the SYMTAB
+    # section, which in turn is the last section in the __LINKEDIT segment
+    assert linkedit_seg.fileoff + linkedit_seg.filesize == \
+           symtab_sec.stroff + symtab_sec.strsize, "Sanity check failed!"
+
+    # Compute the old/declared file size (header.offset is zero for
+    # single-arch thin binaries)
+    old_file_size = \
+        header.offset + linkedit_seg.fileoff + linkedit_seg.filesize
+    delta = file_size - old_file_size
+    # Expand the string table in SYMTAB section...
+    symtab_sec.strsize += delta
+    # .. as well as its parent __LINEDIT segment
+    linkedit_seg.filesize += delta
+    # FIXME: do we actually need to adjust in-memory size as well? It
+    # seems unnecessary, as we have no use for the extended part being
+    # loaded in the executable's address space...
+    #linkedit_seg.vmsize += delta
+
+    # NOTE: according to spec, segments need to be aligned to page
+    # boundaries: 0x4000 (16 kB) for arm64, 0x1000 (4 kB) for other arches.
+    # But it seems we can get away without rounding and padding the segment
+    # size - perhaps because it's the last one?
+
+    # Write changes
+    with open(filename, 'rb+') as fp:
+        executable.write(fp)
+
+    # In fat binaries, we also need to adjust the fat header. macholib as
+    # of version 1.14 does not support this, so we need to do it ourselves...
+    if executable.fat:
+        from macholib.mach_o import FAT_MAGIC, FAT_MAGIC_64
+        from macholib.mach_o import fat_header, fat_arch, fat_arch64
+        with open(filename, 'rb+') as fp:
+            # Taken from MachO.load_fat() implementation. The fat
+            # header's signature has already been validated when we
+            # loaded the file for the first time.
+            fat = fat_header.from_fileobj(fp)
+            if fat.magic == FAT_MAGIC:
+                archs = [fat_arch.from_fileobj(fp)
+                         for i in range(fat.nfat_arch)]
+            elif fat.magic == FAT_MAGIC_64:
+                archs = [fat_arch64.from_fileobj(fp)
+                         for i in range(fat.nfat_arch)]
+            # Adjust the size in the fat header for the last slice
+            arch = archs[-1]
+            arch.size = file_size - arch.offset
+            # Now write the fat headers back to the file
+            fp.seek(0)
+            fat.to_fileobj(fp)
+            for arch in archs:
+                arch.to_fileobj(fp)
