4a29e8fc4b32869b45f44f8918c5e9363e6f3bcc
space9bug <1392669598@qq.com>
2021-02-06 14:51:04 +0800

    Add support for PyQt6

---
+++ PyInstaller/hooks/hook-PyQt6.py
@@ -0,0 +1,32 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2005-2021, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License (version 2
+# or later) with exception for distributing the bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#
+# SPDX-License-Identifier: (GPL-2.0-or-later WITH Bootloader-exception)
+#-----------------------------------------------------------------------------
+import os
+
+from PyInstaller.utils.hooks import collect_system_data_files
+from PyInstaller.utils.hooks.qt import pyqt6_library_info, get_qt_binaries
+
+# Ensure PyQt6 is importable before adding info depending on it.
+if pyqt6_library_info.version is not None:
+    hiddenimports = [
+        # PyQt5.10 and earlier uses sip in an separate package;
+        'sip',
+        # PyQt5.11 and later provides SIP in a private package. Support both.
+        'PyQt6.sip'
+    ]
+
+    # Collect the ``qt.conf`` file.
+    datas = [x for x in
+             collect_system_data_files(pyqt6_library_info.location['PrefixPath'],
+                                       os.path.join('PyQt6', 'Qt'))
+             if os.path.basename(x[0]) == 'qt.conf']
+
+    # Collect required Qt binaries.
+    binaries = get_qt_binaries(pyqt6_library_info)
