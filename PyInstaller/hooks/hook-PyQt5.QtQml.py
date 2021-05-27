716fbc07f8992708422fe960a60577374ca4e1e3
Rok Mandeljc <rok.mandeljc@gmail.com>
2021-05-27 22:04:07 +0200

    hookutils/qt: add get_qt_qml_files() function
    Simplify PyQt5.QtQml and PySide2.QtQml hooks by
    extracting the duplicated code into common function.

--- PyInstaller/hooks/hook-PyQt5.QtQml.py
+++ PyInstaller/hooks/hook-PyQt5.QtQml.py
@@ -8,32 +8,10 @@
 #
 # SPDX-License-Identifier: (GPL-2.0-or-later WITH Bootloader-exception)
 #-----------------------------------------------------------------------------
-import os
+from PyInstaller.utils.hooks.qt import pyqt5_library_info, \
+    add_qt5_dependencies, get_qt_qml_files
 
-from PyInstaller.utils import misc
-from PyInstaller.utils.hooks.qt import pyqt5_library_info, add_qt5_dependencies
-from PyInstaller import log as logging
-
-# Ensure PyQt5 is importable before adding info depending on it.
-if pyqt5_library_info.version is not None:
-    logger = logging.getLogger(__name__)
-
-    hiddenimports, binaries, datas = add_qt5_dependencies(__file__)
-
-    qmldir = pyqt5_library_info.location['Qml2ImportsPath']
-    # Per https://github.com/pyinstaller/pyinstaller/pull/3229#issuecomment-359735031,
-    # not all PyQt5 installs have QML files. In this case, ``qmldir`` is empty. In
-    # addition, the directory may not exist even if ``qmldir`` is not empty, per
-    # https://github.com/pyinstaller/pyinstaller/issues/3864.
-    if not os.path.exists(qmldir):
-        logger.warning('Unable to find Qt5 QML files. QML files not packaged.')
-    else:
-        qml_rel_dir = os.path.join(pyqt5_library_info.qt_rel_dir, 'qml')
-        datas += [(qmldir, qml_rel_dir)]
-        binaries += [
-            # Produce ``/path/to/Qt/Qml/path_to_qml_binary/qml_binary,
-            # PyQt5/Qt/Qml/path_to_qml_binary``.
-            (f, os.path.join(qml_rel_dir,
-                             os.path.dirname(os.path.relpath(f, qmldir))))
-            for f in misc.dlls_in_subdirs(qmldir)
-        ]
+hiddenimports, binaries, datas = add_qt5_dependencies(__file__)
+qml_binaries, qml_datas = get_qt_qml_files(pyqt5_library_info)
+binaries += qml_binaries
+datas += qml_datas
