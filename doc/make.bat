26b6e0f8a3c4192f903c2cf48e3416c0c199200c
bwoodsend <bwoodsend@gmail.com>
2021-06-14 16:22:48 +0100

    Doc: Build with nitpick (validate cross-references) enabled.
    And whitelist some cross references that have no target (yet).

--- doc/make.bat
+++ doc/make.bat
@@ -6,7 +6,7 @@ if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
 set BUILDDIR=_build
-set ALLSPHINXOPTS=-d %BUILDDIR%/doctrees %SPHINXOPTS% .
+set ALLSPHINXOPTS=-n -d %BUILDDIR%/doctrees %SPHINXOPTS% .
 set I18NSPHINXOPTS=%SPHINXOPTS% .
 if NOT "%PAPER%" == "" (
 	set ALLSPHINXOPTS=-D latex_paper_size=%PAPER% %ALLSPHINXOPTS%
