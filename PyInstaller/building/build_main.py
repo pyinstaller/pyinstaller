3b37b0fe12bb8b91a2d3e7e78f09a90e4f969b45
Ankith <46915066+ankith26@users.noreply.github.com>
2021-08-02 11:53:14 +0530

    Fix hookloader function bug when whitespaces in path

--- PyInstaller/building/build_main.py
+++ PyInstaller/building/build_main.py
@@ -127,7 +127,9 @@ def discover_hook_directories():
                       file=sys.stderr)
     """)
 
-    for line in output.split():
+    # use splitlines rather than split, because split can break on any white
+    # spaces in the path
+    for line in output.splitlines():
         # Filter out extra output by checking for the special prefix
         # and suffix
         if line.startswith("$_pyi:") and line.endswith("*"):
