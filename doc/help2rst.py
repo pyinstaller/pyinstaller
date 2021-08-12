bd359b7ec652dda52b47d663fcd05d3d8e31940e
Rok Mandeljc <rok.mandeljc@gmail.com>
2021-08-12 21:21:32 +0200

    Manual fixup of python files in doc

--- doc/help2rst.py
+++ doc/help2rst.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #-----------------------------------------------------------------------------
 # Copyright (c) 2015-2021, PyInstaller Development Team.
 # Copyright (c) 2015-2020, Hartmut Goebel.
@@ -16,14 +16,12 @@ This script reformats the output of `myprog --help` to decent rst.
 There are two sphinx plugins which could replace this eventually:
   - https://github.com/ashb/sphinx-argparse
   - https://github.com/gaborbernat/sphinx-argparse-cli
-The former's output looks really nice but lacks support for cross referencing
-arguments. The latter supports cross referencing but its output looks hideous.
-Hopefully either one of them will evolve into something we can use eventually.
+The former's output looks really nice, but lacks support for cross-referencing arguments. The latter supports
+cross-referencing, but its output looks hideous. Hopefully either one of them will eventually evolve into something
+we can use.
 
-The main functions here are parser_to_rst() and help_to_rst(). Throughout this
-code the **cross_references** option controls whether or not section headings
-and options should be given cross reference targets.
-
+The main functions here are parser_to_rst() and help_to_rst(). Throughout this code the, **cross_references** option
+controls whether or not section headings and options should be given cross reference targets.
 """
 
 from textwrap import indent, wrap, dedent
@@ -33,8 +31,9 @@ from functools import partial
 
 
 def parser_to_rst(parser: ArgumentParser, cross_references=True):
-    """Extract the ``--help`` output from an argparse parser and convert it to
-    restructured text."""
+    """
+    Extract the ``--help`` output from an argparse parser and convert it to restructured text.
+    """
     help = parser.format_help()
     return help_to_rst(help, cross_references)
 
@@ -47,28 +46,28 @@ SECTION_REGEX = re.compile(
 
     # Followed by a non-zero number of either blank lines or indented lines.
     (?:(?:\ +.*)?\n)+
-
 """, re.MULTILINE | re.VERBOSE
 )
 
 
 def help_to_rst(help: str, cross_references=True):
-    """Convert the output of a ``cli --help`` call to rst."""
+    """
+    Convert the output of a ``cli --help`` call to rst.
+    """
     summary, *sections = SECTION_REGEX.findall(help)
 
-    # We could stick the summary (``usage: pyinstaller [-h] [--help] ...``)
-    # into a code block but it's pretty unhelpful so I'm choosing to omit it.
+    # We could stick the summary (``usage: pyinstaller [-h] [--help] ...``) into a code block but it's pretty unhelpful,
+    # so I'm choosing to omit it.
     sections = "\n".join(section_to_rst(section, cross_references) for section in sections)
 
     return sections
 
 
 def section_to_rst(section: str, cross_references=True) -> str:
-    """Convert a single option group's ``--help`` output to rst.
+    """
+    Convert a single option group's ``--help`` output to rst.
 
-    This generates a heading for the option group followed by each option
-    within that group.
-
+    This generates a heading for the option group followed by each option within that group.
     """
     title, body = section.split("\n", maxsplit=1)
 
@@ -90,8 +89,7 @@ OPTION_REGEX = re.compile(
     (?:\ {2,}(.*))?\n
 
     # More lines of description.
-    # Each line starts with more spaces than which prefixed the option name (so
-    # as to avoid picking up the next option).
+    # Each line starts with more spaces than which prefixed the option name (so as to avoid picking up the next option).
     # Blank lines are allowed.
     (((?:\1\ +(.*))?\n)*)
 
@@ -100,14 +98,14 @@ OPTION_REGEX = re.compile(
 
 
 def option_to_rst(m: re.Match, cross_references=True) -> str:
-    """Convert a single option to rst.
+    """
+    Convert a single option to rst.
 
     The output should look like::
 
         .. option:: --option-name -n
 
             The help for that option nicely text-wrapped.
-
     """
     name = m.group(2)
     assert name
@@ -117,14 +115,15 @@ def option_to_rst(m: re.Match, cross_references=True) -> str:
     # Re-wrap the help block.
     body = "\n".join(wrap(dedent(body), width=75, break_on_hyphens=False, break_long_words=False))
 
-    template = ".. option:: {}\n\n{}\n\n" \
-        if cross_references else "{}\n\n{}\n\n"
+    template = ".. option:: {}\n\n{}\n\n" if cross_references else "{}\n\n{}\n\n"
 
     return template.format(name, indent(body, "    "))
 
 
 def rst_headerise(title: str, cross_references=True) -> str:
-    """Create a title with the correct length '---' underline."""
+    """
+    Create a title with the correct length '---' underline.
+    """
     title = title.strip(" \n:").title()
     out = f"{title}\n{'-' * len(title)}\n\n"
     if cross_references:
