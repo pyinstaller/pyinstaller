3ef900ad63f57310b27dfc3d472ee86d73f619b0
Rok Mandeljc <rok.mandeljc@gmail.com>
2021-08-12 23:26:23 +0200

    Manual fixup of files located in bootloader/

--- bootloader/tools/strip.py
+++ bootloader/tools/strip.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 #-----------------------------------------------------------------------------
 # Copyright (c) 2014-2021, PyInstaller Development Team.
 #
