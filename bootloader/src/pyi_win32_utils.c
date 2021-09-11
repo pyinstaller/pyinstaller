2f213d8813d04af35cf216031c64e634e2c11623
Rok Mandeljc <rok.mandeljc@gmail.com>
2021-09-11 20:09:32 +0200

    bootloader: Windows: remove attempt at creating our own activation context
    The frozen application always has the default activation context
    due to presence of the manifest; either external one (onedir builds)
    or embedded one (onefile builds). Therefore, our attempt at creating
    a new default activation context always fails with: An attempt to
    set the process default activation context failed because the process
    default activation context was already set.
    
    This codepath used to be required for loading the manifest in
    onefile applications. However, it did not seem to work correctly
    with the  manifest-related options (e.g., uac-admin), and has since
    been replaced by direct manifest embedding in #3746.

--- bootloader/src/pyi_win32_utils.c
+++ bootloader/src/pyi_win32_utils.c
@@ -37,13 +37,6 @@
 #include "pyi_utils.h"
 #include "pyi_win32_utils.h"
 
-static HANDLE hCtx = INVALID_HANDLE_VALUE;
-static ULONG_PTR actToken;
-
-#ifndef STATUS_SXS_EARLY_DEACTIVATION
-    #define STATUS_SXS_EARLY_DEACTIVATION 0xC015000F
-#endif
-
 #ifndef IO_REPARSE_TAG_SYMLINK
     #define IO_REPARSE_TAG_SYMLINK 0xA000000CL
 #endif
@@ -94,53 +87,6 @@ char * GetWinErrorString(DWORD error_code) {
         return "PyInstaller: pyi_win32_utils_to_utf8 failed.";
     }
     return errorString;
-}
-
-int
-CreateActContext(const char *manifestpath)
-{
-    wchar_t * manifestpath_w;
-    ACTCTXW ctx;
-    BOOL activated;
-    HANDLE k32;
-
-    HANDLE (WINAPI * CreateActCtx)(PACTCTXW pActCtx);
-    BOOL (WINAPI * ActivateActCtx)(HANDLE hActCtx, ULONG_PTR * lpCookie);
-
-    /* Setup activation context */
-    VS("LOADER: manifestpath: %s\n", manifestpath);
-    manifestpath_w = pyi_win32_utils_from_utf8(NULL, manifestpath, 0);
-
-    k32 = LoadLibraryA("kernel32");
-    CreateActCtx = (void*)GetProcAddress(k32, "CreateActCtxW");
-    ActivateActCtx = (void*)GetProcAddress(k32, "ActivateActCtx");
-
-    if (!CreateActCtx || !ActivateActCtx) {
-        VS("LOADER: Cannot find CreateActCtx/ActivateActCtx exports in kernel32.dll\n");
-        return 0;
-    }
-
-    ZeroMemory(&ctx, sizeof(ctx));
-    ctx.cbSize = sizeof(ACTCTX);
-    ctx.lpSource = manifestpath_w;
-    ctx.dwFlags = ACTCTX_FLAG_SET_PROCESS_DEFAULT;
-
-    hCtx = CreateActCtx(&ctx);
-    free(manifestpath_w);
-
-    if (hCtx != INVALID_HANDLE_VALUE) {
-        VS("LOADER: Activation context created\n");
-        activated = ActivateActCtx(hCtx, &actToken);
-
-        if (activated) {
-            VS("LOADER: Activation context activated\n");
-            return 1;
-        }
-    }
-
-    hCtx = INVALID_HANDLE_VALUE;
-    VS("LOADER: Error activating the context: ActivateActCtx: \n%s\n", GetWinErrorString(0));
-    return 0;
 }
 
 /* Convert a wide string to an ANSI string.
