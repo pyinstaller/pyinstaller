df2a02b453f3bccdf07dbf08681d1844bd220c77
Rok Mandeljc <rok.mandeljc@gmail.com>
2021-09-24 21:32:24 +0200

    loader: fix FrozenImporter.get_source for packages' __init__.py files
    If the specified fullname a.b.c is a package, we need to search
    for "a/b/c/__init__.py" instead of "a/b/c.py". Fixes errors about
    missing source files with packages that use pytorch's JIT even
    when the source files are, in fact, collected.

--- PyInstaller/loader/pyimod03_importers.py
+++ PyInstaller/loader/pyimod03_importers.py
@@ -308,6 +308,8 @@ class FrozenImporter:
         """
         if fullname in self.toc:
             # Try loading the .py file from the filesystem (only for collected modules)
+            if self.is_package(fullname):
+                fullname += '.__init__'
             filename = pyi_os_path.os_path_join(SYS_PREFIX, fullname.replace('.', pyi_os_path.os_sep) + '.py')
             try:
                 # Read in binary mode, then decode
