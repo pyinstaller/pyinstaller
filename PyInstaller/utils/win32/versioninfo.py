cf3c326a3a47409c611fb9f0a382a7af061dbfbe
bwoodsend <bwoodsend@gmail.com>
2021-10-10 21:16:05 +0100

    Cleanup: Remove a redundant import.

--- PyInstaller/utils/win32/versioninfo.py
+++ PyInstaller/utils/win32/versioninfo.py
@@ -10,7 +10,6 @@
 # SPDX-License-Identifier: (GPL-2.0-or-later WITH Bootloader-exception)
 #-----------------------------------------------------------------------------
 
-import codecs
 import struct
 
 # ::TODO:: #1920 revert to using pypi version
