0e4c82be7806d883b302b03e90c31858cd57f93c
bwoodsend <bwoodsend@gmail.com>
2021-11-13 14:49:57 +0000

    Hooks: Force all unused subprocess pipes to DEVNULL in windowed mode. (#6364)
    A windowed app's stdin, stdout and stderr are not valid pipes. Invoking
    subprocess.run() (or any of its siblings) without explicitly setting all three
    pipes to either subprocess.PIPE or subprocess.DEVNULL causes pipe inheritance
    where subprocess will attempt to use the invalid parent process's pipe. This
    causes an error.
    
    Add a runtime hook which monkey-patches subprocess.Popen() so that any unused
    pipes are replaced with DEVNULL.

---
+++ PyInstaller/hooks/rthooks/pyi_rth_subprocess.py
@@ -0,0 +1,28 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2021, PyInstaller Development Team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#
+# SPDX-License-Identifier: Apache-2.0
+#-----------------------------------------------------------------------------
+
+import subprocess
+import sys
+import io
+
+
+class Popen(subprocess.Popen):
+
+    # In windowed mode, force any unused pipes (stdin, stdout and stderr) to be DEVNULL instead of inheriting the
+    # invalid corresponding handles from this parent process.
+    if sys.platform == "win32" and not isinstance(sys.stdout, io.IOBase):
+
+        def _get_handles(self, stdin, stdout, stderr):
+            stdin, stdout, stderr = (subprocess.DEVNULL if pipe is None else pipe for pipe in (stdin, stdout, stderr))
+            return super()._get_handles(stdin, stdout, stderr)
+
+
+subprocess.Popen = Popen
