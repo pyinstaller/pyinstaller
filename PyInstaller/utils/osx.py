pyinstaller/pyinstaller

commit 131c4dc9ba225f215c9e6e6721761928c1607a48

macOS: robustify macOS assembly pipeline (#6188)
* osxutils: robustify the fix_exe_for_code_signing

When adjusting the size of the SYMTAB section and its parent
__LINKEDIT segment, directly compute the new size as the difference
between end-of-file and the corresponding offset. Previously, we
reconstructed the old file size from the __LINKEDIT segment's start
offset and size, and then computed the size delta based on the
new file size.

The new method is more direct and follows the basic assumption
that in the final file with appended CArchive, the SYMTAB section
and the __LINKEDIT segment must cover everything up to the end of
the file.

It also gives us some resilience against the codesign utility on
Mac OS 10.13 producing bogus __LINKEDIT sizes when removing  the
signature from the executable.

* building: macOS: disable signature removal when modifying binaries

Disable signature removal before modifying headers of the collected
binaries. Doing so makes the process faster, and avoids potential
bugs in the codesign utility (e.g., on Mac OS 10.13).

PyInstaller/utils/osx.py
@@ -186,18 +186,22 @@ def fix_exe_for_code_signing(filename):
     symtab_sec = [cmd for cmd in header.commands if cmd[0].cmd == LC_SYMTAB]
     assert len(symtab_sec) == 1, "Expected exactly one SYMTAB section!"
     symtab_sec = symtab_sec[0][1]  # Take the symtab command entry
-    # Sanity check; the string table is located at the end of the SYMTAB section, which in turn is the last section in
-    # the __LINKEDIT segment.
-    assert linkedit_seg.fileoff + linkedit_seg.filesize == symtab_sec.stroff + symtab_sec.strsize, \
-        "Sanity check failed!"
 
-    # Compute the old/declared file size (header.offset is zero for single-arch thin binaries).
-    old_file_size = header.offset + linkedit_seg.fileoff + linkedit_seg.filesize
-    delta = file_size - old_file_size
-    # Expand the string table in SYMTAB section...
-    symtab_sec.strsize += delta
-    # .. as well as its parent __LINEDIT segment.
-    linkedit_seg.filesize += delta
+    # The string table is located at the end of the SYMTAB section, which in turn is the last section in the __LINKEDIT
+    # segment. Therefore, the end of SYMTAB section should be aligned with the end of __LINKEDIT segment, and in turn
+    # both should be aligned with the end of the file (as we are in the last or the only arch slice).
+    #
+    # However, when removing the signature from the executable using codesign under Mac OS 10.13, the codesign utility
+    # may produce an invalid file, with the declared length of the __LINKEDIT segment (linkedit_seg.filesize) pointing
+    # beyond the end of file, as reported in issue #6167.
+    #
+    # We can compensate for that by not using the declared sizes anywhere, and simply recompute them. In the final
+    # binary, the __LINKEDIT segment and the SYMTAB section MUST end at the end of the file (otherwise, we have bigger
+    # issues...). So simply recompute the declared sizes as difference between the final file length and the
+    # corresponding start offset.
+    symtab_sec.strsize = file_size - symtab_sec.stroff
+    linkedit_seg.filesize = file_size - linkedit_seg.fileoff
+
     # Compute new vmsize by rounding filesize up to full page size.
     page_size = (0x4000 if _get_arch_string(header.header).startswith('arm64') else 0x1000)
     linkedit_seg.vmsize = math.ceil(linkedit_seg.filesize / page_size) * page_size

PyInstaller/building/utils.py
@@ -352,11 +352,13 @@ def checkCache(
     cache_index[basenm] = digest
     misc.save_py_data_struct(cacheindexfn, cache_index)
 
-    # On Mac OS we need relative paths to dll dependencies starting with @executable_path. Modifying headers
-    # invalidates signatures, so remove any existing signature and then re-add it after paths are rewritten.
+    # On Mac OS we need relative paths to dll dependencies starting with @executable_path. While modifying
+    # the headers invalidates existing signatures, we avoid removing them in order to speed things up (and
+    # to avoid potential bugs in the codesign utility, like the one reported on Mac OS 10.13 in #6167).
+    # The forced re-signing at the end should take care of the invalidated signatures.
     if is_darwin:
         osxutils.binary_to_target_arch(cachedfile, target_arch, display_name=fnm)
-        osxutils.remove_signature_from_binary(cachedfile)
+        #osxutils.remove_signature_from_binary(cachedfile)  # Disabled as per comment above.
         dylib.mac_set_relative_dylib_deps(cachedfile, dist_nm)
         osxutils.sign_binary(cachedfile, codesign_identity, entitlements_file)
     return cachedfile

commit d36bd7f8d05426a63a56079c7dadfdfc9765c296

osxutils: fix codesign regression in construction of universal2 executables
131c4dc broke universal2 support, because the simplified
computation of section/segment length fails to account for the
fact that their starting offsets are not relative to start of the
file, but rather to the start of the arch slice. In thin binaries,
the start of slice corresponds to the start of file, but in fat
binaries, we need to account for the non-zero header.offset that
contains the start offset of the slice.

Due to incorrectly computed section and segment sizes, the resulting
universal2 executable failed to pass strict codesign validation.

PyInstaller/utils/osx.py
@@ -198,9 +198,11 @@ def fix_exe_for_code_signing(filename):
     # We can compensate for that by not using the declared sizes anywhere, and simply recompute them. In the final
     # binary, the __LINKEDIT segment and the SYMTAB section MUST end at the end of the file (otherwise, we have bigger
     # issues...). So simply recompute the declared sizes as difference between the final file length and the
-    # corresponding start offset.
-    symtab_sec.strsize = file_size - symtab_sec.stroff
-    linkedit_seg.filesize = file_size - linkedit_seg.fileoff
+    # corresponding start offset (NOTE: the offset is relative to start of the slice, which is stored in header.offset.
+    # In thin binaries, header.offset is zero and start offset is relative to the start of file, but with fat binaries,
+    # header.offset is non-zero)
+    symtab_sec.strsize = file_size - (header.offset + symtab_sec.stroff)
+    linkedit_seg.filesize = file_size - (header.offset + linkedit_seg.fileoff)
 
     # Compute new vmsize by rounding filesize up to full page size.
     page_size = (0x4000 if _get_arch_string(header.header).startswith('arm64') else 0x1000)