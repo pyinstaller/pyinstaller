ec520365db47ca1ba10261baa23e851b3beec3bb
Rok Mandeljc <rok.mandeljc@gmail.com>
2021-12-05 20:15:15 +0100

    hooks: add support for QtWebEngineWidgets in PySide6/PyQt6
    The QtWebEngineWidgets support for PySide6/PyQt6 requires Qt6
    6.2.2 or later; earlier versions containa a bug that make it
    incompatible with our deployment strategy. Therefore, if an
    incompatible version is encountered, we now exit with an error
    message instead of producing a faulty build.

---
+++ PyInstaller/hooks/rthooks/pyi_rth_pyqt6webengine.py
@@ -0,0 +1,29 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2015-2021, PyInstaller Development Team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#
+# SPDX-License-Identifier: Apache-2.0
+#-----------------------------------------------------------------------------
+
+import os
+import sys
+
+# See ``pyi_rth_qt6.py`: use a "standard" PyQt6 layout.
+if sys.platform == 'darwin':
+    # NOTE: QtWebEngine support was added in Qt6 6.2.x series, so we do not need to worry about pre-6.0.3 path layout.
+    pyqt_path = os.path.join(sys._MEIPASS, 'PyQt6', 'Qt6')
+
+    # If QtWebEngineProcess was collected from a framework-based Qt build, we need to set QTWEBENGINEPROCESS_PATH.
+    # If not (a dylib-based build; Anaconda on macOS), it should be found automatically, same as on other OSes.
+    process_path = os.path.normpath(
+        os.path.join(
+            pyqt_path, 'lib', 'QtWebEngineCore.framework', 'Helpers', 'QtWebEngineProcess.app', 'Contents', 'MacOS',
+            'QtWebEngineProcess'
+        )
+    )
+    if os.path.exists(process_path):
+        os.environ['QTWEBENGINEPROCESS_PATH'] = process_path
---
+++ PyInstaller/hooks/rthooks/pyi_rth_pyside6webengine.py
@@ -0,0 +1,25 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2015-2021, PyInstaller Development Team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#
+# SPDX-License-Identifier: Apache-2.0
+#-----------------------------------------------------------------------------
+
+import os
+import sys
+
+if sys.platform == 'darwin':
+    # If QtWebEngineProcess was collected from a framework-based Qt build, we need to set QTWEBENGINEPROCESS_PATH.
+    # If not (a dylib-based build; Anaconda on macOS), it should be found automatically, same as on other OSes.
+    process_path = os.path.normpath(
+        os.path.join(
+            sys._MEIPASS, 'PySide6', 'Qt', 'lib', 'QtWebEngineCore.framework', 'Helpers', 'QtWebEngineProcess.app',
+            'Contents', 'MacOS', 'QtWebEngineProcess'
+        )
+    )
+    if os.path.exists(process_path):
+        os.environ['QTWEBENGINEPROCESS_PATH'] = process_path
