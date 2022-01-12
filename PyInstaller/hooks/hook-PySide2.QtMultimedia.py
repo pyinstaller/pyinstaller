ebe4ff6227d36b84d97a4fe432d6f8af1918138d
Rok Mandeljc <rok.mandeljc@gmail.com>
2022-01-12 19:22:16 +0100

    hooks: qt: handle hidden import in PySide{2,6}.QtMultimedia
    When the PySide's true_property feature is used, the QtMultimedia
    module ends up with a hidden import of QtMultimediaWidgets.
    
    As we have no way of reliably detecting the use of that feature,
    we need to  pre-emptively add QtMultimediaWidgets to the hidden
    imports in the QtMultimedia hook.

--- PyInstaller/hooks/hook-PySide2.QtMultimedia.py
+++ PyInstaller/hooks/hook-PySide2.QtMultimedia.py
@@ -12,3 +12,8 @@
 from PyInstaller.utils.hooks.qt import add_qt5_dependencies
 
 hiddenimports, binaries, datas = add_qt5_dependencies(__file__)
+
+# Using PySide2 true_properties ("from __feature__ import true_properties") causes a hidden dependency on
+# QtMultimediaWidgets python module:
+# https://github.com/qtproject/pyside-pyside-setup/blob/5.15.2/sources/shiboken2/shibokenmodule/files.dir/shibokensupport/signature/mapping.py#L577-L586
+hiddenimports += ['PySide2.QtMultimediaWidgets']
