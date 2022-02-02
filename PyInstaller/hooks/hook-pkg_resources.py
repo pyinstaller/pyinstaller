03370bb56fd7ae3e36763e828d0053e7053930be
Rok Mandeljc <rok.mandeljc@gmail.com>
2022-02-02 19:58:58 +0100

    hooks: add a work-around for compatibility with setuptools 60.7.0
    Setuptools v.60.7.0 vendored jaraco.text package in pkg_resources,
    and their VendoredImporter does not seem to play nicely with our
    FrozenImporter. So ensure we do not collect the vendored package
    (pkg_resources._vendor.jaraco) and its submodules into PYZ archive,
    but instead collect them as source .py files, so that they are
    handled by VendoredImporter at the runtime.

--- PyInstaller/hooks/hook-pkg_resources.py
+++ PyInstaller/hooks/hook-pkg_resources.py
@@ -9,7 +9,7 @@
 # SPDX-License-Identifier: (GPL-2.0-or-later WITH Bootloader-exception)
 #-----------------------------------------------------------------------------
 
-from PyInstaller.utils.hooks import collect_submodules
+from PyInstaller.utils.hooks import collect_submodules, is_module_satisfies, collect_data_files
 
 # pkg_resources keeps vendored modules in its _vendor subpackage, and does sys.meta_path based import magic to expose
 # them as pkg_resources.extern.*
@@ -28,3 +28,18 @@ excludedimports = ['__main__']
 hiddenimports += collect_submodules('packaging')
 
 hiddenimports += ['pkg_resources.markers']
+
+# As of v60.7, setuptools vendored jaraco and has pkg_resources use it. There is a custom importer,
+# pkg_resources.extern.VendorImporter, that is used to find the vendored package, and redirects
+# pkg_resources.extern.jaraco to pkg_resources._vendor.jaraco. Unfortunately, that does not seem to play nicely with
+# our FrozenImporter, so for now, we work around that by collecting all pkg_resources._vendor.jaraco submodules as
+# source .py files and let the VendorImporter deal with them (which also means we need to ensure that we do not collect
+# any of those modules into our PYZ archive).
+if is_module_satisfies("setuptools >= 60.7"):
+    excludedimpoirts = ["pkg_resources._vendor.jaraco"]
+    # Despite its appearance, this actually collects all source files from pkg_resources._vendor.jaraco (plus the
+    # "Lorem Ipsum.txt" data file from pkg_resources._vendor.jaraco.text if it is present). Calling collect_data_files
+    # on pkg_resources._vendor.jaraco itself does not work due to bug(s) in its handling of namespace packages.
+    datas = collect_data_files(
+        'pkg_resources._vendor.jaraco.functools', include_py_files=True, excludes=['**/__pycache__']
+    )
