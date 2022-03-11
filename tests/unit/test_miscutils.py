15f23a8a89be5453b3520df8fc3e667346e103a6
Jasper Harrison <legorooj@protonmail.com>
2022-03-11 11:55:05 +0000

    update copyright years

--- tests/unit/test_miscutils.py
+++ tests/unit/test_miscutils.py
@@ -1,5 +1,5 @@
 #-----------------------------------------------------------------------------
-# Copyright (c) 2005-2020, PyInstaller Development Team.
+# Copyright (c) 2005-2022, PyInstaller Development Team.
 #
 # Distributed under the terms of the GNU General Public License (version 2
 # or later) with exception for distributing the bootloader.
