e6d6f5bfe9d36c3fd1f8f821d9245fd86799461f
Zev Lee <60147316+zevlee@users.noreply.github.com>
2022-05-18 14:15:35 -0500

    Add GTK4 support

--- PyInstaller/hooks/hook-gi.repository.Graphene.py
+++ PyInstaller/hooks/hook-gi.repository.Graphene.py
@@ -13,22 +13,5 @@ Import hook for PyGObject https://wiki.gnome.org/PyGObject
 """
 
 from PyInstaller.utils.hooks.gi import get_gi_typelibs
-from PyInstaller.utils.hooks import get_hook_config, logger
 
-
-def hook(hook_api):
-    module_versions = get_hook_config(hook_api, 'gi', 'module-versions')
-    if module_versions:
-        version = module_versions.get('Gdk')
-        if not version:
-            version = module_versions.get('Gtk', '3.0')
-    else:
-        version = '3.0'
-    logger.info(f'Gdk version is {version}')
-
-    binaries, datas, hiddenimports = get_gi_typelibs('Gdk', version)
-    hiddenimports += ['gi._gi_cairo', 'gi.repository.cairo']
-
-    hook_api.add_datas(datas)
-    hook_api.add_binaries(binaries)
-    hook_api.add_imports(*hiddenimports)
+binaries, datas, hiddenimports = get_gi_typelibs('Graphene', '1.0')
