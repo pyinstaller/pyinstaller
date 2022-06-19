a6295b8f4383263090fe03b5985b9c22cae436f6
Rok Mandeljc <rok.mandeljc@gmail.com>
2022-06-19 23:47:23 +0200

    hooks: disable QtWebEngine sandboxing for Qt6 on macOS
    The QtWebEngine sandboxing in Qt 6.3.1 is apparently stricter than
    in earlier versions, and the way PyInstaller currently collects
    shared libraries from .framework bundles prevents the helper
    process from running.
    
    So disable the sandboxing until we come up with proper solution.

--- PyInstaller/hooks/rthooks/pyi_rth_pyside6webengine.py
+++ PyInstaller/hooks/rthooks/pyi_rth_pyside6webengine.py
@@ -23,3 +23,9 @@ if sys.platform == 'darwin':
     )
     if os.path.exists(process_path):
         os.environ['QTWEBENGINEPROCESS_PATH'] = process_path
+
+        # As of Qt 6.3.1, we need to disable sandboxing to make QtWebEngineProcess to work at all with the way
+        # PyInstaller currently collects libraries from Qt .framework bundles.
+        # This runtime hook should avoid importing PySide6, so we have no way of querying the version, and always
+        # disable sandboxing.
+        os.environ['QTWEBENGINE_DISABLE_SANDBOX'] = '1'
