c4ebf4a81deeb94c0185d175272cc80e8af09260
Rok Mandeljc <rok.mandeljc@gmail.com>
2022-08-08 20:17:38 +0200

    hooks: win32api: account for the new pywintypes DLL location
    Due to preservation of the directory layout, the pywintypes DLLs
    (pywintypes3X.dll and pythoncom3X.dll) are not collected into
    top-level application directory anymore, but rather into the
    pywin32_system32 sub-directory.
    
    Add this sub-directory to sys.path to keep the pywintypes' loader
    code happy...

--- PyInstaller/hooks/rthooks/pyi_rth_win32api.py
+++ PyInstaller/hooks/rthooks/pyi_rth_win32api.py
@@ -15,4 +15,18 @@
 # https://github.com/mhammond/pywin32/commit/71afa71e11e6631be611ca5cb57cda526
 # As a work-around, import pywintypes prior to win32api.
 
-import pywintypes  # noqa: F401
+# Unfortunately, __import_pywin32_system_module__ from pywintype module assumes that in a frozen application, the
+# pythoncom3X.dll and pywintypes3X.dll that are normally found in site-packages/pywin32_system32, are located
+# directly in the sys.path, without bothering to check first if they are actually available in the standard location.
+# This obviously runs afoul of our attempts at preserving the directory layout and placing them in the pywin32_system32
+# sub-directory instead of the top-level application directory. So as a work-around, add the sub-directory to sys.path
+# to keep pywintypes happy...
+import sys
+import os
+
+pywin32_system32_path = os.path.join(sys._MEIPASS, 'pywin32_system32')
+if os.path.isdir(pywin32_system32_path) and pywin32_system32_path not in sys.path:
+    sys.path.append(pywin32_system32_path)
+del pywin32_system32_path
+
+import pywintypes  # noqa: F401, E402
