39bafd0263659381a196b0854611b0ec89f00947
Rok Mandeljc <rok.mandeljc@gmail.com>
2022-10-18 14:36:40 +0200

    hooks: scipy: improve compatibility with scipy 1.9.2 on Windows
    Windows wheels for scipy 1.9.2 switched to delvewheel, so we need
    to handle the external shared library directory.

--- PyInstaller/hooks/hook-scipy.py
+++ PyInstaller/hooks/hook-scipy.py
@@ -13,9 +13,10 @@ import glob
 import os
 
 from PyInstaller.compat import is_win
-from PyInstaller.utils.hooks import get_module_file_attribute
+from PyInstaller.utils.hooks import get_module_file_attribute, is_module_satisfies, collect_delvewheel_libs_directory
 
 binaries = []
+datas = []
 
 # Package the DLL bundle that official scipy wheels for Windows ship The DLL bundle will either be in extra-dll on
 # windows proper and in .libs if installed on a virtualenv created from MinGW (Git-Bash for example)
@@ -25,6 +26,10 @@ if is_win:
         dll_glob = os.path.join(os.path.dirname(get_module_file_attribute('scipy')), location, "*.dll")
         if glob.glob(dll_glob):
             binaries.append((dll_glob, "."))
+
+# Handle delvewheel-enabled win32 wheels, which have external scipy.libs directory (scipy >= 0.9.2)
+if is_module_satisfies("scipy >= 1.9.2") and is_win:
+    datas, binaries = collect_delvewheel_libs_directory('scipy', datas=datas, binaries=binaries)
 
 # collect library-wide utility extension modules
 hiddenimports = ['scipy._lib.%s' % m for m in ['messagestream', "_ccallback_c", "_fpumode"]]
