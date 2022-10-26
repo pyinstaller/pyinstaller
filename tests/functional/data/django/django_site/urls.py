ade2817cf25f4179bc63792cef21be7110643ecb
Rok Mandeljc <rok.mandeljc@gmail.com>
2022-10-26 14:09:10 +0200

    tests: django: update the test django project
    Re-generate the test django project using
    `django-admin startproject django_site`
    under Django 4.1.2. Fixes the `ImportError: cannot import name
    'url' from 'django.conf.urls'` error due to `url` being deprecated
    in Django 3.0 and removed in 4.0.
    
    Also run the migrations to populate the database.

--- tests/functional/data/django/django_site/urls.py
+++ tests/functional/data/django/django_site/urls.py
@@ -1,22 +1,21 @@
-"""
-django_site URL Configuration
+"""django_site URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Add an import:  from blog import urls as blog_urls
-    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
+from django.urls import path
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
