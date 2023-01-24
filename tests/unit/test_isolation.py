5a73ea4f7bde63e4e6f258ab1fb3423225a6e1fd
Brénainn Woodsend <bwoodsend@gmail.com>
2023-01-24 20:54:57 +0000

    Re-license PyInstaller.isolated + its tests under MIT. [skip ci]
    It has been requested that PyInstaller.isolated be made reusable
    by other, permissively licensed projects. (#7386)
    
    That portion of the codebase has so far only picked up two contributors
    and can therefore be re-licensed with just the consent of those two.
    
    Signed-off-by: Brénainn Woodsend <bwoodsend@gmail.com>
    Signed-off-by: Rok Mandeljc <rok.mandeljc@gmail.com>

--- tests/unit/test_isolation.py
+++ tests/unit/test_isolation.py
@@ -2,11 +2,11 @@
 # Copyright (c) 2021-2023, PyInstaller Development Team.
 #
 # Distributed under the terms of the GNU General Public License (version 2
-# or later) with exception for distributing the bootloader.
+# or later) or, at the user's discretion, the MIT License.
 #
 # The full license is in the file COPYING.txt, distributed with this software.
 #
-# SPDX-License-Identifier: (GPL-2.0-or-later WITH Bootloader-exception)
+# SPDX-License-Identifier: (GPL-2.0-or-later WITH Bootloader-exception OR MIT)
 #-----------------------------------------------------------------------------
 
 import os
