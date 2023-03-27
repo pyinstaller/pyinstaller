1268547787384b10bd8091629ff5e0cd5c0920b6
Rok Mandeljc <rok.mandeljc@gmail.com>
2023-03-27 20:11:05 +0200

    tests: reorganize and clean up multiprocessing tests
    Reogranize and clean up the existing `multiprocessing` tests.
    
    Rename the main test file, the script files, and the tests
    themselves; use the name `multiprocessing` instead of `multiprocess`,
    because the latter refers to a 3rd party fork of stdlib `multiprocessing`
    that we might also support one day.
    
    Consolidate the test code into common test cases (run a sub-process,
    use a queue, use a pool, use a semaphore), and run them parametrized
    with all available start methods ('spawn', 'fork', and 'forkserver'
    on non-Windows, 'spawn' on Windows). This allows us to cover all
    start methods without duplicated code.
    
    Combine former `test_multiprocess` and `test_multiprocess_forking`
    into single `test_multiprocessing_process`, because they turned
    out to be practically identical.
    
    Ensure that all test scripts have proper `if __name__ == '__main__'`
    guards and that work when ran unfrozen. In cases where we run a
    `multiprocessing.Process', check the exit code of process to ensure
    that it ran successfully.

---
+++ tests/functional/test_multiprocessing.py
@@ -0,0 +1,54 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2005-2023, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License (version 2
+# or later) with exception for distributing the bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#
+# SPDX-License-Identifier: (GPL-2.0-or-later WITH Bootloader-exception)
+# ----------------------------------------------------------------------------
+
+import os
+import sys
+import pytest
+
+from PyInstaller.compat import is_win
+
+START_METHODS = ['spawn'] if is_win else ['spawn', 'fork', 'forkserver']
+
+
+@pytest.mark.timeout(timeout=60)
+@pytest.mark.parametrize("start_method", START_METHODS)
+def test_multiprocessing_process(pyi_builder, start_method):
+    pyi_builder.test_script("pyi_multiprocessing_process.py", app_args=[start_method])
+
+
+@pytest.mark.timeout(timeout=60)
+@pytest.mark.parametrize("start_method", START_METHODS)
+def test_multiprocessing_queue(pyi_builder, start_method):
+    pyi_builder.test_script("pyi_multiprocessing_queue.py", app_args=[start_method])
+
+
+@pytest.mark.timeout(timeout=60)
+@pytest.mark.parametrize("start_method", START_METHODS)
+def test_multiprocessing_pool(pyi_builder, start_method):
+    pyi_builder.test_script("pyi_multiprocessing_pool.py", app_args=[start_method])
+
+
+@pytest.mark.timeout(timeout=60)
+@pytest.mark.parametrize("start_method", START_METHODS)
+def test_multiprocessing_semaphore(pyi_builder, start_method, capfd):
+    pyi_builder.test_script("pyi_multiprocessing_semaphore.py", app_args=[start_method])
+
+    out, err = capfd.readouterr()
+
+    # Print the captured output and error so that it will show up in the test output.
+    sys.stderr.write(err)
+    sys.stdout.write(out)
+
+    expected = ["In main", "In subprocess"]
+
+    assert os.linesep.join(expected) in out
+    for substring in expected:
+        assert out.count(substring) == 1
