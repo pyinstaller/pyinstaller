pyinstaller/pyinstaller

commit 4a9eea97ceba69d76637684b05dc3f33978cdc70

bootloader: implement optional strict unpack mode
Implement a strict unpack mode, where a duplicated file in the
onefile build's PKG triggers a run-time error instead of a warning.

This mode is dynamically enabled at runtime based on the
`PYINSTALLER_STRICT_UNPACK_MODE` environment variable being set
and its value being different from 0.

This feature is primarily intended for the use on our CI as means
for catching file duplication in onefile builds.

bootloader/src/pyi_utils.c
@@ -582,6 +582,23 @@ pyi_remove_temp_path(const char *dir)
 }
 #endif /* ifdef _WIN32 */
 
+
+static int
+_check_strict_unpack_mode ()
+{
+    static int enabled = -1;
+    if (enabled == -1) {
+        char *env_strict = pyi_getenv("PYINSTALLER_STRICT_UNPACK_MODE"); /* strdup'd copy or NULL */
+        if (strcmp(env_strict, "0") == 0) {
+            enabled = 0;
+        } else {
+            enabled = 1;
+        }
+        free(env_strict);
+    }
+    return enabled;
+}
+
 /*
  * helper for extract2fs
  * which may try multiple places
@@ -642,12 +659,22 @@ pyi_open_target(const char *path, const char* name_)
     pyi_win32_utils_from_utf8(wchar_buffer, fnm, PATH_MAX);
 
     if (_wstat(wchar_buffer, &sbuf) == 0) {
-        OTHERERROR("WARNING: file already exists but should not: %s\n", fnm);
+        if (_check_strict_unpack_mode()) {
+            OTHERERROR("ERROR: file already exists but should not: %s\n", fnm);
+            return NULL;
+        } else {
+            OTHERERROR("WARNING: file already exists but should not: %s\n", fnm);
+        }
     }
 #else
 
     if (stat(fnm, &sbuf) == 0) {
-        OTHERERROR("WARNING: file already exists but should not: %s\n", fnm);
+        if (_check_strict_unpack_mode()) {
+            OTHERERROR("ERROR: file already exists but should not: %s\n", fnm);
+            return NULL;
+        } else {
+            OTHERERROR("WARNING: file already exists but should not: %s\n", fnm);
+        }
     }
 #endif
     /*

commit fc95133a452d7c549c38e42d3f090ebc8a4c821f

bootloader: fix crash when encountering duplicated file in CArchive/PKG
Fix crash in onefile executable when CArchive/PKG contains a
duplicated file and `PYINSTALLER_STRICT_UNPACK_MODE` environment
variable is not set. This is regression that was introduced by
4a9eea9 as part of #7273.

bootloader/src/pyi_utils.c
@@ -589,12 +589,16 @@ _check_strict_unpack_mode ()
     static int enabled = -1;
     if (enabled == -1) {
         char *env_strict = pyi_getenv("PYINSTALLER_STRICT_UNPACK_MODE"); /* strdup'd copy or NULL */
-        if (strcmp(env_strict, "0") == 0) {
-            enabled = 0;
+        if (env_strict) {
+            if (strcmp(env_strict, "0") == 0) {
+                enabled = 0;
+            } else {
+                enabled = 1;
+            }
+            free(env_strict);
         } else {
-            enabled = 1;
+            enabled = 0;
         }
-        free(env_strict);
     }
     return enabled;
 }