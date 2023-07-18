88bd03192ce65a8a2a2cbcf3a7a6c75f366ea251
axoroll7 <42817400+axoroll7@users.noreply.github.com>
2023-07-18 11:24:47 +0200

    If an image uses a custom palette + transparency, convert it to RGBA for a better alpha mask depth when used by the Pillow plugin IcoImagePlugin.
    The bit depth of the alpha channel will be higher, and the images will look better when eventually scaled to multiple sizes for the ICO format for example.

--- PyInstaller/building/icon.py
+++ PyInstaller/building/icon.py
@@ -63,7 +63,8 @@ def normalize_icon_type(icon_path: str, allowed_types: Tuple[str], convert_type:
         _generated_name = f"generated-{hashlib.sha256(icon_path.encode()).hexdigest()}.{convert_type}"
         generated_icon = os.path.join(workpath, _generated_name)
         with PILImage.open(icon_path) as im:
-            img = img.convert("RGBA")
+            if im.mode == "P" and im.info.get("transparency", None) is not None:
+                im = im.convert("RGBA")
             im.save(generated_icon)
         icon_path = generated_icon
     except PIL.UnidentifiedImageError:
