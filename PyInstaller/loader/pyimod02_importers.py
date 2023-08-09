71cac807ef350eb8171e3501370e4e349e91548c
byehack <khosravim861@gmail.com>
2023-08-09 20:24:28 +0330

    `io` is in `base_library.zip`

--- PyInstaller/loader/pyimod02_importers.py
+++ PyInstaller/loader/pyimod02_importers.py
@@ -19,6 +19,7 @@ PEP-302 and PEP-451 importers for frozen applications.
 
 import sys
 import os
+import io
 import tokenize
 
 import _frozen_importlib
@@ -49,10 +50,9 @@ def _decode_source(source_bytes):
     Based on CPython's implementation of the same functionality:
     https://github.com/python/cpython/blob/3.9/Lib/importlib/_bootstrap_external.py#L679-L688
     """
-    from io import BytesIO, IncrementalNewlineDecoder
-    source_bytes_readline = BytesIO(source_bytes).readline
+    source_bytes_readline = io.BytesIO(source_bytes).readline
     encoding = tokenize.detect_encoding(source_bytes_readline)
-    newline_decoder = IncrementalNewlineDecoder(decoder=None, translate=True)
+    newline_decoder = io.IncrementalNewlineDecoder(decoder=None, translate=True)
     return newline_decoder.decode(source_bytes.decode(encoding[0]))
 
 
