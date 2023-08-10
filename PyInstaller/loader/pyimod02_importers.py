9bc447f29bbcd164a64f2d5dd3d38ecbcabf9ea2
byehack <khosravim861@gmail.com>
2023-08-11 02:52:20 +0330

    Lazy import `pathlib` for `pyimod02_importers` (#7836)
    This effectively reverts 6cb49d9; by delaying the load of pathlib, we avoid
    having to collect urllib into base_library.zip, which in turn allows us to
    collect only parts of urllib that the application really needs (aside from
    urllib.parse required by pathlib

--- PyInstaller/loader/pyimod02_importers.py
+++ PyInstaller/loader/pyimod02_importers.py
@@ -19,9 +19,7 @@ PEP-302 and PEP-451 importers for frozen applications.
 
 import sys
 import os
-import pathlib
 import io
-import tokenize
 
 import _frozen_importlib
 
@@ -51,8 +49,10 @@ def _decode_source(source_bytes):
     Based on CPython's implementation of the same functionality:
     https://github.com/python/cpython/blob/3.9/Lib/importlib/_bootstrap_external.py#L679-L688
     """
+    # Local import to avoid including `tokenize` and its dependencies in `base_library.zip`
+    from tokenize import detect_encoding
     source_bytes_readline = io.BytesIO(source_bytes).readline
-    encoding = tokenize.detect_encoding(source_bytes_readline)
+    encoding = detect_encoding(source_bytes_readline)
     newline_decoder = io.IncrementalNewlineDecoder(decoder=None, translate=True)
     return newline_decoder.decode(source_bytes.decode(encoding[0]))
 
@@ -424,8 +424,10 @@ class PyiFrozenResourceReader:
       https://github.com/python/cpython/blob/839d7893943782ee803536a47f1d4de160314f85/Lib/importlib/abc.py#L312
     """
     def __init__(self, importer, name):
+        # Local import to avoid including `pathlib` and its dependencies in `base_library.zip`
+        from pathlib import Path
         self.importer = importer
-        self.path = pathlib.Path(sys._MEIPASS).joinpath(*name.split('.'))
+        self.path = Path(sys._MEIPASS).joinpath(*name.split('.'))
 
     def open_resource(self, resource):
         return self.files().joinpath(resource).open('rb')
