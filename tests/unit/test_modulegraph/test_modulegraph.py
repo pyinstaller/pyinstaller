55e4bd0ba4a00bd51729e3fbfd19f3e9c20e7786
Rok Mandeljc <rok.mandeljc@gmail.com>
2023-09-16 17:05:40 +0200

    modulegraph: rework support for legacy setuptools namespace packages
    Rework the support for legacy setuptools-based namespace packages
    and port it to `importlib.metadata`.
    
    The legacy namespace packages are regular packages with `__init__.py`
    that calls deprecated  `pkg_resources.declare_namespace(__name__)`.
    
    We could get rid of this altogether once we are fine with not
    supporting legacy namespace packages that are split across
    multiple locations. Currently, our test suite contains such an
    example (e.g., `test_import.py::test_nspkg1`). The code has been
    refactored so that dropping support will also be easier.
    
    To properly support legacy namespace sub-packages that are split
    across different locations, we need to scan the metadata of all
    dists, similarly to how it was done by the original `pkg_resources`
    based implementation. However, within the life-time of a module
    graph instance, we can perform the scan only once, and cache the
    results.

--- tests/unit/test_modulegraph/test_modulegraph.py
+++ tests/unit/test_modulegraph/test_modulegraph.py
@@ -1,6 +1,5 @@
 import unittest
 from PyInstaller.lib.modulegraph import modulegraph
-import pkg_resources
 import os
 import sys
 import shutil
@@ -100,54 +99,6 @@ class TestFunctions (unittest.TestCase):
         self.assertRaises(ValueError, modulegraph._eval_str_tuple, "'a', 'b'")
         self.assertRaises(ValueError, modulegraph._eval_str_tuple, "('a', ('b', 'c'))")
         self.assertRaises(ValueError, modulegraph._eval_str_tuple, "('a', ('b\", 'c'))")
-
-    def test_namespace_package_path(self):
-        class DS (object):
-            def __init__(self, path, namespace_packages=None):
-                self.location = path
-                self._namespace_packages = namespace_packages
-
-            def has_metadata(self, key):
-                if key == 'namespace_packages.txt':
-                    return self._namespace_packages is not None
-
-                raise ValueError("invalid lookup key")
-
-            def get_metadata(self, key):
-                if key == 'namespace_packages.txt':
-                    if self._namespace_packages is None:
-                        raise ValueError("no file")
-
-                    return self._namespace_packages
-
-                raise ValueError("invalid lookup key")
-
-        class WS (object):
-            def __init__(self, path=None):
-                pass
-
-            def __iter__(self):
-                yield DS("/pkg/pkg1")
-                yield DS("/pkg/pkg2", "foo\n")
-                yield DS("/pkg/pkg3", "bar.baz\n")
-                yield DS("/pkg/pkg4", "foobar\nfoo\n")
-
-        saved_ws = pkg_resources.WorkingSet
-        try:
-            pkg_resources.WorkingSet = WS
-
-            self.assertEqual(modulegraph._namespace_package_path("sys", ["appdir/pkg"]),
-                             ["appdir/pkg"])
-            self.assertEqual(modulegraph._namespace_package_path("foo", ["appdir/pkg"]),
-                             ["appdir/pkg",
-                              os.path.join("/pkg/pkg2", "foo"),
-                              os.path.join("/pkg/pkg4", "foo")])
-            self.assertEqual(modulegraph._namespace_package_path("bar.baz", ["appdir/pkg"]),
-                             ["appdir/pkg",
-                              os.path.join("/pkg/pkg3", "bar", "baz")])
-
-        finally:
-            pkg_resources.WorkingSet = saved_ws
 
     def test_os_listdir(self):
         root = os.path.join(
