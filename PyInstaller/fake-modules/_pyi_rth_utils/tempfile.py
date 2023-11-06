e157bb6c16311053d8b9dbeaa383fc76b137793c
Rok Mandeljc <rok.mandeljc@gmail.com>
2023-11-06 13:34:16 +0100

    hooks: add a hook for our _pyi_rth_utils fake package
    Add a hook for our `_pyi_rth_utils` that excludes Windows-specific
    `_pyi_rth_utils._win32` module on non-Windows.

--- PyInstaller/fake-modules/_pyi_rth_utils/tempfile.py
+++ PyInstaller/fake-modules/_pyi_rth_utils/tempfile.py
@@ -20,7 +20,8 @@ import tempfile
 # that restricts the access by passing appropriate security attributes to the `CreateDirectory` function.
 
 if os.name == 'nt':
-    from . import _win32
+    # FIXME: work-around for `from . import _win32` not working with our exclusion mechanism...
+    import _pyi_rth_utils._win32 as _win32
 
     def secure_mkdtemp(suffix=None, prefix=None, dir=None):
         """
