edf1694e2d06866fe027439aaba3d62dba5f2b86
Rok Mandeljc <rok.mandeljc@gmail.com>
2024-04-12 11:55:28 +0200

    hooks: PySide6.Qt3DRender: add hiddenimport of PySide6.QtOpenGL
    In PySide6 6.7.0, `PySide6.Qt3DRender` module added a reference
    to type system of `PySide6.QtOpenGL` module, so we need to add
    a hidden import for OSes where the `PySide6.QtOpenGL` module is
    not picked up automatically; i.e., on Windows, where the `Qt6OpenGL`
    shared  library does not appear in the binary dependency tree of
    the `PySide6.Qt3DRender` module.

--- PyInstaller/hooks/hook-PySide6.Qt3DRender.py
+++ PyInstaller/hooks/hook-PySide6.Qt3DRender.py
@@ -9,6 +9,12 @@
 # SPDX-License-Identifier: (GPL-2.0-or-later WITH Bootloader-exception)
 #-----------------------------------------------------------------------------
 
-from PyInstaller.utils.hooks.qt import add_qt6_dependencies
+from PyInstaller.utils.hooks.qt import add_qt6_dependencies, pyside6_library_info
 
 hiddenimports, binaries, datas = add_qt6_dependencies(__file__)
+
+# In PySide 6.7.0, Qt3DRender module added a reference to QtOpenGL type system. The hidden import is required on
+# Windows, while on macOS and Linux we seem to pick it up automatically due to the corresponding Qt shared library
+# appearing among binary dependencies. Keep it around on all OSes, though - just in case this ever changes.
+if pyside6_library_info.version is not None and pyside6_library_info.version >= [6, 7]:
+    hiddenimports += ['PySide6.QtOpenGL']
