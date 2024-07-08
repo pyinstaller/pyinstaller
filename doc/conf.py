bd54850e567f727e65a4d35e46d47f9b0f8d533b
Brénainn Woodsend <bwoodsend@gmail.com>
2024-07-08 08:15:50 +0100

    Bump doc requirements [skip ci] (#8644)
    Not to solve any specific -- just for the sake of not letting then
    become too stagnant.

--- doc/conf.py
+++ doc/conf.py
@@ -129,7 +129,7 @@ release = __version__
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
