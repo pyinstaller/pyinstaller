9b48c0435e106e341c5124d203fd801d956b5f33
Rok Mandeljc <rok.mandeljc@gmail.com>
2024-07-22 21:27:22 +0200

    tests: add a test for package with mixed module_collection_mode
    Add a test for package that is collected with mixed
    module_collection_mode, i.e., that has one module collected as a
    source .py file only.
    
    This verifies that PyInstaller's frozen importer and python's
    `_frozen_importlib_external.PathFinder` complement, and not exclude,
    each other. This in turn allows us to have packages that have modules
    in PYZ archive, while their binary extensions are collected as
    individual files (same goes for some pure-python modules that we
    may need to collect in source-only form).
    
    The same test is implcitly performed by any other test that uses
    a package with pure-python modules and binary extensions, but it
    is better to also have an explicit test.

---
+++ tests/functional/modules/pyi_mixed_collection_mode/hooks/hook-mypackage.py
@@ -0,0 +1,5 @@
+# mypackage.__init__ and mypackage.a are collected into PYZ archive (the default behavior), while mypackage.b is
+# collected as source .py file only.
+module_collection_mode = {
+    'mypackage.b': 'py',
+}
---
+++ tests/functional/modules/pyi_mixed_collection_mode/modules/mypackage/b.py
@@ -0,0 +1,1 @@
+# This module is collected as source .py file only.
