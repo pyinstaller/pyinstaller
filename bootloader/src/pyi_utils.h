c3d262b39b0e7ba856be43a8a35a66bc5260cbf8
Rok Mandeljc <rok.mandeljc@gmail.com>
2024-07-25 15:13:43 +0200

    bootloader: support execution via dynamic loader invocation, cont'd
    Cosmetic fixes to previous commit.
    Fix allocation of new argv to account for terminating NULL.
    Add support for onefile mode.

--- bootloader/src/pyi_utils.h
+++ bootloader/src/pyi_utils.h
@@ -56,7 +56,7 @@ int pyi_utils_set_library_search_path(const char *path);
 int pyi_utils_initialize_args(struct PYI_CONTEXT *pyi_ctx, const int argc, char *const argv[]);
 int pyi_utils_append_to_args(struct PYI_CONTEXT *pyi_ctx, const char *arg);
 void pyi_utils_free_args(struct PYI_CONTEXT *pyi_ctx);
-char **prepend_string_array(char *argv[], int argc, char *prepend);
+char *const *pyi_prepend_dynamic_loader_to_argv(const int argc, char *const argv[], char *const loader_filename);
 #endif
 
 /* Magic pattern matching */
