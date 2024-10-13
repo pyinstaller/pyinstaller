be5d8b5e453dac4e1d1918472272eb33e7cb9007
Rok Mandeljc <rok.mandeljc@gmail.com>
2024-10-13 18:10:05 +0200

    hooks: add hook for PySide6.QtGraphsWidgets
    Add hook for `PySide6.QtGraphsWidgets`, which was added in PySide6
    v6.8.0 and has a hidden dependency on `PySide6.QtQuickWidgets`
    (as well as `PySide6.QtGraphs` on Windows).

---
+++ PyInstaller/hooks/hook-PySide6.QtGraphsWidgets.py
@@ -0,0 +1,17 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2024, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License (version 2
+# or later) with exception for distributing the bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#
+# SPDX-License-Identifier: (GPL-2.0-or-later WITH Bootloader-exception)
+#-----------------------------------------------------------------------------
+
+from PyInstaller.utils.hooks.qt import add_qt6_dependencies
+
+hiddenimports, binaries, datas = add_qt6_dependencies(__file__)
+
+# These dependencies cannot seem to be inferred from linked libraries.
+hiddenimports += ['PySide6.QtQuickWidgets', 'PySide6.QtGraphs']
