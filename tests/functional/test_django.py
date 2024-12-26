5e7cc38d52bb3a4157c61c68f0e08bedfa4b6516
Rok Mandeljc <rok.mandeljc@gmail.com>
2024-12-26 23:19:45 +0100

    tests: remove xfail mark from test_django
    Remove the Windows-specific xfail mark from `test_django`.
    
    Remove the comment pertaining an old version of Django and old
    version of python.

--- tests/functional/test_django.py
+++ tests/functional/test_django.py
@@ -14,20 +14,10 @@ Functional tests for the Django content management system (CMS).
 
 import pytest
 
-from PyInstaller.compat import is_win
-from PyInstaller.utils.tests import importorskip, xfail
+from PyInstaller.utils.tests import importorskip
 
 
-# In Django 2.1, ``django/contrib/auth/password_validation.py``, line 168, which is
-#
-#   ``DEFAULT_PASSWORD_LIST_PATH = Path(__file__).resolve().parent / 'common-passwords.txt.gz'``,
-#
-# the call to ``resolve()`` causes Python 3.5 to raise an exception that ``password_validation.pyc`` does not exist.
-# Python 3.6 added the default argument ``strict=False``, which ignores this exception. This file is in the archive, but
-# not the filesystem.
 @importorskip('django')
-# Import error occurs on windows.
-@xfail(is_win, reason='Fails on windows.')
 # Django test might sometimes hang.
 @pytest.mark.timeout(timeout=7 * 60)
 def test_django(pyi_builder, monkeypatch, data_dir):
