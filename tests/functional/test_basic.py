c8c849af1cb1f09125ef0ef16bc419d2c0dab6d3
Rok Mandeljc <rok.mandeljc@gmail.com>
2025-01-02 12:23:32 +0100

    tests: remove test_base_modules_regex
    Remove `test_base_modules_regex`, since we do not construct regex
    from `PY3_BASE_MODULES` anymore.
    
    The test was also likely a no-op in the first place, since the
    test module would have been importable regardless if it ended
    in the PYZ archive or in `base_library.zip`.

--- tests/functional/test_basic.py
+++ tests/functional/test_basic.py
@@ -67,16 +67,6 @@ def test_pyz_as_external_file(pyi_builder, monkeypatch):
     pyi_builder.test_source("print('Hello Python!')")
 
 
-def test_base_modules_regex(pyi_builder):
-    """
-    Verify that the regex for excluding modules listed in PY3_BASE_MODULES does not exclude other modules.
-    """
-    pyi_builder.test_source("""
-        import resources_testmod
-        print('OK')
-        """)
-
-
 def test_celementtree(pyi_builder):
     pyi_builder.test_source("""
         from xml.etree.cElementTree import ElementTree
