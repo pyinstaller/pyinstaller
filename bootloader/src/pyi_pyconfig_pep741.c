a55e2132129ed68132e461d98eabf9f58062b696
Rok Mandeljc <rok.mandeljc@gmail.com>
2025-05-31 20:29:24 +0200

    bootloader: implement UTF8-naive PEP 741 configuration
    Implement basic proof-of-concept PEP 741 configuration helpers
    that are naive w.r.t. UTF-8; i.e., they assume that the narrow-char
    strings stored by bootloader are already in UTF-8 encoding.
    
    This is the case on Windows (where we convert the given wide-char
    strings into UTF-8 narrow-char ones), on macOS (where UTF-8 is
    the system encoding), and on most contemporary Linux systems (where
    UTF-8 should be the default as well).
    
    However, on POSIX systems, other encodings might be used by the
    system, and we need to implement support for that. As in the
    existing PEP 587 codepath, we already pre-initialize python to
    set up system locale and encoding, and can convert the input
    locale-encoded narrow-char strings (i.e., the command-line arguments
    and the resolved path to the executable) into wide-char strings
    using `Py_DecodeLocale`. However, we still need to convert these
    wide-char strings into UTF-8 narrow-char strings, which are expected
    by the PEP 741 API. Since python does not export its low-level UTF-8
    encoder (`_Py_EncodeUTF8Ex`) and since high-level `PyUnicode` API
    requires initialized interpreter, we will likely need to implement
    our own wide-char to UTF-8 converter.

--- bootloader/src/pyi_pyconfig_pep741.c
+++ bootloader/src/pyi_pyconfig_pep741.c
@@ -13,6 +13,15 @@
 
 /*
  * Functions to deal with PEP 741 python initialization configuration.
+ *
+ * NOTE: in contrast to PEP 587 API where wide-char strings are used
+ * (PyConfig_SetString, PyConfig_SetWideStringList) and narrow-char
+ * strings are assumed to be in the locale encoding (PyConfig_SetBytesString),
+ * PEP 741 API uses narrow-char strings in UTF-8 encoding (PyInitConfig_SetStr,
+ * PyInitConfig_SetStrList). On Windows, any narrow-char strings we have
+ * are already in UTF-8 encoding. On POSIX systems, they are in locale
+ * encoding, and need to be converted to UTF-8; unless locale encoding
+ * also happens to be UTF-8, but aside from macOS, we cannot assume that.
  */
 
 #include <stdlib.h>
@@ -32,8 +41,19 @@
 int
 pyi_pyconfig_pep741_set_program_name(PyInitConfig *config, const struct PYI_CONTEXT *pyi_ctx)
 {
-    /* TODO */
-    return -1;
+    const struct DYLIB_PYTHON *dylib_python = pyi_ctx->dylib_python;
+
+    /* TODO: ensure proper conversion to UTF-8 where necessary */
+    const char *program_name_utf8 = pyi_ctx->executable_filename;
+
+    if (dylib_python->PyInitConfig_SetStr(config, "program_name", program_name_utf8) < 0) {
+        const char *error_message = NULL;
+        dylib_python->PyInitConfig_GetError(config, &error_message);
+        PYI_ERROR("Failed to set program name: %s\n", error_message);
+        return -1;
+    }
+
+    return 0;
 }
 
 
@@ -43,8 +63,19 @@ pyi_pyconfig_pep741_set_program_name(PyInitConfig *config, const struct PYI_CONT
 int
 pyi_pyconfig_pep741_set_python_home(PyInitConfig *config, const struct PYI_CONTEXT *pyi_ctx)
 {
-    /* TODO */
-    return -1;
+    const struct DYLIB_PYTHON *dylib_python = pyi_ctx->dylib_python;
+
+    /* TODO: ensure proper conversion to UTF-8 where necessary */
+    const char *python_home_utf8 = pyi_ctx->application_home_dir;
+
+    if (dylib_python->PyInitConfig_SetStr(config, "home", python_home_utf8) < 0) {
+        const char *error_message = NULL;
+        dylib_python->PyInitConfig_GetError(config, &error_message);
+        PYI_ERROR("Failed to set python home path: %s\n", error_message);
+        return -1;
+    }
+
+    return 0;
 }
 
 
@@ -60,20 +91,131 @@ pyi_pyconfig_pep741_set_python_home(PyInitConfig *config, const struct PYI_CONTE
 int
 pyi_pyconfig_pep741_set_module_search_paths(PyInitConfig *config, const struct PYI_CONTEXT *pyi_ctx)
 {
-    /* TODO */
-    return -1;
+    const struct DYLIB_PYTHON *dylib_python = pyi_ctx->dylib_python;
+
+    char home_dir[PYI_PATH_MAX + 1];
+    char base_library_path[PYI_PATH_MAX + 1];
+    char lib_dynload_path[PYI_PATH_MAX + 1];
+
+    char *module_search_paths_utf8[3];
+
+    /* TODO: ensure proper conversion to UTF-8 where necessary */
+
+    /* home */
+    if (snprintf(home_dir, PYI_PATH_MAX, "%s", pyi_ctx->application_home_dir) >= PYI_PATH_MAX) {
+        return -1;
+    }
+
+    /* home/base_library.zip */
+    if (snprintf(base_library_path, PYI_PATH_MAX, "%s%c%s", home_dir, PYI_SEP, "base_library.zip") >= PYI_PATH_MAX) {
+        return -1;
+    }
+
+    /* home/lib-dynload */
+    if (snprintf(lib_dynload_path, PYI_PATH_MAX, "%s%c%s", home_dir, PYI_SEP, "lib-dynload") >= PYI_PATH_MAX) {
+        return -1;
+    }
+
+    module_search_paths_utf8[0] = base_library_path;
+    module_search_paths_utf8[1] = lib_dynload_path;
+    module_search_paths_utf8[2] = home_dir;
+
+    if (dylib_python->PyInitConfig_SetStrList(config, "module_search_paths", 3, module_search_paths_utf8) < 0) {
+        const char *error_message = NULL;
+        dylib_python->PyInitConfig_GetError(config, &error_message);
+        PYI_ERROR("Failed to set module search paths: %s\n", error_message);
+        return -1;
+    }
+
+    return 0;
 }
 
 
 /*
  * Set program arguments (sys.argv).
  */
+#ifdef _WIN32
+
+/* On Windows, we have the original argv available in wide-char format,
+ * so we need to convert it to UTF-8 narrow-char string array. */
+
 int
 pyi_pyconfig_pep741_set_argv(PyInitConfig *config, const struct PYI_CONTEXT *pyi_ctx)
 {
-    /* TODO */
-    return -1;
+    const struct DYLIB_PYTHON *dylib_python = pyi_ctx->dylib_python;
+    int ret = 0;
+
+    int argc;
+    char **argv_utf8;
+    int i;
+
+    /* Allocate */
+    argc = pyi_ctx->argc;
+    argv_utf8 = calloc(argc, sizeof(char *));
+    if (argv_utf8 == NULL) {
+        return -1;
+    }
+
+    /* Convert */
+    for (i = 0; i < argc; i++) {
+        argv_utf8[i] = pyi_win32_wcs_to_utf8(pyi_ctx->argv_w[i], NULL, 0);
+        if (argv_utf8[i] == NULL) {
+            ret = -1;
+            goto end;
+        }
+    }
+
+    /* Set */
+    if (dylib_python->PyInitConfig_SetStrList(config, "argv", argc, argv_utf8) < 0) {
+        const char *error_message = NULL;
+        dylib_python->PyInitConfig_GetError(config, &error_message);
+        PYI_ERROR("Failed to set sys.argv: %s\n", error_message);
+        ret = -1;
+    }
+
+end:
+    /* Cleanup */
+    for (i = 0; i < argc; i++) {
+        free(argv_utf8[i]);
+    }
+    free(argv_utf8);
+
+    return ret;
 }
+
+#else
+
+int
+pyi_pyconfig_pep741_set_argv(PyInitConfig *config, const struct PYI_CONTEXT *pyi_ctx)
+{
+    const struct DYLIB_PYTHON *dylib_python = pyi_ctx->dylib_python;
+
+    int argc;
+    char **argv_utf8;
+
+    /* Select original argc/argv vs. modified pyi_argc/pyi_argv */
+    /* TODO: ensure proper conversion to UTF-8 where necessary */
+    if (pyi_ctx->pyi_argv != NULL) {
+        /* Modified pyi_argc/pyi_argv are available; use those */
+        argc = pyi_ctx->pyi_argc;
+        argv_utf8 = pyi_ctx->pyi_argv;
+    } else {
+        /* Use original argc/argv */
+        argc = pyi_ctx->argc;
+        argv_utf8 = pyi_ctx->argv;
+    }
+
+    if (dylib_python->PyInitConfig_SetStrList(config, "argv", argc, argv_utf8) < 0) {
+        const char *error_message = NULL;
+        dylib_python->PyInitConfig_GetError(config, &error_message);
+        PYI_ERROR("Failed to set sys.argv: %s\n", error_message);
+        return -1;
+    }
+
+    return 0;
+}
+
+#endif
 
 
 /*
@@ -82,6 +224,94 @@ pyi_pyconfig_pep741_set_argv(PyInitConfig *config, const struct PYI_CONTEXT *pyi
 int
 pyi_pyconfig_pep741_set_runtime_options(PyInitConfig *config, const struct PYI_CONTEXT *pyi_ctx, const struct PyiRuntimeOptions *runtime_options)
 {
-    /* TODO */
-    return -1;
+    const struct DYLIB_PYTHON *dylib_python = pyi_ctx->dylib_python;
+    const char *error_message = NULL;
+
+    /* Disable site */
+    if (dylib_python->PyInitConfig_SetInt(config, "site_import", 0) < 0) {
+        dylib_python->PyInitConfig_GetError(config, &error_message);
+        PYI_ERROR("Failed to set 'site_import': %s\n", error_message);
+        return -1;
+    }
+
+    /* Do not write bytecode */
+    if (dylib_python->PyInitConfig_SetInt(config, "write_bytecode", 0) < 0) {
+        dylib_python->PyInitConfig_GetError(config, &error_message);
+        PYI_ERROR("Failed to set 'write_bytecode': %s\n", error_message);
+        return -1;
+    }
+
+    /* Configure C standard I/O streams (e.g., to apply buffered/unbuffered mode */
+    if (dylib_python->PyInitConfig_SetInt(config, "configure_c_stdio", 0) < 0) {
+        dylib_python->PyInitConfig_GetError(config, &error_message);
+        PYI_ERROR("Failed to set 'configure_c_stdio': %s\n", error_message);
+        return -1;
+    }
+
+    /* Set optimization level */
+    if (dylib_python->PyInitConfig_SetInt(config, "optimization_level", runtime_options->optimize) < 0) {
+        dylib_python->PyInitConfig_GetError(config, &error_message);
+        PYI_ERROR("Failed to set 'optimization_level': %s\n", error_message);
+        return -1;
+    }
+
+    /* Set buffered/unbuffered standard I/O streams */
+    if (dylib_python->PyInitConfig_SetInt(config, "buffered_stdio", !runtime_options->unbuffered) < 0) {
+        dylib_python->PyInitConfig_GetError(config, &error_message);
+        PYI_ERROR("Failed to set 'buffered_stdio': %s\n", error_message);
+        return -1;
+    }
+
+    /* Import verbosity */
+    if (dylib_python->PyInitConfig_SetInt(config, "verbose", runtime_options->verbose) < 0) {
+        dylib_python->PyInitConfig_GetError(config, &error_message);
+        PYI_ERROR("Failed to set 'verbose': %s\n", error_message);
+        return -1;
+    }
+
+    /* Hash seed setting */
+    if (dylib_python->PyInitConfig_SetInt(config, "use_hash_seed", runtime_options->use_hash_seed) < 0) {
+        dylib_python->PyInitConfig_GetError(config, &error_message);
+        PYI_ERROR("Failed to set 'use_hash_seed': %s\n", error_message);
+        return -1;
+    }
+
+    if (dylib_python->PyInitConfig_SetInt(config, "hash_seed", runtime_options->hash_seed) < 0) {
+        dylib_python->PyInitConfig_GetError(config, &error_message);
+        PYI_ERROR("Failed to set 'hash_seed': %s\n", error_message);
+        return -1;
+    }
+
+    /* Dev mode; should already be set during interpreter pre-initialization,
+     * but set it again, just in case. */
+    if (dylib_python->PyInitConfig_SetInt(config, "dev_mode", runtime_options->dev_mode) < 0) {
+        dylib_python->PyInitConfig_GetError(config, &error_message);
+        PYI_ERROR("Failed to set 'dev_mode': %s\n", error_message);
+        return -1;
+    }
+
+    /* Have python install signal handlers */
+    if (dylib_python->PyInitConfig_SetInt(config, "install_signal_handlers", 1) < 0) {
+        dylib_python->PyInitConfig_GetError(config, &error_message);
+        PYI_ERROR("Failed to set 'install_signal_handlers': %s\n", error_message);
+        return -1;
+    }
+
+    /* Apply W-flags and X-flags */
+    /* For simplicity, our run-time flag parser code collects these into
+     * narrow-char string arrays (which should contain only ASCII characters
+     * anyway) */
+    if (dylib_python->PyInitConfig_SetStrList(config, "warnoptions", runtime_options->num_wflags, runtime_options->wflags) < 0) {
+        dylib_python->PyInitConfig_GetError(config, &error_message);
+        PYI_ERROR("Failed to set 'warnoptions': %s\n", error_message);
+        return -1;
+    }
+
+    if (dylib_python->PyInitConfig_SetStrList(config, "xoptions", runtime_options->num_xflags, runtime_options->xflags) < 0) {
+        dylib_python->PyInitConfig_GetError(config, &error_message);
+        PYI_ERROR("Failed to set 'xoptions': %s\n", error_message);
+        return -1;
+    }
+
+    return 0;
 }
