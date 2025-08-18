c019ca33629fbc11963b51db15ef277fd3eee6e8
Rok Mandeljc <rok.mandeljc@gmail.com>
2025-08-18 20:07:52 +0200

    tests: have AppBuilder._run_executable display total run time
    Have the `AppBuilder._run_executable()` implementation include
    total run time in the "Process exited on its own..." status
    message.

--- PyInstaller/utils/conftest.py
+++ PyInstaller/utils/conftest.py
@@ -18,6 +18,7 @@ import re
 import shutil
 import subprocess
 import sys
+import time
 
 # Set a handler for the root-logger to inhibit 'basicConfig()' (called in PyInstaller.log) is setting up a stream
 # handler writing to stderr. This avoids log messages to be written (and captured) twice: once on stderr and
@@ -355,6 +356,7 @@ class AppBuilder:
 
         # Run the executable
         self._display_message('RUN-EXE', f'Running {exe_path!r}, args: {args!r}')
+        start_time = time.time()
         process = popen_implementation(args, executable=exe_path, env=prog_env, cwd=prog_cwd)
 
         # Wait for the process to finish. If no run-time (= timeout) is specified, we expect the process to exit on
@@ -365,8 +367,11 @@ class AppBuilder:
         try:
             timeout = runtime if runtime else _EXE_TIMEOUT
             stdout, stderr = process.communicate(timeout=timeout)
+            elapsed = time.time() - start_time
             retcode = process.returncode
-            self._display_message('RUN-EXE', f'Process exited on its own with return code {retcode}.')
+            self._display_message(
+                'RUN-EXE', f'Process exited on its own after {elapsed:.1f} seconds with return code {retcode}.'
+            )
         except (subprocess.TimeoutExpired) if psutil is None else (psutil.TimeoutExpired, subprocess.TimeoutExpired):
             if runtime:
                 # When 'runtime' is set, the expired timeout is a good sign that the executable was running successfully
