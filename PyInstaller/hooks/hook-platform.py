ce82d394d8f2d3679047334143327c91b51b631b
Rok Mandeljc <rok.mandeljc@gmail.com>
2025-12-15 18:57:30 +0100

    hooks: platform: exclude `ios_support` for `sys.platform != 'ios'`
    Have hook for stdlib `platform` module exclude the `_ios_support`
    module when `sys.platform != 'ios'`. This prevents unnecessary
    collection of `ctypes`-imported `libobjc` shared library if the
    latter happens to be available on the build system.

--- PyInstaller/hooks/hook-platform.py
+++ PyInstaller/hooks/hook-platform.py
@@ -10,7 +10,18 @@
 #-----------------------------------------------------------------------------
 import sys
 
+excludedimports = []
+
 # see https://github.com/python/cpython/blob/3.9/Lib/platform.py#L411
 # This will exclude `plistlib` for sys.platform != 'darwin'
 if sys.platform != 'darwin':
-    excludedimports = ["plistlib"]
+    excludedimports += ["plistlib"]
+
+# Avoid collecting `_ios_support`, which in turn triggers unnecessary collection of `libobjc` shared library if the
+# latter happens to be available on the build system.
+# See https://github.com/pyinstaller/pyinstaller/issues/9333, as well as
+# https://github.com/python/cpython/blob/v3.13.0/Lib/platform.py#L508-L521
+# and
+# https://github.com/python/cpython/blob/v3.13.0/Lib/_ios_support.py#L13
+if sys.platform != 'ios':
+    excludedimports += ["_ios_support"]
