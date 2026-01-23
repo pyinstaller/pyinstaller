6faa6db6110d8a8d413d3a82f50da60e22081137
Alain Kadar <alaink@umich.edu>
2026-01-23 13:43:42 -0500

    Use dict get method for depends key

--- PyInstaller/utils/hooks/conda.py
+++ PyInstaller/utils/hooks/conda.py
@@ -112,13 +112,10 @@ class Distribution:
         """
         dependencies = []
         # For each dependency:
-        if "depends" in self.raw.keys():
-            for dependency in self.raw["depends"]:
-                # ``dependency`` is a string of the form: "[name] [version constraints]"
-                name, *version_constraints = dependency.split(maxsplit=1)
-                dependencies.append(name)
-        else:
-            dependencies.append("[]")
+        for dependency in self.raw.get("depends", []):
+            # ``dependency`` is a string of the form: "[name] [version constraints]"
+            name, *version_constraints = dependency.split(maxsplit=1)
+            dependencies.append(name)
         return dependencies
 
     def _init_package_names(self):
