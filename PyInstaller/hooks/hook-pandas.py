ffe36403537857ee1f36c513744e02b4a6d15bef
Rok Mandeljc <rok.mandeljc@gmail.com>
2026-01-30 12:14:29 +0100

    hooks: pandas: use delvewheel hook utility function to collect DLLs
    Starting with `pandas` 2.1.0, their Windows PyPI wheels have been
    using `delvewheel` to put DLLs into external `pandas.libs` directory.
    
    Up until now, the bundled copy of `msvcp140-*.dll` seems to have
    been picked up automatically by binary dependency analysis;
    however, it is more future-proof to explicitly ensure that the
    external `pandas.libs` directory is collected by using the
    `collect_delvewheel_libs_directory` hook utility function.

--- PyInstaller/hooks/hook-pandas.py
+++ PyInstaller/hooks/hook-pandas.py
@@ -9,12 +9,28 @@
 # SPDX-License-Identifier: (GPL-2.0-or-later WITH Bootloader-exception)
 #-----------------------------------------------------------------------------
 
-from PyInstaller.utils.hooks import collect_submodules, check_requirement
+from PyInstaller import compat
+from PyInstaller.utils.hooks import collect_submodules, get_installer
+
+from packaging.version import Version
+
+pandas_version = Version(compat.importlib_metadata.version("pandas")).release
+pandas_installer = get_installer('pandas')
+
+datas = []
+binaries = []
 
 # Pandas keeps Python extensions loaded with dynamic imports here.
 hiddenimports = collect_submodules('pandas._libs')
 
 # Pandas 1.2.0 and later require cmath hidden import on linux and macOS. On Windows, this is not strictly required, but
 # we add it anyway to keep things simple (and future-proof).
-if check_requirement('pandas >= 1.2.0'):
+if pandas_version >= (1, 2, 0):
     hiddenimports += ['cmath']
+
+# Pandas 2.1.0 started using `delvewheel` for its Windows PyPI wheels. Ensure that DLLs from `pandas.libs` directory are
+# collected regardless of whether binary dependency analysis manages to pick them up or not. See a similar block in the
+# `numpy` hook for additional explanation.
+if compat.is_win and pandas_version >= (2, 1, 0) and pandas_installer != 'conda':
+    from PyInstaller.utils.hooks import collect_delvewheel_libs_directory
+    datas, binaries = collect_delvewheel_libs_directory("pandas", datas=datas, binaries=binaries)
