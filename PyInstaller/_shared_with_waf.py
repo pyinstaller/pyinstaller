pyinstaller/pyinstaller

commit b194152ceeab572bd879057748b3ecd064b16724

Fix some errors on non x86 platforms. (#6306)
Fix compatibility with MIPS and loongarch64 architectures. (#6306)

* Add an explicit architecture aliase for mips variants.
* Disable bootloader cross compiling support for loongarch64 (which waf doesn't
  recognise).
* Prevent inspection of machine dependent locations /usr/lib/i386-linux-gnu and
  /usr/lib/x86_64-linux-gnu are only available for x86.

PyInstaller/_shared_with_waf.py
@@ -41,9 +41,9 @@ def _pyi_machine(machine, system):
     """
     # See the corresponding tests in tests/unit/test_compat.py for examples.
 
-    if platform.machine() == "sw_64":
-        # This explicitly inhibits cross compiling the bootloader for or on a SunWay machine.
-        return "sw_64"
+    if platform.machine() == "sw_64" or platform.machine() == "loongarch64":
+        # This explicitly inhibits cross compiling the bootloader for or on SunWay and LoongArch machine.
+        return platform.machine()
 
     if system != "Linux":
         # No architecture specifier for anything par Linux.
@@ -64,8 +64,10 @@ def _pyi_machine(machine, system):
     if machine.startswith(("ppc", "powerpc")):
         # PowerPC comes in 64 vs 32 bit and little vs big endian variants.
         return "ppc"
+    if machine in ("mips64", "mips"):
+        return "mips"
     # Machines with no known aliases :)
-    if machine in ("s390x", "mips"):
+    if machine in ("s390x",):
         return machine
 
     # Unknown architectures are allowed by default, but will all be placed under one directory. In theory, trying to

tests/unit/test_compat.py
@@ -43,6 +43,8 @@ def test_exec_command_subprocess_wrong_encoding_reports_nicely(capsys):
         ("ppc32le", "ppc"),
         ("powerpc", "ppc"),
         ("s390x", "s390x"),
+        ("mips", "mips"),
+        ("mips64", "mips"),
         ("something-alien", "unknown"),
     ]
 )

PyInstaller/depend/bindepend.py
@@ -736,9 +736,15 @@ def findLibrary(name):
         paths = ['/lib', '/usr/lib']
         # Architecture dependent locations.
         if compat.architecture == '32bit':
-            paths.extend(['/lib32', '/usr/lib32', '/usr/lib/i386-linux-gnu'])
+            paths.extend(['/lib32', '/usr/lib32'])
         else:
-            paths.extend(['/lib64', '/usr/lib64', '/usr/lib/x86_64-linux-gnu'])
+            paths.extend(['/lib64', '/usr/lib64'])
+        # Machine dependent locations.
+        if compat.machine == 'intel':
+            if compat.architecture == '32bit':
+                paths.extend(['/usr/lib/i386-linux-gnu'])
+            else:
+                paths.extend(['/usr/lib/x86_64-linux-gnu'])
 
         # On Debian/Ubuntu /usr/bin/python is linked statically with libpython. Newer Debian/Ubuntu with multiarch
         # support puts the libpythonX.Y.so in paths like /usr/lib/i386-linux-gnu/.

commit 899e70fa3abaf3fa324ad08046f92a0319b4700a

_pyi_machine: Avoid using platform.machine() directly
The `machine` argument sould be used on all platforms.
This change fixes _pyi_machine tests on loongarch64.

Fixes: b194152ceeab572bd879057748b3ecd064b16724
Fixes: 018fa3fe39cba357aaa636b75f1320a4c4ca3464

PyInstaller/_shared_with_waf.py
@@ -41,9 +41,9 @@ def _pyi_machine(machine, system):
     """
     # See the corresponding tests in tests/unit/test_compat.py for examples.
 
-    if platform.machine() == "sw_64" or platform.machine() == "loongarch64":
+    if machine == "sw_64" or machine == "loongarch64":
         # This explicitly inhibits cross compiling the bootloader for or on SunWay and LoongArch machine.
-        return platform.machine()
+        return machine
 
     if system == "Windows":
         if machine.lower().startswith("arm"):

tests/unit/test_compat.py
@@ -38,6 +38,7 @@ def test_exec_command_subprocess_wrong_encoding_reports_nicely(capsys):
         ("armv7a", "arm"),
         ("arm", "arm"),
         ("aarch64", "arm"),
+        ("loongarch64", "loongarch64"),
         ("ppc64le", "ppc"),
         ("ppc64", "ppc"),
         ("ppc32le", "ppc"),