f400bdeda01b288ec44d93e2dff730fd8c15d92b
Brénainn Woodsend <bwoodsend@gmail.com>
2026-04-26 16:58:48 +0100

    Prevent import webbrowser from including libobjc.so
    Fixes #9436

---
+++ PyInstaller/hooks/hook-webbrowser.py
@@ -0,0 +1,22 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2005-2026, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License (version 2
+# or later) with exception for distributing the bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#
+# SPDX-License-Identifier: (GPL-2.0-or-later WITH Bootloader-exception)
+#-----------------------------------------------------------------------------
+import sys
+
+excludedimports = []
+
+# Avoid collecting `_ios_support`, which in turn triggers unnecessary collection of `libobjc` shared library if the
+# latter happens to be available on the build system.
+# See https://github.com/pyinstaller/pyinstaller/issues/9333, as well as
+# https://github.com/python/cpython/blob/v3.13.0/Lib/webbrowser.py#L621-L625
+# and
+# https://github.com/python/cpython/blob/v3.13.0/Lib/_ios_support.py#L13
+if sys.platform != 'ios':
+    excludedimports += ["_ios_support"]
