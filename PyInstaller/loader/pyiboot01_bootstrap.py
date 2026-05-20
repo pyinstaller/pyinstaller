c80b8e3fe2a8049107864b4a0472783453f3036e
Rok Mandeljc <rok.mandeljc@gmail.com>
2026-05-20 14:10:57 +0200

    loader: clean up the bootstrap script
    Remove the part that explicitly set `sys.prefix`, `sys.exec_prefix`,
    `sys.base_prefix`, and `sys.base_exec_prefix` to `sys._MEIPASS`.
    The surrounding comments referred to the old interpreter setup
    approach, which has since been superseded by PEP-587 and PEP-741
    codepaths in our bootloader. Therefore, these variables should now
    be properly set by the core python interpreter setup, and this
    should be verified by the up-to-date `test_python_home` (from one
    of previous commits).
    
    Remove the part that ensured that `sys.path` contains absolute
    paths; these paths are nowadays set by bootloader, so unless
    something weird is going on there, there should be no relative
    paths involved.
    
    Put the `import warnings` into try-catch block, just in case.
    
    Update couple of outdated comments.

--- PyInstaller/loader/pyiboot01_bootstrap.py
+++ PyInstaller/loader/pyiboot01_bootstrap.py
@@ -12,34 +12,23 @@
 
 def _pyi_bootstrap():
     #-- Start bootstrap process
-    # Only python built-in modules can be used.
+    # Only python built-in modules and modules from base_library.zip can be used at this point.
 
-    import sys
+    import sys  # built-in
+    import os  # base_library.zip
 
+    # Extend Python import machinery with our importer(s).
     import pyimod02_importers
-
-    # Extend Python import machinery by adding PEP302 importers to sys.meta_path.
     pyimod02_importers.install()
 
     #-- Bootstrap process is complete.
-    # We can use other python modules (e.g. os)
-
-    import os  # noqa: E402
+    # We can now use python modules that were collected into PYZ archive.
 
     # Let other python modules know that the code is running in frozen mode.
     if not hasattr(sys, 'frozen'):
         sys.frozen = True
 
-    # sys._MEIPASS is now set in the bootloader. Hooray.
-
-    # Python 3 C-API function Py_SetPath() resets sys.prefix to empty string. Python 2 was using PYTHONHOME for
-    # sys.prefix. Let's do the same for Python 3.
-    sys.prefix = sys._MEIPASS
-    sys.exec_prefix = sys.prefix
-
-    # Python 3.3+ defines also sys.base_prefix. Let's set them too.
-    sys.base_prefix = sys.prefix
-    sys.base_exec_prefix = sys.exec_prefix
+    # NOTE: sys._MEIPASS is set by the bootloader.
 
     # Some packages behave differently when running inside virtual environment. E.g., IPython tries to append path
     # VIRTUAL_ENV to sys.path. For the frozen app we want to prevent this behavior.
@@ -49,13 +38,6 @@ def _pyi_bootstrap():
         # delete it from the environment.
         os.environ[VIRTENV] = ''
         del os.environ[VIRTENV]
-
-    # Ensure sys.path contains absolute paths. Otherwise, import of other python modules will fail when current working
-    # directory is changed by the frozen application.
-    python_path = []
-    for pth in sys.path:
-        python_path.append(os.path.abspath(pth))
-        sys.path = python_path
 
     # At least on Windows, Python seems to hook up the codecs on this import, so it is not enough to just package up all
     # the encodings.
@@ -72,11 +54,13 @@ def _pyi_bootstrap():
 
     # In the Python interpreter 'warnings' module is imported when 'sys.warnoptions' is not empty. Mimic this behavior.
     if sys.warnoptions:
-        import warnings  # noqa: F401
+        try:
+            import warnings  # noqa: F401
+        except ImportError:
+            pass
 
     # Install the hooks for ctypes
     import pyimod03_ctypes  # noqa: E402
-
     pyimod03_ctypes.install()
 
     # Install the hooks for pywin32 (Windows only)
